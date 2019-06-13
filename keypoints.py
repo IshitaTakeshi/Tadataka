8800de26bea908db8806b81330bb6808fcc4297b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-06-13 20:24:12 +0900

    Clanup the keypoint extractor

--- keypoints.py
+++ keypoints.py
@@ -3,18 +3,18 @@ from skimage.feature import (match_descriptors, corner_peaks, corner_harris,
 
 
 def extract_keypoints(image1, image2):
-    keypoints1 = corner_peaks(corner_harris(image1), min_distance=5)
-    keypoints2 = corner_peaks(corner_harris(image2), min_distance=5)
-
     extractor = BRIEF(mode="normal", sigma=0.4)
 
-    extractor.extract(image1, keypoints1)
-    keypoints1 = keypoints1[extractor.mask]
-    descriptors1 = extractor.descriptors
+    def extract_(image):
+        keypoints = corner_peaks(corner_harris(image), min_distance=5)
+        extractor.extract(image, keypoints)
+        keypoints = keypoints[extractor.mask]
+        descriptors = extractor.descriptors
+        return keypoints, descriptors
 
-    extractor.extract(image2, keypoints2)
-    keypoints2 = keypoints2[extractor.mask]
-    descriptors2 = extractor.descriptors
+    keypoints1, descriptors1 = extract_(image1)
+    keypoints2, descriptors2 = extract_(image2)
 
     matches = match_descriptors(descriptors1, descriptors2, cross_check=True)
+
     return keypoints1, keypoints2, matches
