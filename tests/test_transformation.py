d3e4d83ea2bee6a9e18ebb34ff23151f1072c65a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-06-20 19:38:37 +0900

    Append one point to the point set in the test case

--- tests/test_transformation.py
+++ tests/test_transformation.py
@@ -7,7 +7,8 @@ from rigid.transformation import transform_each
 def test_transform_each():
     points = np.array([
         [1, 2, 5],
-        [4, -2, 3]
+        [4, -2, 3],
+        [0, 0, 6]
     ])
 
     rotations = np.array([
@@ -25,12 +26,12 @@ def test_transform_each():
 
     expected = np.array([
         [[2, -3, 5],   # [ 1, -5,  2] + [ 1,  2,  3]
-         [-1, 7, 7]],  # [-5,  2,  1] + [ 4,  5,  6]
-        [[5, -1, 1],   # [ 4, -3, -2] + [ 1,  2,  3]
-         [1, 3, 10]]   # [-3, -2,  4] + [ 4,  5,  6]
+         [5, -1, 1],   # [ 4, -3, -2] + [ 1,  2,  3]
+         [1, -4, 3]],  # [ 0, -6,  0] + [ 1,  2,  3]
+        [[-1, 7, 7],   # [-5,  2,  1] + [ 4,  5,  6]
+         [1, 3, 10],   # [-3, -2,  4] + [ 4,  5,  6]
+         [-2, 5, 6]]   # [-6,  0,  0] + [ 4,  5,  6]
     ])
 
-    print(transform_each(rotations, translations, points))
-    print(expected)
     assert_array_equal(transform_each(rotations, translations, points),
                        expected)
