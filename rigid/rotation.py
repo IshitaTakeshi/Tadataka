205d1b6a54f5eca3bdfe54a3fef49c6ccf06acb7
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-06-20 15:15:57 +0900

    Simplify tangent space calculation

--- rigid/rotation.py
+++ rigid/rotation.py
@@ -1,65 +1,41 @@
 from autograd import numpy as np
 
 
-# def onehot_3x3(i, j):
-#     K = np.zeros((3, 3))
-#     K[i, j] = 1
-#     return K
-#
-#
-# def tangent_so3(v):
-#     # What we want is the skew matrix in the form
-#     # [[0, -z, y],
-#     #  [z, 0, -x],
-#     #  [-y, x, 0]]
-#     # however, autograd doesn't support item assinments so what
-#     # we have to write like below
-#     return onehot_3x3(2, 1) * v[0]  # x
-#          + onehot_3x3(1, 2) * -v[0]  # -x
-#          + onehot_3x3(0, 2) * v[1]  # y
-#          + onehot_3x3(2, 0) * -v[1]  # -y
-#          + onehot_3x3(1, 0) * v[2]  # z
-#          + onehot_3x3(0, 1) * -v[2]  # -z
+def tangent_so3(V):
+    # Generate skew matrices by calculating below for
+    # all elements along the 0-th axis of V
+    #
+    #        [0  0   0]            [ 0  0  1]            [0  -1  0]
+    # V[0] * [0  0  -1]  +  V[1] * [ 0  0  0]  +  V[2] * [1   0  0]
+    #        [0  1   0]            [-1  0  0]            [0   0  0]
 
-
-
-def tangents_so3(V):
     N = V.shape[0]
 
-    def onehots(indices):
-        X = np.zeros((N, len(indices), 3, 3))
-        for i, (j, k) in enumerate(indices):
-            X[i, :, j, k] = 1
-        return X
+    def bases(indices):
+        """
+        Generate bases of so(3)
+        """
+        G = np.zeros((N, len(indices), 3, 3))
+        for k, (i, j) in enumerate(indices):
+            G[:, k, i, j] = 1
+            G[:, k, j, i] = -1
+        return G
 
     indices = [
         [2, 1],
-        [1, 2],
         [0, 2],
-        [2, 0],
         [1, 0],
-        [0, 1]
     ]
 
-    # W.shape == (N, 6)
-    W = np.vstack((
-        V[:, 0],  # x
-        -V[:, 0],  # -x
-        V[:, 1],  # y
-        -V[:, 1],  # -y
-        V[:, 2],  # z
-        -V[:, 2],  # -z
-    )).T
-
-    X = onehots(indices)
-    return np.einsum('ijkl,ij->ikl', X, W)
+    G = bases(indices)
+    return np.einsum('ijkl,ij->ikl', G, V)
 
 
 def rodrigues(V):
     """
-    # see
-    # https://docs.opencv.org/2.4/modules/calib3d/doc/
-    # camera_calibration_and_3d_reconstruction.html#rodrigues
+    see
+    https://docs.opencv.org/2.4/modules/calib3d/doc/
+    camera_calibration_and_3d_reconstruction.html#rodrigues
 
     .. codeblock:
         theta = np.linalg.norm(r)
@@ -68,19 +44,17 @@ def rodrigues(V):
         I = np.eye(3, 3)
         return I + np.sin(theta) * K + (1-np.cos(theta)) * np.dot(K, K)
 
-    # I + sin(theta) * K + (1-cos(theta)) * dot(K, K) is equivalent to
-    # cos(theta) * I + (1-cos(theta)) * outer(r, r) + sin(theta) * K
+    I + sin(theta) * K + (1-cos(theta)) * dot(K, K) is equivalent to
+    cos(theta) * I + (1-cos(theta)) * outer(r, r) + sin(theta) * K
     """
 
-    print(V.shape)
     assert(V.shape[1] == 3)
 
     N = V.shape[0]
 
-    # HACK this can be accelerated by calculating (V * V).sum(axis=1)
     theta = np.linalg.norm(V, axis=1)
     V = V / theta[:, np.newaxis]
-    K = tangents_so3(V)
+    K = tangent_so3(V)
 
     A = np.zeros((N, 3, 3))
     A[:, [0, 1, 2], [0, 1, 2]] = 1  # [np.eye(3) for i in range(N)]
