aa0308aeb822324c0065057d54594fc7e7879dd8
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-06-20 16:48:19 +0900

    Add tests for projection functions

---
+++ tests/test_projection.py
@@ -0,0 +1,39 @@
+from autograd import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+from camera import CameraParameters
+from projection.projections import pi, PerspectiveProjection
+
+
+def test_pi():
+    P = np.array([
+        [1., 2., 2.],
+        [2., 1., 4.],
+    ])
+
+    expected = np.array([
+        [0.5, 1.0],
+        [0.5, 0.25]
+    ])
+
+    assert_array_equal(pi(P), expected)
+
+
+def test_perspective_projection():
+    camera_parameters = CameraParameters(
+        focal_length=[0.8, 1.2],
+        offset=[1.0, 2.0]
+    )
+    projection = PerspectiveProjection(camera_parameters)
+    points = np.array([
+        [1.0, 2.0, 2.0],
+        [4.0, 3.0, 5.0]
+    ])
+
+    print(camera_parameters.matrix)
+
+    expected = np.array([
+        [1.4, 3.2],
+        [1.64, 2.72],
+    ])
+    assert_array_almost_equal(projection.project(points), expected)
