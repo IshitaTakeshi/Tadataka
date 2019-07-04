ccd8a2372e43af57f15b8b69cb35f552c217f75f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-04 17:10:26 +0900

    Add the (incomplete) implementation of log_so3 and the tests

--- rigid/rotation.py
+++ rigid/rotation.py
@@ -1,10 +1,49 @@
 from autograd import numpy as np
 
 
+EPSILON = 1e-8
+
+
+def log_so3(RS):
+    # FIXME
+    # this implementation cannot calculate omega correctly
+    # in the case if R is symmetric but not identity, such as
+    #
+    #     [-1 0 0]
+    # R = [0 -1 0]
+    #     [0 0 0]
+    #
+    # The corresponding omega = [0 0 0] and theta = pi but
+    # exp(omega * theta) = identity
+    #
+    # This happens because omega is at singularity
+
+    assert(RS.shape[1] == RS.shape[2] == 3)
+    N = RS.shape[0]
+
+    traces = np.trace(RS, axis1=1, axis2=2)
+    thetas = np.arccos((traces - 1) / 2)
+
+    mask = np.abs(thetas) > EPSILON
+
+    omegas = np.empty((N, 3))
+
+    omegas[np.logical_not(mask), :] = 0.
+
+    omegas[mask, 0] = RS[mask, 2, 1] - RS[mask, 1, 2]
+    omegas[mask, 1] = RS[mask, 0, 2] - RS[mask, 2, 0]
+    omegas[mask, 2] = RS[mask, 1, 0] - RS[mask, 0, 1]
+
+    sins = np.sin(thetas[mask]).reshape(-1, 1)  # align shape as omegas
+    omegas[mask] = omegas[mask] / (2 * sins)
+
+    return omegas, thetas
+
+
 def tangent_so3(V):
     """
     V: np.ndarray
-        Array of se3 elements
+        Array of se3 elements.
     """
     # Generate skew matrices by calculating below for
     # all elements along the 0-th axis of V
--- tests/test_rotation.py
+++ tests/test_rotation.py
@@ -1,6 +1,50 @@
 from autograd import numpy as np
-from numpy.testing import assert_array_equal, assert_array_almost_equal
-from rigid.rotation import tangent_so3, rodrigues
+from numpy.testing import assert_array_equal, assert_array_almost_equal, assert_equal
+from rigid.rotation import tangent_so3, log_so3, rodrigues
+
+
+def test_log_so3():
+    # FIXME the current implementation of log_so3 cannot calculate
+    # the cases commented out below
+    RS = np.array([
+        [[1, 0, 0],
+         [0, 1, 0],
+         [0, 0, 1]],
+      # [[-1, 0, 0],
+      #  [0, -1, 0],
+      #  [0, 0, 1]],
+        [[0, 0, 1],
+         [0, 1, 0],
+         [-1, 0, 0]],
+        [[1, 0, 0],
+         [0, 1 / np.sqrt(2), -1 / np.sqrt(2)],
+         [0, 1 / np.sqrt(2), 1 / np.sqrt(2)]]
+    ])
+    omegas = np.array([
+        [0, 0, 0],
+      # [0, 0, np.pi],
+        [0, np.pi / 2, 0],
+        [np.pi / 4, 0, 0]]
+    )
+
+    omegas_expected = np.array([
+        [0, 0, 0],
+      # [0, 0, 1],
+        [0, 1, 0],
+        [1, 0, 0]
+    ])
+    thetas_expected = np.array([
+        0,
+      # np.pi,
+        np.pi / 2,
+        np.pi / 4
+    ])
+
+    omegas_pred, thetas_pred = log_so3(RS)
+    assert_equal(omegas_pred.shape[0], thetas_pred.shape[0])
+    assert_equal(omegas_pred.shape, omegas.shape)
+    assert_array_almost_equal(thetas_expected, thetas_pred)
+    assert_array_almost_equal(omegas_expected, omegas_pred)
 
 
 def test_tangents_so3():
