acf190cb1763574a6d81a4d2f359b78aea261801
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-08 20:23:44 +0900

    Add a test for initialization of bundle adjustment

--- tests/test_bundle_adjustment.py
+++ tests/test_bundle_adjustment.py
@@ -1,6 +1,11 @@
 from autograd import numpy as np
-from numpy.testing import assert_array_equal
-from bundle_adjustment.bundle_adjustment import ParameterConverter
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+from bundle_adjustment.bundle_adjustment import ParameterConverter, initialize
+from camera import CameraParameters
+from rigid.transformation import transform_each
+from rigid.rotation import rodrigues
+from projection.projections import PerspectiveProjection
 
 
 def test_parameter_converter():
@@ -44,3 +49,61 @@ def test_parameter_converter():
             [10, 11, 12]
         ])
     )
+
+
+
+def test_initialize():
+    def projection(rotations, translations, points, camera_parameters):
+        points = transform_each(rotations, translations, points)
+        keypoints = PerspectiveProjection(camera_parameters).project(points.reshape(-1, 3))
+        keypoints = keypoints.reshape(points.shape[0], points.shape[1], 2)
+        return keypoints
+
+
+    camera_parameters = CameraParameters(
+        focal_length=[1., 1.],
+        offset=[0., 0.]
+    )
+
+    rotations_true = np.array([
+        [[0, 1, 0],
+         [1, 0, 0],
+         [0, 0, -1]],
+        [[0, -1, 0],
+         [0, 0, -1],
+         [1, 0, 0]]
+    ])
+
+    translations_true = np.array([
+        [0, 0, 3],
+        [0, 0, 2]
+    ])
+
+    points_true = np.array([
+        [-1, -1, -1], [-1, -1, 0], [-1, -1, 1],
+        [-1, 0, -1], [-1, 0, 0], [-1, 0, 1],
+        [-1, 1, -1], [-1, 1, 0], [-1, 1, 1],
+        [0, -1, -1], [0, -1, 0], [0, -1, 1],
+        [0, 0, -1], [0, 0, 0], [0, 0, 1],
+        [0, 1, -1], [0, 1, 0], [0, 1, 1],
+        [1, -1, -1], [1, -1, 0], [1, -1, 1],
+        [1, 0, -1], [1, 0, 0], [1, 0, 1],
+        [1, 1, -1], [1, 1, 0], [1, 1, 1],
+    ])
+
+    keypoints_true = projection(
+        rotations_true, translations_true, points_true,
+        camera_parameters
+    )
+
+    omegas_pred, translations_pred, points_pred =\
+        initialize(keypoints_true, camera_parameters.matrix)
+
+    rotations_pred = rodrigues(omegas_pred)
+
+    keypoints_pred = projection(
+        rotations_pred, translations_pred, points_pred,
+        camera_parameters
+    )
+
+    assert_array_almost_equal(keypoints_true, keypoints_pred)
