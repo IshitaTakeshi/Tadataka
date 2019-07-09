bc8a9c8bebec774e48bcfe024cb4a6610ace5c32
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-09 15:27:41 +0900

    Move tests to proper locations

---
+++ tests/test_matrix.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.testing import assert_array_almost_equal, assert_equal
+
+from matrix import solve_linear
+
+
+def test_find_kernel():
+    # some random matrix
+    A = np.array(
+        [[7, 3, 6, 7, 4, 3, 7, 2],
+         [0, 1, 5, 2, 9, 5, 9, 7],
+         [7, 5, 2, 3, 4, 1, 4, 3]]
+    )
+    x = solve_linear(A)
+    assert_equal(x.shape, (8,))
+    assert_array_almost_equal(np.dot(A, x), np.zeros(3))
