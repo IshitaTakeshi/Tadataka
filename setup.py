f0daad6f59bd434d8b02431f76513c04869082f7
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-12 19:46:46 +0900

    Add 'setup.py'

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='vitamine',
+    description='VITAMIN-E',
+    url='http://github.com/IshitaTakeshi/VITAMIN-E',
+    author='Takeshi Ishita',
+    author_email='ishitah.takeshi@gmail.com',
+    license='MIT',
+    packages=['vitamine'],
+    install_requires=[
+        'autograd',
+        'scipy',
+        'scikit-image',
+        'matplotlib',
+        'numpy'
+    ]
+)
