bb2782d2da6c211a10659ce680b8fb05608b3644
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-16 15:25:15 +0900

    Wrap the test case by a class

--- tests/optimization/test_errors.py
+++ tests/optimization/test_errors.py
@@ -4,15 +4,16 @@ from vitamine.optimization.robustifiers import SquaredRobustifier
 from vitamine.optimization.errors import SumRobustifiedNormError
 
 
-def test_sum_robustified_norm_error():
-    robustifier = SquaredRobustifier()
-    residuals = np.array([
-        [4.0, 3.0],
-        [-1.0, 2.0],
-        [3.0, 0.0]
-    ])
-    expected = 25.0 + 5.0 + 9.0
-    assert_equal(
-        SumRobustifiedNormError(robustifier).compute(residuals),
-        expected
-    )
+class TestSumRobustifiedNormError(object):
+    def test_compute(self):
+        robustifier = SquaredRobustifier()
+        residuals = np.array([
+            [4.0, 3.0],
+            [-1.0, 2.0],
+            [3.0, 0.0]
+        ])
+        expected = 25.0 + 5.0 + 9.0
+        assert_equal(
+            SumRobustifiedNormError(robustifier).compute(residuals),
+            expected
+        )
