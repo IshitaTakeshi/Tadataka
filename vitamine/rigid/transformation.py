f837322114dd2262f9cfdd1cfb0c88f5dbc55fb4
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-17 20:01:56 +0900

    Add 'inv_transform_each'

--- vitamine/rigid/transformation.py
+++ vitamine/rigid/transformation.py
@@ -2,6 +2,11 @@ from autograd import numpy as np
 
 
 def transform_each(rotations, translations, points):
+    # same as computing
+    # for R, t in zip(rotations, translations):
+    #     for p in points:
+    #         np.dot(R, p) + t
+
     # translations.shape == (n_viewpoints, 3)
     # rotations.shape == (n_viewpoints, 3)
     # points.shape == (n_points, 3)
@@ -21,6 +26,25 @@ def transform_each(rotations, translations, points):
     points = np.einsum('ijk,lk->ilj', rotations, points)
     points = points + translations
     return points
+
+
+def inv_transform_each(rotations, translations, points):
+    # same as computing
+    # for R, t in zip(rotations, translations):
+    #     for p in points:
+    #         np.dot(R.T, p-t)
+
+    # we separate the computation of np.dot(R.T, p-t) to
+    # np.dot(R.T, p) - np.dot(R.T, t)
+
+    rotations = np.swapaxes(rotations, 1, 2)  # [R.T for R in rotations]
+    # points.shape = (n_viewpoints, n_points, 3)
+    points = np.einsum('ijk,lk->ilj', rotations, points)
+    # translations.shape = (n_viewpoints, 3)
+    translations = np.einsum('ijk,ik->ij', rotations, translations)
+    shape = translations.shape
+    translations = translations.reshape(shape[0], 1, shape[1])
+    return points - translations
 
 
 def transform(R, t, X):
