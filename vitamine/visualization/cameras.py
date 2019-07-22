cba686091a57b450bdb7723cf314ef6ec96adc48
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-22 21:50:54 +0900

    Add a camera visualizer

---
+++ vitamine/visualization/cameras.py
@@ -0,0 +1,35 @@
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+import numpy as np
+
+from vitamine.rigid.transformation import transform_all
+
+
+def cameras_poly3d(camera_rotations, camera_locations):
+    # this code is the modified version of
+    # [an answer](https://stackoverflow.com/a/44920709)
+    # by [serenity](https://stackoverflow.com/users/2666859/serenity)
+
+    v = np.array([
+        [-0.5, -0.5, 1],
+        [+0.5, -0.5, 1],
+        [+0.5, +0.5, 1],
+        [-0.5, +0.5, 1],
+        [0, 0, 0]
+    ])
+
+    V = transform_all(camera_rotations, camera_locations, v)
+
+    verts = []
+    for v in V:
+        P = np.array([
+            [v[0], v[1], v[4]],
+            [v[0], v[3], v[4]],
+            [v[2], v[1], v[4]],
+            [v[2], v[3], v[4]],
+        ])
+        verts.append(P)
+    verts = np.vstack(verts)
+
+    return Poly3DCollection(verts, facecolors='cyan', linewidths=1,
+                            edgecolors='r', alpha=.25)
