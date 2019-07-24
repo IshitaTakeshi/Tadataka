4f0209414a2750d22e0f51ce3224aff3f4e1a324
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-24 19:05:14 +0900

    Add a test for the donut dataset

---
+++ tests/dataset/test_points.py
@@ -0,0 +1,61 @@
+from autograd import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+from vitamine.dataset.points import donut
+
+
+def test_donut():
+    inner_r = 4
+    outer_r = 8
+    height = 5
+    point_density = 6
+    n_viewpoints = 4
+    offset = 0.0  # 1e-2
+
+    camera_rotations, camera_locations, points =\
+        donut(inner_r, outer_r, height, point_density, n_viewpoints, offset)
+
+    assert_array_equal(points[0*point_density:2*point_density, 1],
+                       0 * np.ones(2*point_density))
+    assert_array_equal(points[2*point_density:4*point_density, 1],
+                       1 * np.ones(2*point_density))
+    assert_array_equal(points[4*point_density:6*point_density, 1],
+                       2 * np.ones(2*point_density))
+
+    for i in range(0, 2*height, 2):
+        assert_array_almost_equal(
+            points[(i+0)*point_density:(i+1)*point_density, 0],
+            4 * np.array([1, 1/2, -1/2, -1, -1/2, 1/2])
+        )
+        assert_array_almost_equal(
+            points[(i+1)*point_density:(i+2)*point_density, 0],
+            8 * np.array([1, 1/2, -1/2, -1, -1/2, 1/2])
+        )
+
+    # radius of locations is 6 (= (inner_r + outer_r) / 2)
+    assert_array_almost_equal(
+        camera_locations,
+        np.array([
+            [6, 2, 0],
+            [0, 2, 6],
+            [-6, 2, 0],
+            [0, 2, -6]
+        ])
+    )
+
+    assert_array_almost_equal(
+        camera_rotations,
+        np.array([
+            [[1, 0, 0],
+             [0, 1, 0],
+             [0, 0, 1]],
+            [[0, 0, -1],
+             [0, 1, 0],
+             [1, 0, 0]],
+            [[-1, 0, 0],
+             [0, 1, 0],
+             [0, 0, -1]],
+            [[0, 0, 1],
+             [0, 1, 0],
+             [-1, 0, 0]]
+        ])
+    )
--- vitamine/dataset/points.py
+++ vitamine/dataset/points.py
@@ -48,7 +48,8 @@ def straight_corridor(width, height, length):
     return
 
 
-def donut(inner_r, outer_r, height=5, point_density=24, n_viewpoints=61):
+def donut(inner_r, outer_r, height=5, point_density=24, n_viewpoints=60,
+          offset=1e-3):
     assert(isinstance(height, int))
     assert(outer_r > inner_r)
 
@@ -62,11 +63,11 @@ def donut(inner_r, outer_r, height=5, point_density=24, n_viewpoints=61):
 
     def rings(level_y):
         thetas = np.linspace(0, 2 * np.pi, point_density + 1)[:-1]
-        outer = outer_r * round_points(thetas)
         inner = inner_r * round_points(thetas)
-        outer[:, 1] = level_y
+        outer = outer_r * round_points(thetas)
         inner[:, 1] = level_y
-        return np.vstack((outer, inner))
+        outer[:, 1] = level_y
+        return np.vstack((inner, outer))
 
     point_ys = np.arange(height)
     points = np.vstack([rings(level_y) for level_y in point_ys])
@@ -74,7 +75,8 @@ def donut(inner_r, outer_r, height=5, point_density=24, n_viewpoints=61):
     camera_r = (inner_r + outer_r) / 2.
     camera_y = (point_ys[0] + point_ys[-1]) / 2.
 
-    thetas = np.linspace(0, 2 * np.pi, n_viewpoints)
+    # add offset to avoid division by zero at projection
+    thetas = np.linspace(0, 2 * np.pi, n_viewpoints + 1)[:-1] + offset
     camera_locations = camera_r * round_points(thetas)
     camera_locations[:, 1] = camera_y
 
