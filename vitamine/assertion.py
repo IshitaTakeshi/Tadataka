1b0cea6f25a1cf59deae1545e2fbda6db5aebd98
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-26 15:39:11 +0900

    Add a function to check shape correspondences between points, camera poses, and the keypoints generated by them

--- vitamine/assertion.py
+++ vitamine/assertion.py
@@ -6,3 +6,11 @@ def check_poses(omegas, translations):
     assert(omegas.shape[0] == translations.shape[0])
     assert(omegas.shape[1] == 3)
     assert(translations.shape[1] == 3)
+
+
+def check_keypoints(keypoints, omegas, translations, points):
+    n_points = keypoints.shape[1]
+    n_viewpoints = keypoints.shape[0]
+    assert(points.shape[0] == n_points)
+    assert(omegas.shape[0] == n_viewpoints)
+    assert(translations.shape[0] == n_viewpoints)
