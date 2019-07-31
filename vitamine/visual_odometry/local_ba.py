a06f4a0a38e900a794b31c89e8e2c3eb29d2b87e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-07-31 14:08:31 +0900

    Fix name errors in the local BA

--- vitamine/visual_odometry/local_ba.py
+++ vitamine/visual_odometry/local_ba.py
@@ -1,5 +1,6 @@
 from vitamine.bundle_adjustment.parameters import (
     ParameterMask, from_params, to_params)
+from vitamine.bundle_adjustment.bundle_adjustment import bundle_adjustment_core
 
 
 class LocalBundleAdjustment(object):
@@ -13,17 +14,15 @@ class LocalBundleAdjustment(object):
 
     def compute(self, keypoints):
         # extract non-nan elements
-        keypoints = self.mask.mask_keypoints(keypoints),
+        keypoints = self.mask.mask_keypoints(keypoints)
         params = bundle_adjustment_core(keypoints, self.initial_params,
-                                        mask.n_valid_viewpoints,
-                                        mask.n_valid_points,
+                                        self.mask.n_valid_viewpoints,
+                                        self.mask.n_valid_points,
                                         self.camera_parameters)
 
         # decompose 'params' into non-nan poses / points
         omegas, translations, points = from_params(
-            params, mask.n_valid_viewpoints, mask.n_valid_points)
+            params, self.mask.n_valid_viewpoints, self.mask.n_valid_points)
 
         # fill with nans to align the shapes
-        return mask.fill(omegas, translations, points)
-
-
+        return self.mask.fill(omegas, translations, points)
