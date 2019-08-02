ba9d77329a7ccf0c66a9041ecb452e1121714c18
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-08-02 16:14:37 +0900

    Fix tests as the original function has renamed

--- tests/visual_odometry/test_initalizers.py
+++ tests/visual_odometry/test_initalizers.py
@@ -4,11 +4,10 @@ from autograd import numpy as np
 from numpy.testing import assert_equal
 
 from vitamine.visual_odometry.initializers import (
-    Initializer, select_next_viewpoint)
+    Initializer, select_new_viewpoint)
 
 
-
-def test_select_next_viewpoint():
+def test_select_new_viewpoint():
     points = np.array([
         [1, 2, 3],
         [4, 5, 6],
@@ -28,13 +27,11 @@ def test_select_next_viewpoint():
         [[1, 2],
          [np.nan, np.nan],
          [3, 4]],
-    ],
-    dtype=np.float64)
+    ])
 
     used_viewpoints = set([1])
-    assert_equal(select_next_viewpoint(points, keypoints, used_viewpoints), 2)
-
+    assert_equal(select_new_viewpoint(points, keypoints, used_viewpoints), 2)
 
     with pytest.raises(ValueError):
-        select_next_viewpoint(points, keypoints,
-                              used_viewpoints=set([0, 1, 2, 3]))
+        select_new_viewpoint(points, keypoints,
+                             used_viewpoints=set([0, 1, 2, 3]))
