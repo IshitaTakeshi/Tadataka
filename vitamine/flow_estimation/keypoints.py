ea7591e6359ce435e45465cef255b81e670d0fe0
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-08-05 18:35:49 +0900

    Add a match visualizer

--- vitamine/flow_estimation/keypoints.py
+++ vitamine/flow_estimation/keypoints.py
@@ -1,20 +1,58 @@
+from autograd import numpy as np
+
 from skimage.feature import (match_descriptors, corner_peaks, corner_harris,
                              BRIEF)
+from skimage import transform
 
 
-def extract_keypoints(image1, image2):
-    extractor = BRIEF(mode="uniform")
+extractor = BRIEF(mode="uniform")
 
-    def extract_(image):
-        keypoints = corner_peaks(corner_harris(image), min_distance=5)
-        extractor.extract(image, keypoints)
-        keypoints = keypoints[extractor.mask]
-        descriptors = extractor.descriptors
-        return keypoints, descriptors
 
-    keypoints1, descriptors1 = extract_(image1)
-    keypoints2, descriptors2 = extract_(image2)
+def extract_keypoints(image):
+    keypoints = corner_peaks(corner_harris(image), min_distance=5)
+    extractor.extract(image, keypoints)
+    keypoints = keypoints[extractor.mask]
+    descriptors = extractor.descriptors
+    return keypoints, descriptors
 
-    matches = match_descriptors(descriptors1, descriptors2, cross_check=True)
 
-    return keypoints1, keypoints2, matches
+def match(descriptors0, descriptors1):
+    return match_descriptors(descriptors0, descriptors1,
+                             metric="hamming", cross_check=False)
+
+
+from matplotlib import pyplot as plt
+from skimage.feature import plot_matches
+
+
+def plot(image0, image1, keypoints0, keypoints1, matches01):
+    fig = plt.figure()
+
+    ax = fig.add_subplot(211)
+    ax.axis("off")
+    ax.imshow(image0)
+
+    ax = fig.add_subplot(212)
+    ax.axis("off")
+    ax.imshow(image1)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    plot_matches(ax, image0, image1, keypoints0, keypoints1, matches01)
+
+
+class MatchOneToMany(object):
+    def __init__(self, base_image):
+        self.keypoints0, self.descriptors0 = extract_keypoints(base_image)
+        self.base_image = base_image
+
+    def compute(self, images):
+        n_images = images.shape[0]
+        n_keypoints = self.keypoints0.shape[0]
+
+        keypoints = np.full((n_images, n_keypoints, 2), np.nan)
+        for i, image in enumerate(images):
+            keypoints1, descriptors1 = extract_keypoints(image)
+            matches01 = match(self.descriptors0, descriptors1)
+            keypoints[i, matches01[:, 0]] = keypoints1[matches01[:, 1]]
+        return keypoints
