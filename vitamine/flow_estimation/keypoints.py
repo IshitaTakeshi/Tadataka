94e39514ee9e21d3589293af4a262caba64b6061
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-08-09 21:17:14 +0900

    Add an ORB extractor

--- vitamine/flow_estimation/keypoints.py
+++ vitamine/flow_estimation/keypoints.py
@@ -6,15 +6,25 @@ from skimage import transform
 
 from vitamine.coordinates import yx_to_xy
 
-extractor = BRIEF(mode="uniform")
 
 
-def extract_keypoints(image):
+
+def extract_brief(image):
     keypoints = corner_peaks(corner_harris(image), min_distance=5)
-    extractor.extract(image, keypoints)
-    keypoints = keypoints[extractor.mask]
-    descriptors = extractor.descriptors
+    brief.extract(image, keypoints)
+    keypoints = keypoints[brief.mask]
+    descriptors = brief.descriptors
     return yx_to_xy(keypoints), descriptors
+
+
+def extract_orb(image):
+    from skimage.feature import ORB
+    orb = ORB(n_keypoints=200, fast_threshold=0.12)
+    orb.detect_and_extract(image)
+    return yx_to_xy(orb.keypoints), orb.descriptors
+
+
+extract_keypoints = extract_brief
 
 
 def match(descriptors0, descriptors1):
