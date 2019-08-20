4009e83b3e4d16c3ff2cdb9f0e7693e46a3dfab0
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-08-20 14:24:41 +0900

    Remove 'straight_collidor' as it is not used

--- vitamine/dataset/points.py
+++ vitamine/dataset/points.py
@@ -9,45 +9,6 @@ def cubic_lattice(N):
     return np.vstack((xs.flatten(), ys.flatten(), zs.flatten())).T
 
 
-def straight_corridor(width, height, length):
-    # they are just 'length's so the number of points are +1
-    NX, NY, NZ = width + 1, height + 1, length + 1
-
-    pillar = np.arange(0, NY)
-    beam = np.arange(0, NX)
-
-    xs = np.concatenate((
-        beam,
-        beam,
-        beam[0] * np.ones(NY),
-        beam[-1] * np.ones(NY)
-    ))
-    xs = np.tile(xs, NZ)
-    xs = xs - width / 2.  # shift to make mean zero
-
-    ys = np.concatenate((
-        pillar[0] * np.ones(NX),
-        pillar[-1] * np.ones(NX),
-        pillar,
-        pillar
-    ))
-    ys = np.tile(ys, NZ)
-    ys = ys - height / 2. # shift to make mean zero
-
-    zs = np.repeat(np.arange(NZ), NX * 2 + NY * 2)
-
-    points = np.vstack((xs, ys, zs)).T
-
-    camera_locations = np.vstack((
-        np.zeros(length),
-        np.zeros(length),
-        np.arange(0, length) - 0.5
-    )).T
-
-    camera_rotations = rodrigues(np.zeros((length, 3)))
-    return
-
-
 def donut(inner_r, outer_r, height=5, point_density=24, n_viewpoints=60,
           offset=1e-3):
     assert(isinstance(height, int))
