d7c4f55b3676c0ac84dcc4f3b30bbf11c55e06ab
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-17 20:36:19 +0900

    Add a test for the entire visual odometry

--- tests/visual_odometry/test_visual_odometry.py
+++ tests/visual_odometry/test_visual_odometry.py
@@ -97,3 +97,54 @@ def test_init_points():
     P = transform_all(rotations, translations, vo.points)
     assert_array_almost_equal(projection.compute(P[0]), keypoints0[10:])
     assert_array_almost_equal(projection.compute(P[1]), keypoints1[10:])
+
+
+def test_visual_odometry():
+    # test the case all descriptors are same
+    vo = VisualOdometry(camera_parameters, FOV(0.0))
+    for i in range(3):
+        vo.try_add(observations[i], np.copy(descriptors))
+
+    rotations, translations = vo.poses
+    P = transform_all(rotations, translations, vo.points)
+    for i in range(3):
+        assert_array_almost_equal(projection.compute(P[i]), observations[i])
+
+    vo = VisualOdometry(camera_parameters, FOV(0.0))
+
+    descriptors0 = descriptors  # nothing modified
+    descriptors1 = add_noise(descriptors, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    descriptors2 = add_noise(descriptors, [0, 1, 2, 3, 4, 10, 11, 12, 13, 14])
+    descriptors3 = add_noise(descriptors, [10, 11, 12, 13, 14])
+
+    # descriptors0[10:27] and descriptors1[10:27] should be matched and
+    # triangulated
+    vo.try_add(observations[0], descriptors0)
+    vo.try_add(observations[1], descriptors1)
+    assert_array_equal(vo.get_untriangulated(0), np.arange(10))
+    # descriptors0[5:10] and descriptors2[5:10] should be matched and the
+    # corresponding keypoints should be triangulated
+    # descriptors0[15:27] and descriptors2[15:27] have the same
+    # descriptors but the corresponding keypoints are already
+    # triangulated so they are ignored
+    vo.try_add(observations[2], descriptors2)
+    # descriptors0[0:5] and descriptors3[0:5] should be matched
+    vo.try_add(observations[3], descriptors3)
+
+    rotations, translations = vo.poses
+    P = transform_all(rotations, translations, vo.points)
+    # points corresponding to descriptors0[10:27] and descriptors1[10:27]
+    assert_array_almost_equal(projection.compute(P[0, 0:17]),
+                              observations[0, 10:])
+    assert_array_almost_equal(projection.compute(P[1, 0:17]),
+                              observations[1, 10:])
+    # points corresponding to descriptors2[5:10] and descriptors0[5:10]
+    assert_array_almost_equal(projection.compute(P[0, 17:22]),
+                              observations[0, 5:10])
+    assert_array_almost_equal(projection.compute(P[2, 17:22]),
+                              observations[2, 5:10])
+    # points corresponding to descriptors3[0:5] and descriptors0[0:5]
+    assert_array_almost_equal(projection.compute(P[0, 22:27]),
+                              observations[0, 0:5])
+    assert_array_almost_equal(projection.compute(P[3, 22:27]),
+                              observations[3, 0:5])
