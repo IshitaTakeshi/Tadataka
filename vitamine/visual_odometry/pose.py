f5ebb171f73dc1deb4fd1be69b3e4da793df4b5d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-20 19:05:10 +0900

    Add a function 'estimate_pose'

--- vitamine/visual_odometry/pose.py
+++ vitamine/visual_odometry/pose.py
@@ -1,6 +1,7 @@
 from vitamine.pose_estimation import solve_pnp
 from vitamine.so3 import rodrigues
 
+
 class PoseManager(object):
     def __init__(self):
         self.rotations = []
@@ -20,3 +21,7 @@ class PoseManager(object):
         return motion_matrix(R, t)
 
 
+def estimate_pose(points, keypoints):
+    omega, t = solve_pnp(points, keypoints)
+    R = rodrigues(omega.reshape(1, -1))[0]
+    return R, t
