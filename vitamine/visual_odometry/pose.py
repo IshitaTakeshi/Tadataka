a11b5526f18b18387dd937a33d3bb2d2c00992d6
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-20 19:03:44 +0900

    Remove a class 'PoseEstimator'

--- vitamine/visual_odometry/pose.py
+++ vitamine/visual_odometry/pose.py
@@ -20,27 +20,3 @@ class PoseManager(object):
         return motion_matrix(R, t)
 
 
-class PoseEstimator(object):
-    def __init__(self, matcher, points0, descriptors0a, descriptors0b):
-        self.matcher = matcher
-        self.points0 = points0
-        self.descriptors0a = descriptors0a
-        self.descriptors0b = descriptors0b
-
-    def match_existing(self, keypoints1, descriptors1):
-        """
-        Match with descriptors that already have corresponding 3D points
-        """
-        matches01a = self.matcher(self.descriptors0a, descriptors1)
-        matches01b = self.matcher(self.descriptors0b, descriptors1)
-
-        if len(matches01a) > len(matches01b):
-            return matches01a[:, 0], matches01a[:, 1]
-        else:
-            return matches01b[:, 0], matches01b[:, 1]
-
-    def estimate(self, keypoints1, descriptors1):
-        indices0, indices1 = self.match_existing(keypoints1, descriptors1)
-        omega1, t1 = solve_pnp(self.points0[indices0], keypoints1[indices1])
-        R1 = rodrigues(omega1.reshape(1, -1))[0]
-        return R1, t1
