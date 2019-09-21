9d8a6b60b0439a131c27b512881c5363b6c59069
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-21 15:33:10 +0900

    Add a test for 'motion_matrix'

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,7 +1,8 @@
 from autograd import numpy as np
-from numpy.testing import assert_array_almost_equal, assert_equal
+from numpy.testing import (
+    assert_array_almost_equal, assert_array_equal, assert_equal)
 
-from vitamine.matrix import solve_linear
+from vitamine.matrix import solve_linear, motion_matrix
 
 
 def test_solve_linear():
@@ -14,3 +15,17 @@ def test_solve_linear():
     x = solve_linear(A)
     assert_equal(x.shape, (8,))
     assert_array_almost_equal(np.dot(A, x), np.zeros(3))
+
+
+def test_motion_matrix():
+    R = np.arange(9).reshape(3, 3)
+    t = np.arange(9, 12)
+    T = motion_matrix(R, t)
+    assert_array_equal(T,
+        np.array([
+            [0, 1, 2, 9],
+            [3, 4, 5, 10],
+            [6, 7, 8, 11],
+            [0, 0, 0, 1]
+        ])
+    )
