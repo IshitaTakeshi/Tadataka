6cc475b2bcdea12a78f8ac537f9946b5022c7ebc
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-21 15:45:32 +0900

    Add functions to manage 3D point indices corresponding to keypoints

--- vitamine/visual_odometry/keypoint.py
+++ vitamine/visual_odometry/keypoint.py
@@ -1,16 +1,50 @@
+from autograd import numpy as np
+
+
+class PointIndices(object):
+    def __init__(self, size):
+        self.point_indices = -np.ones(size, dtype=np.int64)
+
+    def subscribe(self, indices, point_indices):
+        self.point_indices[indices] = point_indices
+
+    @property
+    def is_triangulated(self):
+        return self.point_indices >= 0
+
+    def get(self):
+        return self.point_indices[self.is_triangulated]
+
+
 class KeypointManager(object):
     def __init__(self):
         self.keypoints = []
         self.descriptors = []
+        self.point_indices = []
 
     def add(self, keypoints, descriptors):
         self.keypoints.append(keypoints)
         self.descriptors.append(descriptors)
+        self.point_indices.append(PointIndices(len(keypoints)))
 
-    def get(self, i, indices):
+    def add_triangulated(self, i, indices, point_indices):
+        self.point_indices[i].subscribe(indices, point_indices)
+
+    def get(self, i, indices_or_mask):
         keypoints = self.keypoints[i]
         descriptors = self.descriptors[i]
-        return keypoints[indices], descriptors[indices]
+        return keypoints[indices_or_mask], descriptors[indices_or_mask]
+
+    def get_triangulated(self, i):
+        mask = self.point_indices[i].is_triangulated
+        return self.get(i, mask)
+
+    def get_untriangulated(self, i):
+        mask = self.point_indices[i].is_triangulated
+        return self.get(i, ~mask)
 
     def size(self, i):
         return len(self.keypoints[i])
+
+    def get_point_indices(self, i):
+        return self.point_indices[i].get()
