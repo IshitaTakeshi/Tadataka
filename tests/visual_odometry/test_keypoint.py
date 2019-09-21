39b5f734162d34e2a62586608f7e08bd23f07864
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-21 15:46:09 +0900

    Add a test for 'PointIndices'

--- tests/visual_odometry/test_keypoint.py
+++ tests/visual_odometry/test_keypoint.py
@@ -1,12 +1,21 @@
 from numpy.testing import assert_array_equal
 from autograd import numpy as np
 
-from vitamine.visual_odometry.keypoint import KeypointManager
+from vitamine.visual_odometry.keypoint import KeypointManager, PointIndices
 from tests.utils import random_binary
 
 
 keypoints = np.arange(16).reshape(8, 2)
 descriptors = random_binary((keypoints.shape[0], 128))
+
+
+def test_point_indices():
+    p = PointIndices(9)
+    p.subscribe([2, 3, 6, 8], [1, 5, 6, 3])
+    assert_array_equal(
+        p.is_triangulated,
+        np.array([0, 0, 1, 1, 0, 0, 1, 0, 1], dtype=np.bool)
+    )
 
 
 def test_keypoint_manager():
