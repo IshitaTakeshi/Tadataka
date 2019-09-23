9326a4e56ed5abc5ad9be17b4128b2c0d3743ecf
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-24 02:16:23 +0900

    Modify as the interfaces have changed

--- run_photo_vo.py
+++ run_photo_vo.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 
+from skimage.color import rgb2gray
+
 from vitamine.camera import CameraParameters
+from vitamine.camera_distortion import FOV
 from vitamine.dataset.tum_rgbd import TUMDataset
 from vitamine.observations import GrayImageObserver
 from vitamine.visual_odometry.visual_odometry import VisualOdometry
@@ -10,8 +13,16 @@ camera_parameters = CameraParameters(
     focal_length=[525.0, 525.0],
     offset=[319.5, 239.5]
 )
-dataset = TUMDataset(Path("datasets", "TUM", "rgbd_dataset_freiburg1_desk"))
+dataset = TUMDataset(Path("datasets", "TUM", "rgbd_dataset_freiburg1_xyz"))
 
-observer = GrayImageObserver(dataset)
-visual_odometry = VisualOdometry(observer, camera_parameters)
-visual_odometry.sequence()
+vo = VisualOdometry(camera_parameters, FOV(0.0))
+
+from matplotlib import pyplot as plt
+
+for i, frame in enumerate(dataset):
+    image = rgb2gray(frame.image)
+    plt.imshow(image, cmap="gray")
+    plt.show()
+    vo.add(image)
+    vo.try_remove()
+    print(f"frame index = {i}")
