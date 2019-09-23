f07a80cc5285fec65b0816f1fc6c7a8c37cb0418
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-24 02:18:09 +0900

    Simplify the pose class. Just hold parameters

--- vitamine/visual_odometry/pose.py
+++ vitamine/visual_odometry/pose.py
@@ -2,23 +2,9 @@ from vitamine.pose_estimation import solve_pnp
 from vitamine.so3 import rodrigues
 
 
-class PoseManager(object):
-    def __init__(self):
-        self.rotations = []
-        self.translations = []
-
-    def add(self, R, t):
-        self.rotations.append(R)
-        self.translations.append(t)
-
-    def get(self, i):
-        R = self.rotations[i]
-        t = self.translations[i]
-        return R, t
-
-    def get_motion_matrix(self, i):
-        R, t = self.get(i)
-        return motion_matrix(R, t)
+class Pose(object):
+    def __init__(self, R, t):
+        self.R, self.t = R, t
 
 
 def estimate_pose(points, keypoints):
