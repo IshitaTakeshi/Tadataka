34a0f76a1d3cdf2d38e6521886d212e2f42a576e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-24 02:22:38 +0900

    Move member functions to the outside of the VO class

--- vitamine/visual_odometry/visual_odometry.py
+++ vitamine/visual_odometry/visual_odometry.py
@@ -1,89 +1,75 @@
 from collections import deque
-from copy import copy
 
 from autograd import numpy as np
 
 from vitamine.keypoints import extract_keypoints, match
-from vitamine.triangulation import pose_point_from_keypoints, points_from_known_poses
 from vitamine.camera_distortion import CameraModel
-
-from vitamine.visual_odometry.pose import PoseManager, estimate_pose
+from vitamine.triangulation import pose_point_from_keypoints
+from vitamine.visual_odometry.pose import Pose, estimate_pose
 from vitamine.visual_odometry.point import Points
-from vitamine.visual_odometry.keyframe import Keyframes
-
-
-def find_best_match(matcher, keyframes, descriptors0, active_keyframe_ids):
-    max_matches = 0
-    argmax_matches01 = None
-    argmax_keyframe_id = None
-
-    for keyframe_id1 in active_keyframe_ids:
-        keypoints1, descriptors1 = keyframes.get_triangulated(keyframe_id1)
-        matches01 = matcher(descriptors0, descriptors1)
-        if len(matches01) > max_matches:
-            max_matches = len(matches01)
-            argmax_matches01 = matches01
-            argmax_keyframe_id = keyframe_id1
-    return argmax_matches01, argmax_keyframe_id
-
-
+from vitamine.visual_odometry.keypoint import Keypoints
 from vitamine.visual_odometry.triangulation import Triangulation
 
-        return points[valid_depth_mask], matches01[valid_depth_mask]
+
+def estimate_pose(point_indices0, keypoints1, matches01):
+    indices0, indices1 = matches01[:, 0], matches01[:, 1]
+    points_ = points.get(point_indices0[indices0])
+    return estimate_pose(points_, keypoints1[indices1])
 
 
-class Initializer(object):
-    def __init__(self, matcher, keypoints0, descriptors0):
-        self.matcher = matcher
-        self.keypoints0 = keypoints0
-        self.descriptors0 = descriptors0
+def find_best_match(matcher, active_descriptors, descriptors1):
+    matchesx1 = [matcher(d0, descriptors1) for d0 in active_descriptors]
+    argmax = np.argmax([len(m) for m in matchesx1])
+    return matchesx1[argmax], argmax
 
-    def initialize(self, keypoints1, descriptors1):
-        keypoints0, descriptors0 = self.keypoints0, self.descriptors0
-        matches01 = self.matcher(descriptors0, descriptors1)
 
-        R1, t1, points, valid_depth_mask = pose_point_from_keypoints(
-            keypoints0[matches01[:, 0]],
-            keypoints1[matches01[:, 1]]
+def init_points_(keypoints0, keypoints1, matches01):
+    R, t, points, valid_depth_mask = pose_point_from_keypoints(
+        keypoints0[matches01[:, 0]],
+        keypoints1[matches01[:, 1]]
+    )
+    return R, t, points[valid_depth_mask], matches01[valid_depth_mask]
+
+
+def init_points(matcher, keypoints0, keypoints1,
+                descriptors0, descriptors1):
+    matches01 = matcher(descriptors0, descriptors1)
+    return init_points_(keypoints0, keypoints1, matches01)
+
+
+def associate_points(keyframe0, keyframe1, matches01, point_indices):
+    keyframe0.associate_points(matches01[:, 0], point_indices)
+    keyframe1.associate_points(matches01[:, 1], point_indices)
+
+
+def triangulation(matcher, points, descriptors_, keyframe1):
+    triangulator = Triangulation(keyframe1.R, keyframe1.t,
+                                 keyframe1.keypoints,
+                                 keyframe1.descriptors)
+
+    for descriptors0 in descriptors_:
+        points_, matches01 = triangulator.triangulate(
+            keyframe0.R, keyframe0.t,
+            keypoints0, descriptors0
         )
+        if len(matches01) == 0:
+            continue
 
-        return R1, t1, matches01[valid_depth_mask], points[valid_depth_mask]
-
-
-def match_existing(matcher, keyframes, descriptors0, keyframe_ids, matches):
-    """
-    Match with descriptors that already have corresponding 3D points
-    """
-
-    # 3D points have corresponding two viewpoits used for triangulation
-    # To estimate the pose of the new frame, match keypoints in the new
-    # frame to keypoints in the two viewpoints
-    # Matched keypoints have corresponding 3D points.
-    # Therefore we can estimate the pose of the new frame using the matched keypoints
-    # and corresponding 3D points.
-    ka, kb = keyframe_ids
-    ma, mb = matches[:, 0], matches[:, 1]
-    # get descriptors already matched
-    _, descriptors1a = keyframes.get_keypoints(ka, ma)
-    _, descriptors1b = keyframes.get_keypoints(kb, mb)
-    matches01a = matcher(descriptors0, descriptors1a)
-    matches01b = matcher(descriptors0, descriptors1b)
-
-    if len(matches01a) > len(matches01b):
-        return matches01a[:, 0], matches01a[:, 1]
-    else:
-        return matches01b[:, 0], matches01b[:, 1]
+        point_indices = points.add(points_)
+        associate_points(keyframe0, keyframe1, matches01, point_indices)
 
 
 class VisualOdometry(object):
     def __init__(self, camera_parameters, distortion_model, matcher=match,
-                 min_keypoints=8, min_active_keyframes=8):
+                 min_keypoints=8, min_active_keyframes=8, min_inliers=8):
+        self.min_inliers = min_inliers
         self.matcher = match
         self.min_keypoints = min_keypoints
         self.min_active_keyframes = min_active_keyframes
         self.camera_model = CameraModel(camera_parameters, distortion_model)
         self.points = Points()
-        self.keyframes = Keyframes()
+        self.keypoints = []
+        self.poses = []
 
     def export_points(self):
         return self.points.get()
@@ -91,13 +77,57 @@ class VisualOdometry(object):
     def export_poses(self):
         return self.keyframes.get_poses()
 
-    @property
-    def reference_keyframe_id(self):
-        return self.keyframes.oldest_keyframe_id
+    def inlier_condition(self, matches):
+        return len(matches) >= self.min_inliers
 
     def add(self, image):
         keypoints, descriptors = extract_keypoints(image)
         return self.try_add(keypoints, descriptors)
+
+    def try_initialize_from_two(self, keypoints1, descriptors1):
+        keypoints0, descriptors0 = self.keypoints[0].get()
+        R1, t1, points, matches01 = init_points(
+            self.matcher,
+            keypoints0, keypoints1,
+            descriptors0, descriptors1
+        )
+        if not self.can_add_keyframe(R1, t1, points, matches01):
+            return False
+
+        kp0 = self.keypoints[0]
+        kp1 = Keypoints(keypoints1, descriptors1)
+        self.keypoints.append(kp1)
+        point_indices = self.points.add(points)
+        associate_points(kp0, kp1, matches01, point_indices)
+        return True
+
+    def try_continue(self, keypoints1, descriptors1):
+        active_keyframes = self.keyframes.get_active()
+
+        descriptors_ = [kf.triangulated()[1] for kf in active_keyframes]
+        matches01, index = find_best_match(matcher, descriptors_, descriptors1)
+        keyframe0 = active_keyframes[argmax]
+
+        R1, t1 = estimate_pose(keyframe0.get_point_indices(),
+                               keypoints, matches01, self.points)
+
+        if not self.can_add_keyframe(R1, t1, points, matches01):
+            return False
+
+        self.keyframes.add(keyframe1)
+        descriptors_ = [kf.untriangulated()[1] for kf in active_keyframes]
+        matches01, index = find_best_match(matcher, descriptors_, descriptors1)
+        triangulation(self.matcher, self.points, descriptors_, keyframe1)
+        return True
+
+    def init_first_keyframe(self, keypoints, descriptors):
+        R, t = np.identity(3), np.zeros(3)
+        self.keypoints.append(Keypoints(keypoints, descriptors))
+        self.poses.append(Pose(R, t))
+
+    @property
+    def n_active_keyframes(self):
+        return len(self.keypoints)
 
     def try_add(self, keypoints, descriptors):
         if len(keypoints) < self.min_keypoints:
@@ -105,85 +135,17 @@ class VisualOdometry(object):
 
         keypoints = self.camera_model.undistort(keypoints)
 
-        if self.keyframes.active_size == 0:
-            return self.init_keypoints(keypoints, descriptors)
+        if len(self.n_active_keyframes) == 0:
+            self.init_first_keyframe(keypoints, descriptors)
+            return True
 
-        if self.keyframes.active_size == 1:
-            return self.try_init_points(keypoints, descriptors)
+        if len(self.n_active_keyframes) == 1:
+            return self.try_initialize_from_two(keypoints, descriptors)
+        return self.try_continue(keypoints, descriptors)
 
-        return self.try_add_keyframe(keypoints, descriptors)
-
-    def try_init_points(self, keypoints1, descriptors1):
-        keyframe_id0 = 0
-        keypoints0, descriptors0 = self.keyframes.get_keypoints(keyframe_id0)
-
-        init = Initializer(self.matcher, keypoints0, descriptors0)
-        R1, t1, matches01, points = init.initialize(keypoints1, descriptors1)
-
-        # if not self.inlier_condition(matches):
-        #     return False
-        # if not pose_condition(R1, t1, points):
-        #     return False
-
-        keyframe_id1 = self.keyframes.add(keypoints1, descriptors1, R1, t1)
-        point_indices = self.points.add(points)
-        indices0, indices1 = matches01[:, 0], matches01[:, 1]
-        self.keyframes.add_triangulated(keyframe_id0, indices0, point_indices)
-        self.keyframes.add_triangulated(keyframe_id1, indices1, point_indices)
-        return True
-
-    def init_keypoints(self, keypoints, descriptors):
-        R, t = np.identity(3), np.zeros(3)
-        keyframe_id = self.keyframes.add(keypoints, descriptors, R, t)
-        return True
-
-    def estimate_pose(self, keypoints0, descriptors0, active_keyframe_ids,
-                      min_matches=4):
-        matches01, keyframe_id1 = find_best_match(
-            self.matcher, self.keyframes,
-            descriptors0, active_keyframe_ids
-        )
-
-        if len(matches01) < min_matches:
-            return False
-
-        point_indices = self.keyframes.get_point_indices(keyframe_id1)
-        keypoints = keypoints0[matches01[:, 0]]
-        points = self.points.get(point_indices[matches01[:, 1]])
-        return estimate_pose(points, keypoints)
-
-    def try_add_keyframe(self, keypoints0, descriptors0):
-        # if not pose_condition(R, t, points):
-        #     return False
-
-        active_keyframe_ids = copy(self.keyframes.active_keyframe_ids)
-        R0, t0 = self.estimate_pose(keypoints0, descriptors0,
-                                    active_keyframe_ids)
-        triangulator = Triangulation(self.matcher, R0, t0,
-                                     keypoints0, descriptors0)
-        keyframe_id0 = self.keyframes.add(keypoints0, descriptors0, R0, t0)
-
-        for keyframe_id1 in active_keyframe_ids:
-            keypoints1, descriptors1 = self.keyframes.get_untriangulated(
-                keyframe_id1
-            )
-            if len(keypoints1) == 0:
-                continue
-
-            R1, t1 = self.keyframes.get_pose(keyframe_id1)
-
-            points, matches01 = triangulator.triangulate(R1, t1,
-                                                         keypoints1, descriptors1)
-            if len(matches01) == 0:
-                continue
-
-            point_indices = self.points.add(points)
-            self.keyframes.add_triangulated(keyframe_id0, matches01[:, 0],
-                                            point_indices)
-            self.keyframes.add_triangulated(keyframe_id1, matches01[:, 1],
-                                            point_indices)
-
-        return True
+    def can_add_keyframe(self, R, t, points, matches):
+        return self.inlier_condition(matches)
+               # and self.pose_condition(R, t, points))
 
     def try_remove(self):
         if self.keyframes.active_size <= self.min_active_keyframes:
