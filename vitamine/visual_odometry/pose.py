41eaf9cec410607211269f7b641c206caf76b468
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-24 21:15:00 +0900

    Add a method to return identity pose. Implement '__eq__'

--- vitamine/visual_odometry/pose.py
+++ vitamine/visual_odometry/pose.py
@@ -1,3 +1,5 @@
+from autograd import numpy as np
+
 from vitamine.pose_estimation import solve_pnp
 from vitamine.so3 import rodrigues
 
@@ -5,6 +7,14 @@ from vitamine.so3 import rodrigues
 class Pose(object):
     def __init__(self, R, t):
         self.R, self.t = R, t
+
+    @staticmethod
+    def identity():
+        return Pose(np.identity(3), np.zeros(3))
+
+    def __eq__(self, other):
+        return (np.isclose(self.R, other.R).all() and
+                np.isclose(self.t, other.t).all())
 
 
 def estimate_pose(points, keypoints):
