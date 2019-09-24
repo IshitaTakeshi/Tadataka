af2d1074a6f1d41209b41bcfeca4020557aa67e6
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-24 21:16:15 +0900

    Raise 'NotEnoughtInliersException' if the number of inliers is not enough

--- vitamine/pose_estimation.py
+++ vitamine/pose_estimation.py
@@ -1,5 +1,7 @@
 from autograd import numpy as np
 
+from vitamine.exceptions import NotEnoughInliersException
+
 # TODO make this independent from cv2
 import cv2
 
@@ -11,7 +13,7 @@ def solve_pnp(points, keypoints):
     assert(points.shape[0] == keypoints.shape[0])
 
     if keypoints.shape[0] < min_correspondences:
-        raise ValueError("No sufficient correspondences")
+        raise NotEnoughInliersException("No sufficient correspondences")
 
     retval, omega, t = cv2.solvePnP(points.astype(np.float64),
                                     keypoints.astype(np.float64),
