049b8e5c1569c193976eee788a0c206e50897899
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-26 15:14:48 +0900

    Remove function 'estimate_pose'

--- vitamine/visual_odometry/pose.py
+++ vitamine/visual_odometry/pose.py
@@ -1,6 +1,5 @@
 from autograd import numpy as np
 
-from vitamine.pose_estimation import estimate_pose as estimate
 from vitamine.so3 import rodrigues
 
 
@@ -15,14 +14,3 @@ class Pose(object):
     def __eq__(self, other):
         return (np.isclose(self.R, other.R).all() and
                 np.isclose(self.t, other.t).all())
-
-
-def estimate_pose(points, point_indices0, keypoints1, matches01):
-    indices0, indices1 = matches01[:, 0], matches01[:, 1]
-    points_ = points.get(point_indices0[indices0])
-    try:
-        R1, t1 = estimate(points_, keypoints1[indices1])
-    except NotEnoughInliersException:
-        return None
-    return Pose(R1, t1)
-
