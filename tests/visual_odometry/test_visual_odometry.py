667bcff3bd888544d68a6e097d8be442578cf3d1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-09-26 14:14:45 +0900

    Add tests for addition of second and later than third keyframes

--- tests/visual_odometry/test_visual_odometry.py
+++ tests/visual_odometry/test_visual_odometry.py
@@ -89,5 +89,103 @@ def test_init_first():
     assert(vo.poses[0] == Pose.identity())
 
 
+
+def test_initialize_second():
+    def case1():
+        keypoints_true0 = keypoints_true[0]
+        keypoints_true1 = keypoints_true[1]
+
+        lf0 = LocalFeatures(keypoints_true0, descriptors)
+        lf1 = LocalFeatures(keypoints_true1, descriptors)
+
+        vo = VisualOdometry(camera_parameters, FOV(0.0))
+        assert(vo.try_add_keyframe(lf0))
+        assert(vo.try_add_keyframe(lf1))
+
+        (R0, t0), (R1, t1) = vo.export_poses()
+        points_pred = vo.export_points()
+
+        keypoints_pred0 = projection.compute(transform(R0, t0, points_pred))
+        keypoints_pred1 = projection.compute(transform(R1, t1, points_pred))
+
+        assert_array_almost_equal(keypoints_true0, keypoints_pred0)
+        assert_array_almost_equal(keypoints_true1, keypoints_pred1)
+
+        assert(vo.local_features[1] is lf1)
+        assert(len(vo.local_features) == 2)
+        assert(len(vo.poses) == 2)
+
+        lf0, lf1 = vo.local_features
+        assert_array_equal(lf0.point_indices, np.arange(len(points_true)))
+        assert_array_equal(lf1.point_indices, np.arange(len(points_true)))
+
+    def case2():
+        # descriptors[0:4] cannot match
+        descriptors0 = add_noise(descriptors, np.arange(0, 4))
+        # descriptors[4:7] cannot match
+        descriptors1 = add_noise(descriptors, np.arange(4, 7))
+        # descriptors[4:6] cannot match
+        descriptors2 = add_noise(descriptors, np.arange(4, 6))
+        lf0 = LocalFeatures(keypoints_true[0], descriptors0)
+        lf1 = LocalFeatures(keypoints_true[1], descriptors1)
+        lf2 = LocalFeatures(keypoints_true[2], descriptors2)
+
+        vo = VisualOdometry(camera_parameters, FOV(0.0), min_matches=8)
+        assert(vo.try_add_keyframe(lf0))
+        print("lf1")
+        # number of matches = 7
+        # not enough matches found between descriptors0 and descriptors1
+        assert(not vo.try_add_keyframe(lf1))
+        # number of matches = 8
+        # enough matches can be found
+        assert(vo.try_add_keyframe(lf2))
+
+        assert(vo.local_features[0] is lf0)
+        assert(vo.local_features[1] is lf2)
+
+        assert_array_equal(lf0.point_indices,
+                           [-1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7])
+        assert_array_equal(lf2.point_indices,
+                           [-1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7])
+
+        points_pred = vo.export_points()
+    case1()
+    case2()
+
+
+def test_try_add_more():
+    return
+    # TODO test with keypoints of different lengths
+    vo = VisualOdometry(camera_parameters, FOV(0.0), min_matches=8)
+
+    descriptors0 = add_noise(descriptors, [0, 1])
+    descriptors1 = add_noise(descriptors, [5])
+    descriptors2 = add_noise(descriptors, [3])
+    lf0 = LocalFeatures(keypoints_true[0], descriptors0)
+    lf1 = LocalFeatures(keypoints_true[1], descriptors1)
+    lf2 = LocalFeatures(keypoints_true[2], descriptors2)
+
+    vo.try_add_keyframe(lf0)
+    assert(vo.n_active_keyframes == 1)
+    assert(vo.try_add_keyframe(lf1))
+    assert(vo.n_active_keyframes == 2)
+
+    assert_array_equal(lf0.point_indices,
+                       [-1, -1, 0, 1, 2, -1, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert_array_equal(lf1.point_indices,
+                       [-1, -1, 0, 1, 2, -1, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    assert(vo.try_add_keyframe(lf2))
+    assert(vo.n_active_keyframes == 3)
+
+    lf0, lf1, lf2 = vo.local_features
+    assert_array_equal(lf0.point_indices,
+                       [11, 12, 0, 1, 2, 13, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert_array_equal(lf1.point_indices,
+                       [11, 12, 0, 1, 2, 13, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert_array_equal(lf2.point_indices,
+                       [11, 12, 0, -1, 2, 13, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
 def test_triangulation():
     pass
