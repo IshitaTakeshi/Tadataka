d1abdf609af663d3b358e9cd341c9494be3f7702
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-02 18:08:08 +0900

    Simplify tests of 'solve_pnp'

--- tests/test_pose_estimation.py
+++ tests/test_pose_estimation.py
@@ -3,14 +3,14 @@ import pytest
 from autograd import numpy as np
 from numpy.testing import assert_array_almost_equal
 
-from vitamine.dataset.observations import generate_translations
-from vitamine.so3 import rodrigues
+from vitamine.so3 import rodrigues, exp_so3
 
 from vitamine.exceptions import NotEnoughInliersException
 from vitamine.camera import CameraParameters
 from vitamine.projection import PerspectiveProjection
-from vitamine.dataset.observations import generate_observations
+from vitamine.dataset.observations import generate_translations
 from vitamine.pose_estimation import solve_pnp
+from vitamine.rigid_transform import transform
 
 
 camera_parameters = CameraParameters(focal_length=[1, 1], offset=[0, 0])
@@ -26,38 +26,38 @@ points = np.array([
    [-4, 4, -1]
 ])
 
-omegas = np.pi * np.array([
-    [0.0, 0.0, 0.0],
+omegas = np.array([
+    [0.1, 0.0, 0.0],
     [0.0, 1.0, 0.0],
-    [5.2, 0.0, 0.1],
-    [3.2, 4.1, 1.0],
+    [-1.2, 0.0, 0.1],
+    [3.2, 1.1, 1.0],
     [3.1, -0.2, 0.0],
     [-1.0, 0.2, 3.1]
 ])
 
-# translations = generate_translations(rodrigues(omegas), points)
-translations = np.array([
-    [ 3.0, 2.0, 7.0],
-    [-1.0, -1.0, 6.0],
-    [-1.5, 2.0, 5.0],
-    [-2.0, -0.5, 8.0],
-    [9.0, 2.1, 3.0],
-    [4.0, 2.5, -3.0]
-])
-
-
-keypoints, positive_depth_mask = generate_observations(
-    rodrigues(omegas), translations, points, projection
-)
+translations = generate_translations(rodrigues(omegas), points)
 
 
 def test_solve_pnp():
-    for keypoints_, omega_true, t_true in zip(keypoints, omegas, translations):
-        omega_pred, t_pred = solve_pnp(points, keypoints_, omega_true, t_true)
-        assert_array_almost_equal(omega_true, omega_pred)
-        assert_array_almost_equal(t_true, t_pred)
+    for omega_true, t_true in zip(omegas, translations):
+        P = transform(exp_so3(omega_true), t_true, points)
+        keypoints_true = projection.compute(P)
 
-    keypoints0 = keypoints[0]
+        omega_pred, t_pred = solve_pnp(points, keypoints_true)
+
+        P = transform(exp_so3(omega_pred), t_pred, points)
+        keypoints_pred = projection.compute(P)
+
+        # omega_true and omega_pred can be different but
+        # they have to be linearly dependent and satisfy
+        # norm(omega_true) - norm(omega_pred) = 2 * n * pi
+
+        assert_array_almost_equal(t_true, t_pred)
+        assert_array_almost_equal(keypoints_true, keypoints_pred)
+
+    P = transform(exp_so3(omegas[0]), translations[0], points)
+    keypoints0 = projection.compute(P)
+
     # 6 correspondences
     # this should be able to run
     solve_pnp(points[0:6], keypoints0[0:6])
