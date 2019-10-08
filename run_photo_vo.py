641e04e2efd5e027b7e2fcb8e59a01929b8d6b3c
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-08 21:27:15 +0900

    Make the VO runner independent from 'LocalFeatures'

--- run_photo_vo.py
+++ run_photo_vo.py
@@ -7,8 +7,9 @@ from vitamine.keypoints import extract_keypoints
 from matplotlib import pyplot as plt
 from vitamine.coordinates import xy_to_yx
 from vitamine.dataset.tum_rgbd import TUMDataset
+from vitamine.keypoints import KeypointDescriptor as KD
 from vitamine.visual_odometry.visual_odometry import VisualOdometry
-from vitamine.visual_odometry.keypoint import LocalFeatures
+from vitamine.visual_odometry.keypoint import is_triangulated
 from vitamine.camera import CameraParameters
 from vitamine.camera_distortion import FOV
 from vitamine.coordinates import camera_to_world
@@ -27,7 +28,7 @@ vo = VisualOdometry(
     CameraParameters(focal_length=[3104.3, 3113.34],
                      offset=[1640, 1232]),
     FOV(-0.01),
-    min_active_keyframes=3
+    min_active_keyframes=2
 )
 
 
@@ -56,28 +57,29 @@ def test_match_point_indices():
 
 
 def add_keyframe(image):
-    print(image.shape)
     keypoints, descriptors = extract_keypoints(image)
     keypoints_ = vo.camera_model.undistort(keypoints)
-    lf = LocalFeatures(keypoints_, descriptors)
-    vo.try_add_keyframe(lf)
-    # keypoints has the same distortion as image
-    return lf, keypoints
+    assert(vo.try_add_keyframe(KD(keypoints_, descriptors)))
+    point_indices = vo.point_indices_list[-1]
+    return keypoints, point_indices
 
 
-def plot_matches_(image1, image2, lf1, lf2, keypoints1, keypoints2):
+def plot_matches_(image1, image2, keypoints1, keypoints2,
+                  point_indices1, point_indices2):
+    mask1 = is_triangulated(point_indices1)
+    mask2 = is_triangulated(point_indices2)
+
     matches12 = match_point_indices(
-        lf1.point_indices[lf1.is_triangulated],
-        lf2.point_indices[lf2.is_triangulated]
+        point_indices1[mask1],
+        point_indices2[mask2]
     )
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     plot_matches(ax, image1, image2,
-                 xy_to_yx(keypoints1[lf1.is_triangulated]),
-                 xy_to_yx(keypoints2[lf2.is_triangulated]),
-                 matches12[[100, 220, 257, 270]],
-                 matches_color='yellow')
+                 xy_to_yx(keypoints1[mask1]),
+                 xy_to_yx(keypoints2[mask2]),
+                 matches12)
     plt.show()
 
 
@@ -85,7 +87,8 @@ def plot_keypoints(image, keypoints):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.imshow(image, cmap="gray")
-    ax.scatter(keypoints[:, 0], keypoints[:, 1], facecolors='none', edgecolors='r')
+    ax.scatter(keypoints[:, 0], keypoints[:, 1],
+               facecolors='none', edgecolors='r')
     plt.show()
 
 
@@ -99,23 +102,29 @@ def plot_map_(poses, points):
 filenames = sorted(Path("./datasets/ball/").glob("*.jpg"))
 images = [rgb2gray(imread(filename)) for filename in filenames[:4]]
 
-lf0, keypoints0 = add_keyframe(images[0])
-lf1, keypoints1 = add_keyframe(images[1])
+keypoints0, point_indices0 = add_keyframe(images[0])
+keypoints1, point_indices1 = add_keyframe(images[1])
 
 plot_map_(vo.export_poses(), vo.export_points())
 
-print(f"len(keypoints0) = {len(keypoints0)}")
-print(f"len(keypoints1) = {len(keypoints1)}")
-# plot_keypoints(images[0], keypoints0)
-plot_matches_(images[0], images[1], lf0, lf1, keypoints0, keypoints1)
+print(f"len(point_indices0) = {len(point_indices0)}")
+print(f"len(point_indices1) = {len(point_indices1)}")
+# plot_point_indices(images[0], point_indices0)
+plot_matches_(images[0], images[1], keypoints0, keypoints1,
+              point_indices0, point_indices1)
 
-lf2, keypoints2 = add_keyframe(images[2])
-print(f"len(keypoints2) = {len(keypoints2)}")
-plot_matches_(images[0], images[2], lf0, lf2, keypoints0, keypoints2)
-plot_matches_(images[1], images[2], lf1, lf2, keypoints1, keypoints2)
+keypoints2, point_indices2 = add_keyframe(images[2])
+print(f"len(point_indices2) = {len(point_indices2)}")
+plot_matches_(images[0], images[2], keypoints0, keypoints2,
+              point_indices0, point_indices2)
+plot_matches_(images[1], images[2], keypoints1, keypoints2,
+              point_indices1, point_indices2)
 
-lf3, keypoints3 = add_keyframe(images[3])
-print(f"len(keypoints3) = {len(keypoints3)}")
-plot_matches_(images[0], images[3], lf0, lf3, keypoints0, keypoints3)
-plot_matches_(images[1], images[3], lf1, lf3, keypoints1, keypoints3)
-plot_matches_(images[2], images[3], lf2, lf3, keypoints2, keypoints3)
+keypoints3, point_indices3 = add_keyframe(images[3])
+print(f"len(point_indices3) = {len(point_indices3)}")
+plot_matches_(images[0], images[3], keypoints0, keypoints3,
+              point_indices0, point_indices3)
+plot_matches_(images[1], images[3], keypoints1, keypoints3,
+              point_indices1, point_indices3)
+plot_matches_(images[2], images[3], keypoints2, keypoints3,
+              point_indices2, point_indices3)
