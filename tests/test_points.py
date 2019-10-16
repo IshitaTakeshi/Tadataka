ce7aabd440e65065a0ca78799d3bfb8c2dba8e84
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-16 15:33:34 +0900

    Remove 'Point' class and add point management functions instead

--- tests/test_points.py
+++ tests/test_points.py
@@ -1,20 +1,29 @@
 from numpy.testing import assert_array_equal
 from autograd import numpy as np
 
-from vitamine.points import Points
+from vitamine.points import get_point_indices, init_empty_points, concat_points
 
 
-def test_points():
-    points = Points()
+def test_init_empty_points():
+    points = init_empty_points()
+    assert(points.shape == (0, 3))
 
-    points_ = np.arange(30).reshape(10, 3)
-    point_indices = points.add(points_)
+
+def test_get_point_indices():
+    points = init_empty_points()
+    new_points = np.arange(30).reshape(10, 3)
+    point_indices = get_point_indices(points, new_points)
     assert_array_equal(point_indices, np.arange(0, 10))
 
-    points_ = np.arange(30, 60).reshape(10, 3)
-    point_indices = points.add(points_)
+    points = np.arange(30, 60).reshape(10, 3)
+    new_points = np.arange(30, 60).reshape(10, 3)
+    point_indices = get_point_indices(points, new_points)
     assert_array_equal(point_indices, np.arange(10, 20))
 
-    assert_array_equal(points.get(np.arange(5, 15)),
-                       np.arange(15, 45).reshape(10, 3))
 
+def test_concat_points():
+    points = np.arange(0, 60).reshape(20, 3)
+    new_points = np.arange(60, 90).reshape(10, 3)
+    points, point_indices = concat_points(points, new_points)
+    assert_array_equal(points, np.arange(90).reshape(30, 3))
+    assert_array_equal(point_indices, np.arange(20, 30))
--- vitamine/points.py
+++ vitamine/points.py
@@ -1,20 +1,17 @@
 from autograd import numpy as np
 
 
-class Points(object):
-    def __init__(self):
-        self.points = np.empty((0, 3), dtype=np.float64)
+def init_empty_points():
+    return np.empty((0, 3), dtype=np.float64)
 
-    def __len__(self):
-        return self.points.shape[0]
 
-    def add(self, points):
-        start = len(self.points)
-        self.points = np.vstack((self.points, points))
-        end = len(self.points)
-        return np.arange(start, end)  # return indices of points
+def get_point_indices(existing_points, new_points):
+    start = len(existing_points)
+    diff = len(new_points)
+    return np.arange(start, start + diff)
 
-    def get(self, indices=None):
-        if indices is None:
-            return self.points
-        return self.points[indices]
+
+def concat_points(points, new_points):
+    point_indices = get_point_indices(points, new_points)
+    points = np.vstack((points, new_points))
+    return points, point_indices
