7e459697e1d03731ab54d2827fd2731f1b149fd3
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-17 20:57:49 +0900

    Manage points and their indices in 'PointManager' class Improve readability by hiding point indices in it

--- tests/test_points.py
+++ tests/test_points.py
@@ -1,29 +1,123 @@
-from numpy.testing import assert_array_equal
+import pytest
+
+from numpy.testing import assert_array_equal, assert_array_almost_equal
 from autograd import numpy as np
 
-from vitamine.points import get_point_indices, init_empty_points, concat_points
+from vitamine.pose import Pose, solve_pnp
+from vitamine.camera import CameraParameters
+from vitamine.projection import PerspectiveProjection
+from vitamine.dataset.observations import generate_translations
+from vitamine.so3 import rodrigues
+from vitamine.points import PointManager
+from vitamine.rigid_transform import transform
+from tests.data import dummy_points as points_true
 
 
-def test_init_empty_points():
-    points = init_empty_points()
-    assert(points.shape == (0, 3))
+omegas = np.array([
+    [0, 0, 0],
+    [0, np.pi / 2, 0],
+    [np.pi / 2, 0, 0],
+    [0, np.pi / 4, 0],
+    [0, -np.pi / 4, 0],
+    [-np.pi / 4, np.pi / 4, 0],
+    [0, np.pi / 8, -np.pi / 4]
+])
+
+rotations = rodrigues(omegas)
+translations = generate_translations(rotations, points_true)
+
+camera_parameters = CameraParameters(focal_length=[1, 1], offset=[0, 0])
+projection = PerspectiveProjection(camera_parameters)
 
 
-def test_get_point_indices():
-    points = init_empty_points()
-    new_points = np.arange(30).reshape(10, 3)
-    point_indices = get_point_indices(points, new_points)
-    assert_array_equal(point_indices, np.arange(0, 10))
-
-    points = np.arange(30, 60).reshape(10, 3)
-    new_points = np.arange(30, 60).reshape(10, 3)
-    point_indices = get_point_indices(points, new_points)
-    assert_array_equal(point_indices, np.arange(10, 20))
+def assert_projection_equal(R, t, points, keypoints_pred):
+    assert_array_almost_equal(
+        projection.compute(transform(R, t, points)),
+        keypoints_pred
+    )
 
 
-def test_concat_points():
-    points = np.arange(0, 60).reshape(20, 3)
-    new_points = np.arange(60, 90).reshape(10, 3)
-    points, point_indices = concat_points(points, new_points)
-    assert_array_equal(points, np.arange(90).reshape(30, 3))
-    assert_array_equal(point_indices, np.arange(20, 30))
+def test_point_manager():
+    point_manager = PointManager()
+
+    # initalization from 0th and 1st view
+    P0 = transform(rotations[0], translations[0], points_true)
+    P1 = transform(rotations[1], translations[1], points_true)
+    P2 = transform(rotations[2], translations[2], points_true)
+    keypoints0 = projection.compute(P0)
+    keypoints1 = projection.compute(P1)
+    keypoints2 = projection.compute(P2)
+
+    matches01 = np.array([[0, 1, 2, 3, 4, 6, 8, 9],
+                          [0, 1, 2, 3, 4, 6, 8, 9]]).T
+
+    pose0, pose1 = point_manager.initialize(keypoints0, keypoints1, matches01,
+                                            viewpoint0=0, viewpoint1=1)
+
+    assert_array_almost_equal(pose0.omega, np.zeros(3))
+    assert_array_almost_equal(pose1.omega, omegas[1])
+
+    P0, mask0 = point_manager.get(0, matches01[:, 0])
+    P1, mask1 = point_manager.get(1, matches01[:, 1])
+    assert(np.all(mask0))
+    assert(np.all(mask1))
+    assert_projection_equal(pose0.R, pose0.t, P0, keypoints0[matches01[:, 0]])
+    assert_projection_equal(pose1.R, pose1.t, P1, keypoints1[matches01[:, 1]])
+    assert_array_equal(P0, P1)
+
+    # add the 2nd view
+
+    # triangulate with the 0th view
+
+    # [0, 1, 2, 3, 8, 9] are already triangulated
+    # [5, 7] are not triangulated yet
+    matches02 = np.array([[0, 1, 2, 3, 5, 7, 8, 9],
+                          [0, 1, 2, 3, 5, 7, 8, 9]]).T
+    P0, mask0 = point_manager.get(0, matches02[:, 0])
+    assert_array_equal(mask0, [1, 1, 1, 1, 0, 0, 1, 1])
+
+    # esitmate the pose by pnp to align the scale
+    pose2 = solve_pnp(P0, keypoints2[matches02[mask0, 1]])
+
+    point_manager.triangulate(pose0, pose2, keypoints0, keypoints2, matches02,
+                              viewpoint0=0, viewpoint1=2)
+    P0, mask0 = point_manager.get(0, matches02[:, 0])
+    P2, mask2 = point_manager.get(2, matches02[:, 1])
+
+    # all masks shuld be true because all matched points are triangulated
+    assert(np.all(mask0))
+    assert(np.all(mask2))
+
+    assert_projection_equal(pose0.R, pose0.t, P0, keypoints0[matches02[:, 0]])
+    assert_projection_equal(pose2.R, pose2.t, P2, keypoints2[matches02[:, 1]])
+
+    # triangulate with the 1st view
+
+    # [0, 1, 2, 3, 8, 9] are already triangulated in viewpoint 0, 1, 2
+    # [4, 6] are triangulated in viewpoint 0, 1
+    # [5, 7] are triangulated in viewpoint 0, 2
+    # [10, 11, 12] are not triangulated yet in any viewpoints
+    matches12 = np.array([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]).T
+    P1, mask1 = point_manager.get(1, matches12[:, 0])
+    # keypoint index           0  1  2  3  4  5  6  7  8  9 10 11 12
+    assert_array_equal(mask1, [1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0])
+
+    # esitmate the pose using matches to the 1st view
+    pose2 = solve_pnp(P1, keypoints2[matches12[mask1, 1]])
+
+    point_manager.triangulate(pose1, pose2, keypoints1, keypoints2, matches12,
+                              viewpoint0=1, viewpoint1=2)
+    P1, mask1 = point_manager.get(1, matches12[:, 0])
+    P2, mask2 = point_manager.get(2, matches12[:, 1])
+
+    assert_projection_equal(pose1.R, pose1.t, P1, keypoints1[matches12[:, 0]])
+    assert_projection_equal(pose2.R, pose2.t, P2, keypoints2[matches12[:, 1]])
+
+    # wrong match
+    matches12 = np.array([[0, 1]])
+    with pytest.warns(UserWarning):
+        point_manager.triangulate(
+            pose1, pose2, keypoints1, keypoints2, matches12,
+            viewpoint0=1, viewpoint1=2
+        )
--- vitamine/points.py
+++ vitamine/points.py
@@ -1,17 +1,192 @@
+from collections import defaultdict
+
 from autograd import numpy as np
 
+from vitamine.exceptions import InvalidDepthException
+from vitamine.triangulation import linear_triangulation, pose_point_from_keypoints
 
-def init_empty_points():
+
+def init_empty():
     return np.empty((0, 3), dtype=np.float64)
 
 
-def get_point_indices(existing_points, new_points):
-    start = len(existing_points)
-    diff = len(new_points)
-    return np.arange(start, start + diff)
+def warn_if_incorrect_match(point_index0, point_index1):
+    if point_index0 != point_index1:
+        warnings.warn(
+            f"Wrong match! "
+            f"Keypoint index {index0} in {viewpoint0} and "
+            f"keypoint index {index1} in {viewpoint1} have matched, "
+            f"but indicate different 3D points!"
+        )
 
 
-def concat_points(points, new_points):
-    point_indices = get_point_indices(points, new_points)
-    points = np.vstack((points, new_points))
-    return points, point_indices
+class Triangulation(object):
+    def __init__(self, pose0, pose1, keypoints0, keypoints1):
+        self.pose0, self.pose1 = pose0, pose1
+        self.keypoints0, self.keypoints1 = keypoints0, keypoints1
+
+    def triangulate(self, index0, index1):
+        keypoint0 = self.keypoints0[index0]
+        keypoint1 = self.keypoints1[index1]
+
+        try:
+            return linear_triangulation(self.pose0, self.pose1,
+                                        keypoint0, keypoint1)
+        except InvalidDepthException:
+            print_error(e)
+            return None
+
+
+class PointManager(object):
+    def __init__(self):
+        # {keyframe index: {keypoint index: point index}}
+        self.point_indices = defaultdict(dict)
+        self.points = init_empty()
+
+    def add_point_(self, point):
+        assert(point.shape == (3,))  # add only one point at a time
+        point_index = len(self.points)
+        self.points = np.vstack((self.points, point))
+        return point_index
+
+    def add_point(self, point, viewpoint0, viewpoint1,
+                  keypoint_index0, keypoint_index1):
+        point_index = self.add_point_(point)
+        self.point_indices[viewpoint0][keypoint_index0] = point_index
+        self.point_indices[viewpoint1][keypoint_index1] = point_index
+
+    def associate_existing(self, src_viewpoint, dst_viewpoint,
+                           src_keypoint_index, dst_keypoint_index):
+        point_index = self.point_indices[src_viewpoint][src_keypoint_index]
+        self.point_indices[dst_viewpoint][dst_keypoint_index] = point_index
+
+    def get(self, viewpoint, keypoint_indices):
+        if viewpoint not in self.point_indices.keys():
+            raise KeyError(f"viewpoint {viewpoint}")
+
+        d = self.point_indices[viewpoint]
+
+        point_indices = []
+        mask = []
+        for i in keypoint_indices:
+            try:
+                point_index = d[i]
+            except KeyError as e:
+                mask.append(False)
+                continue
+            point_indices.append(point_index)
+            mask.append(True)
+
+        return self.points[point_indices], mask
+
+    def initialize(self, keypoints0, keypoints1, matches01,
+                   viewpoint0, viewpoint1):
+        # no viewpoints added so far
+        assert(len(self.point_indices.keys()) == 0)
+
+        pose0, pose1, points, valid_matches01 = pose_point_from_keypoints(
+            keypoints0, keypoints1, matches01
+        )
+
+        for point, (index0, index1) in zip(points, valid_matches01):
+            point_index = self.add_point(point, viewpoint0, viewpoint1,
+                                         index0, index1)
+
+        return pose0, pose1
+
+    def both_observed(self, triangulation,
+                      viewpoint0, viewpoint1, indices0, indices1):
+        # Triangulate in the assumption that
+        # both viewpoints have already subscribed
+        # There's a constraint that one 3D point have
+        # only one corresponding keypoint in a frame
+
+        keypoint_indices0 = self.point_indices[viewpoint0].keys()
+        keypoint_indices1 = self.point_indices[viewpoint1].keys()
+
+        for index0, index1 in zip(indices0, indices1):
+            is_trinagulated0 = index0 in keypoint_indices0
+            is_trinagulated1 = index1 in keypoint_indices1
+
+            if is_trinagulated0 and is_trinagulated1:
+                # Both matched keypoints have already triangulated.
+                # In this case, index0 and index1 should indicate
+                # the same 3D point otherwise it is a wrong match
+                warn_if_incorrect_match(
+                    self.point_indices[viewpoint0][index0],
+                    self.point_indices[viewpoint1][index1]
+                )
+                continue
+
+            if is_trinagulated0:  # is_trinagulated1 == False
+                # keypoint corresponding to index0 is already triangulated
+                # index1 is indicating a keypoint in viewpoint1
+                # that is extracted but have not been matched
+                self.associate_existing(viewpoint0, viewpoint1, index0, index1)
+                continue
+
+            if is_trinagulated1:  # is_trinagulated0 == False
+                # keypoint corresponding to index1 is already triangulated
+                # index0 is indicating a keypoint in viewpoint1
+                # that is extracted but have not been matched
+                self.associate_existing(viewpoint1, viewpoint0, index1, index0)
+                continue
+
+            # neither index0 nor index1 has been triangulated
+            point = triangulation.triangulate(index0, index1)
+            if point is None:
+                continue
+
+            self.add_point(point, viewpoint0, viewpoint1, index0, index1)
+
+    def either_one_observed(self, triangulation,
+                            src_viewpoint, dst_viewpoint, src_indices, dst_indices):
+        # assume src_viewpoint have already been observed
+
+        src_keypoint_indices = self.point_indices[src_viewpoint].keys()
+        for src_index, dst_index in zip(src_indices, dst_indices):
+            if src_index in src_keypoint_indices:
+                # keypoint corresponding to src_index has
+                # already been triangulated
+                self.associate_existing(src_viewpoint, dst_viewpoint,
+                                        src_index, dst_index)
+                continue
+
+            # both keypoints corresponding to src_index and dst_index
+            # have not been observed
+            # create it by triangulation
+
+            point = triangulation.triangulate(src_index, dst_index)
+            if point is None:
+                continue
+
+            self.add_point(point, src_viewpoint, dst_viewpoint,
+                           src_index, dst_index)
+
+    def triangulate(self, pose0, pose1,
+                    keypoints0, keypoints1, matches01,
+                    viewpoint0, viewpoint1):
+        triangulation = Triangulation(pose0, pose1, keypoints0, keypoints1)
+
+        viewpoints = self.point_indices.keys()
+        has_observed0 = viewpoint0 in viewpoints
+        has_observed1 = viewpoint1 in viewpoints
+
+        indices0, indices1 = matches01[:, 0], matches01[:, 1]
+
+        if has_observed0 and has_observed1:
+            self.both_observed(triangulation, viewpoint0, viewpoint1,
+                               indices0, indices1)
+            return
+
+        if has_observed0:
+            self.either_one_observed(triangulation, viewpoint0, viewpoint1,
+                                     indices0, indices1)
+            return
+
+        if has_observed1:
+            self.either_one_observed(triangulation, viewpoint1, viewpoint0,
+                                     indices1, indices0)
+            return
+
+        raise ValueError("Neither viewpoint0 nor viewpoint1 has been observed")
