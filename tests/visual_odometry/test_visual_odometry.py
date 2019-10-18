1708e4ddd249217325396026531ff82ee5e717aa
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-18 17:05:20 +0900

    Modify tests as VO has been simplified

--- tests/visual_odometry/test_visual_odometry.py
+++ tests/visual_odometry/test_visual_odometry.py
@@ -16,10 +16,10 @@ from vitamine.keypoints import Matcher
 from vitamine.exceptions import NotEnoughInliersException
 from vitamine.visual_odometry.visual_odometry import VisualOdometry
 from vitamine.pose import Pose
-from vitamine.point_index import PointIndices
 from vitamine.rigid_transform import transform_all
 from vitamine.utils import random_binary, break_other_than
 from tests.data import dummy_points as points_true
+from tests.assertion import assert_projection_equal
 
 
 matcher = Matcher(enable_ransac=False, enable_homography_filter=False)
@@ -47,14 +47,6 @@ keypoints_true, positive_depth_mask = generate_observations(
 descriptors = random_binary((len(points_true), 1024))
 
 
-def test_init_first():
-    kd0 = KD(keypoints_true[0], descriptors)
-    vo = VisualOdometry(camera_parameters, FOV(0.0), matcher=matcher)
-    vo.init_first(kd0, PointIndices(len(keypoints_true[0])))
-    assert(vo.keypoint_descriptor_list[0] is kd0)
-    assert(vo.poses[0] == Pose.identity())
-
-
 def test_try_init_second():
     def case1():
         keypoints_true0 = keypoints_true[0]
@@ -78,13 +70,9 @@ def test_try_init_second():
         assert_array_almost_equal(keypoints_true0, keypoints_pred0)
         assert_array_almost_equal(keypoints_true1, keypoints_pred1)
 
-        assert(vo.keypoint_descriptor_list[1] is kd1)
-        assert(len(vo.keypoint_descriptor_list) == 2)
+        assert(vo.kds[1] is kd1)
+        assert(len(vo.kds) == 2)
         assert(len(vo.poses) == 2)
-
-        indices0, indices1 = vo.point_indices_list
-        assert_array_equal(indices0.triangulated, np.arange(len(points_true)))
-        assert_array_equal(indices1.triangulated, np.arange(len(points_true)))
 
     def case2():
         # descriptors[0:4] cannot match
@@ -102,7 +90,6 @@ def test_try_init_second():
             descriptors,
             [0, 1, 2, 3, 6, 7, 8, 9, 10, 11, 12, 13]
         )
-        # point_indices   0  1  2  3   4   5   6   7
         # matches02.T = [[6, 7, 8, 9, 10, 11, 12, 13],
         #                [6, 7, 8, 9, 10, 11, 12, 13]]
         kd0 = KD(keypoints_true[0], descriptors0)
@@ -119,18 +106,8 @@ def test_try_init_second():
         # enough matches can be found
         assert(vo.try_add_keyframe(kd2))
 
-        assert(vo.keypoint_descriptor_list[0] is kd0)
-        assert(vo.keypoint_descriptor_list[1] is kd2)
-
-        point_indices0, point_indices2 = vo.point_indices_list
-        assert_array_equal(point_indices0.is_triangulated,
-                           [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
-        assert_array_equal(point_indices0.triangulated,
-                           [0, 1, 2, 3, 4, 5, 6, 7])
-        assert_array_equal(point_indices2.is_triangulated,
-                           [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1])
-        assert_array_equal(point_indices2.triangulated,
-                           [0, 1, 2, 3, 4, 5, 6, 7])
+        assert(vo.kds[0] is kd0)
+        assert(vo.kds[1] is kd2)
 
         points_pred = vo.export_points()
         pose0, pose2 = vo.poses
@@ -141,14 +118,9 @@ def test_try_init_second():
 
         assert_array_almost_equal(keypoints_true[0, 6:14], keypoints_pred0)
         assert_array_almost_equal(keypoints_true[2, 6:14], keypoints_pred2)
+
     case1()
     case2()
-
-
-def assert_projection_equal(points, point_indices, pose, keypoints):
-    P = transform(pose.R, pose.t, points.get(point_indices.triangulated))
-    assert_array_almost_equal(projection.compute(P),
-                              keypoints[point_indices.is_triangulated])
 
 
 def test_try_add_more():
@@ -162,8 +134,6 @@ def test_try_add_more():
                                     [0, 1, 2, 4, 6, 7, 8, 10, 11, 12, 13])
     descriptors2 = break_other_than(descriptors,
                                     [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-    descriptors3 = break_other_than(descriptors,
-                                    [1, 3, 4, 5, 6, 8, 9])
 
     keypoints0, keypoints1, keypoints2 = keypoints_true[0:3]
     kd0 = KD(keypoints0, descriptors0)
@@ -177,61 +147,47 @@ def test_try_add_more():
     assert(vo.n_active_keyframes == 2)
 
     # first triangulation
-    # point_indices   0  1  2  3  4   5   6   7   8
-    # matches01.T = [[0, 2, 4, 6, 7, 10, 11, 12, 13],
-    #                [0, 2, 4, 6, 7, 10, 11, 12, 13]]
+    # point indices        0  1  2  3  4   5   6   7   8
+    matches01 = np.array([[0, 2, 4, 6, 7, 10, 11, 12, 13],
+                          [0, 2, 4, 6, 7, 10, 11, 12, 13]]).T
 
-    point_indices0, point_indices1 = vo.point_indices_list
-    assert_array_equal(point_indices0.is_triangulated,
-                       [1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1])
-    assert_array_equal(point_indices0.triangulated,
-                       [0, 1, 2, 3, 4, 5, 6, 7, 8])
-    assert_array_equal(point_indices1.is_triangulated,
-                       [1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1])
-    assert_array_equal(point_indices1.triangulated,
-                       [0, 1, 2, 3, 4, 5, 6, 7, 8])
-
+    print(omegas[2])
+    indices0, indices1 = matches01[:, 0], matches01[:, 1]
     pose0, pose1 = vo.poses
-    assert_projection_equal(vo.points, point_indices0, pose0, keypoints0)
-    assert_projection_equal(vo.points, point_indices1, pose1, keypoints1)
+    points0, _ = vo.point_manager.get(0, indices0)
+    points1, _ = vo.point_manager.get(1, indices1)
+    assert_projection_equal(projection, pose0, points0, keypoints0[indices0])
+    assert_projection_equal(projection, pose1, points1, keypoints1[indices1])
 
     # kd0 and kd1 are already observed and kd2 is added
-    # point_indices   2     3  4      5   6   7      existing
-    # point_indices      9       10                  newly triangulated
-    # matches02.T = [[4, 5, 6, 7, 9, 10, 11, 12],
-    #                [4, 5, 6, 7, 9, 10, 11, 12]]
+    # point indices        2     3  4      5   6   7      existing
+    # point indices           9       10                  newly triangulated
+    matches02 = np.array([[4, 5, 6, 7, 9, 10, 11, 12],
+                          [4, 5, 6, 7, 9, 10, 11, 12]]).T
     #
-    # point_indices      2  3  4      5   6   7      existing
-    # point_indices  11          12                  newly triangulated
-    # matches12.T = [[1, 4, 6, 7, 8, 10, 11, 12],
-    #                [1, 4, 6, 7, 8, 10, 11, 12]]
-    np.set_printoptions(suppress=True)
+    # point indices           2  3  4      5   6   7      existing
+    # point indices       11          12                  newly triangulated
+    matches12 = np.array([[1, 4, 6, 7, 8, 10, 11, 12],
+                          [1, 4, 6, 7, 8, 10, 11, 12]]).T
 
     assert(vo.try_add_keyframe(kd2))
     assert(vo.n_active_keyframes == 3)
 
     assert(len(vo.export_points()) == 13)
 
-    point_indices0, point_indices1, point_indices2 = vo.point_indices_list
-    assert_array_equal(point_indices0.is_triangulated,
-                       [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1])
-    assert_array_equal(point_indices0.triangulated,
-                       [0, 1, 2, 9, 3, 4, 10, 5, 6, 7, 8])
-    assert_array_equal(point_indices1.is_triangulated,
-                       [1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1])
-    assert_array_equal(point_indices1.triangulated,
-                       [0, 11, 1, 2, 3, 4, 12, 5, 6, 7, 8])
-    # [0, 2, 3, 8] cannot be found in matches01 and matches02
-    # so they should not be triangulated
-    assert_array_equal(point_indices2.is_triangulated,
-                       [0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0])
-    assert_array_equal(point_indices2.triangulated,
-                       [11, 2, 9, 3, 4, 12, 10, 5, 6, 7])
-
     pose0, pose1, pose2 = vo.poses
-    assert_projection_equal(vo.points, point_indices0, pose0, keypoints0)
-    assert_projection_equal(vo.points, point_indices1, pose1, keypoints1)
-    assert_projection_equal(vo.points, point_indices2, pose2, keypoints2)
+
+    indices0, indices2 = matches02[:, 0], matches02[:, 1]
+    points0, _ = vo.point_manager.get(0, indices0)
+    points2, _ = vo.point_manager.get(2, indices2)
+    assert_projection_equal(projection, pose0, points0, keypoints0[indices0])
+    assert_projection_equal(projection, pose2, points2, keypoints2[indices2])
+
+    indices1, indices2 = matches12[:, 0], matches12[:, 1]
+    points1, _ = vo.point_manager.get(1, indices1)
+    points2, _ = vo.point_manager.get(2, indices2)
+    assert_projection_equal(projection, pose1, points1, keypoints1[indices1])
+    assert_projection_equal(projection, pose2, points2, keypoints2[indices2])
 
 
 def test_try_remove():
