d28bca5b0982c24893ee923cbe5e001b76572f23
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-23 02:07:57 +0900

    Add index conversion for running BA in pose-point correspondences

--- vitamine/local_ba.py
+++ vitamine/local_ba.py
@@ -104,3 +104,97 @@ class LocalBundleAdjustment(object):
 
         omegas, translations = poses[:, 0:3], poses[:, 3:6]
         return omegas, translations, points
+
+
+class IndexConverter(object):
+    def __init__(self):
+        self.viewpoint_indices = []
+        self.point_indices = []
+
+        self.poses = []
+        self.translations = []
+        self.points = []
+        self.keypoints = []
+
+        self.point_map = dict()
+        self.viewpoint_map = dict()
+        self.point_index = 0
+        self.viewpoint_index = 0
+
+    def add(self, viewpoint_id, point_id, pose, point, keypoint):
+        if viewpoint_id not in self.viewpoint_map.keys():
+            self.viewpoint_map[viewpoint_id] = self.viewpoint_index
+            self.viewpoint_index += 1
+            self.poses.append(pose)
+
+        if point_id not in self.point_map.keys():
+            self.point_map[point_id] = self.point_index
+            self.point_index += 1
+            self.points.append(point)
+
+        self.viewpoint_indices.append(self.viewpoint_map[viewpoint_id])
+        self.point_indices.append(self.point_map[point_id])
+        self.keypoints.append(keypoint)
+
+    def export_projection(self):
+        return (np.array(self.viewpoint_indices),
+                np.array(self.point_indices),
+                np.array(self.keypoints))
+
+    def export_pose_points(self):
+        return self.poses, self.points
+
+
+def get_converter(index_map, poses, points, keypoints_list, viewpoints):
+    converter = IndexConverter()
+    for viewpoint, pose, keypoints in zip(viewpoints, poses, keypoints_list):
+        M = index_map[viewpoint]
+        assert(len(M.keys()) == len(set(M.values())))
+        for keypoint_index, point_index in index_map[viewpoint].items():
+            converter.add(viewpoint, point_index, pose,
+                          points[point_index], keypoints[keypoint_index])
+    return converter
+
+
+def try_run_ba_(viewpoint_indices, point_indices,
+                poses, points, keypoints_true):
+    """
+    points:
+    """
+
+    ba = LocalBundleAdjustment(viewpoint_indices, point_indices,
+                               keypoints_true)
+
+    omegas = np.array([p.omega for p in poses])
+    ts = np.array([p.t for p in poses])
+
+    omegas, ts, points = ba.compute(omegas, ts, points)
+
+    poses = [Pose(omega, t) for omega, t in zip(omegas, ts)]
+    return poses, points
+
+
+def test_unique(viewpoint_indices, point_indices):
+    A = np.vstack((viewpoint_indices, point_indices))
+    assert(np.unique(A, axis=1).shape[1] == A.shape[1])
+
+
+def try_run_ba(index_map, points, poses, keypoints_list, viewpoints):
+    assert(len(poses) == len(keypoints_list) == len(viewpoints))
+
+    converter = get_converter(index_map, poses, points,
+                              keypoints_list, viewpoints)
+
+    viewpoint_indices, point_indices, keypoints_true =\
+        converter.export_projection()
+    local_poses, local_points = converter.export_pose_points()
+
+    test_unique(viewpoint_indices, point_indices)
+
+    if not can_run_ba(n_viewpoints=len(local_poses),
+                      n_points=len(local_points),
+                      n_visible=len(keypoints_true),
+                      n_pose_params=6, n_point_params=3):
+        return local_poses, local_points
+    return try_run_ba_(viewpoint_indices, point_indices,
+                       local_poses, local_points, keypoints_true)
