84155c3c297b42ae79af646f8f3d59d89c1868be
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-10-28 16:11:01 +0900

    Make p-value specifiable

--- vitamine/cost.py
+++ vitamine/cost.py
@@ -15,16 +15,17 @@ def transfer21(tform, keypoints1, keypoints2):
     return keypoints1 - tform.inverse(keypoints2)
 
 
-def transfer_outlier_detector(keypoints1, keypoints2):
+def transfer_outlier_detector(keypoints1, keypoints2, p=0.95):
     tform = ProjectiveTransform()
     success = tform.estimate(keypoints1, keypoints2)
     if not success:
         print_error("Failed to estimate homography")
         return None
 
+    tester = ChiSquaredTest(p)
+
     # e1 and e2 may follow the Chi-squared distribution
     D12 = transfer12(tform, keypoints1, keypoints2)
     D21 = transfer21(tform, keypoints1, keypoints2)
-    tester = ChiSquaredTest()
     mask = np.logical_and(tester.test(D12), tester.test(D21))
     return mask
