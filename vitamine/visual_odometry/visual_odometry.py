4b5e452efc91da5531b5b957cbb40c50d0867f6d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-11-13 18:31:32 +0900

    Refactor a lot. Remove a very big class that managed 3D points

--- vitamine/visual_odometry/visual_odometry.py
+++ vitamine/visual_odometry/visual_odometry.py
@@ -4,35 +4,81 @@ from vitamine.exceptions import NotEnoughInliersException, print_error
 from vitamine.keypoints import extract_keypoints, Matcher
 from vitamine.keypoints import KeypointDescriptor as KD
 from vitamine.camera_distortion import CameraModel
-from vitamine.points import PointManager
+from vitamine.point_keypoint_map import (
+    init_point_keypoint_map, copy_existing_points, correspondences,
+    triangulation_required
+)
+from vitamine.utils import merge_dicts
 from vitamine.pose import Pose, solve_pnp, estimate_pose_change
 from vitamine.triangulation import Triangulation
 from vitamine.keyframe_index import KeyframeIndices
-from skimage.color import rgb2gray
+from vitamine.random import random_bytes
 from vitamine.so3 import rodrigues
+
+from skimage.color import rgb2gray
 from vitamine.local_ba import try_run_ba
 
 
-def get_array_len_geq(min_length):
-    return lambda array: len(array) >= min_length
+def triangulation(pose0, pose1, keypoints0, keypoints1, matches01):
+    triangulator = Triangulation(pose0, pose1, keypoints0, keypoints1)
+    point_array, depth_mask = triangulator.triangulate(matches01)
+    return point_array, matches01[depth_mask]
 
 
-def accumulate_correspondences(point_manager, keypoints, matches, viewpoints):
-    points = []
-    keypoints_ = []
-    for matches01, viewpoint in zip(matches, viewpoints):
-        for index0, index1 in matches01:
-            try:
-                point = point_manager.get(viewpoint, index0)
-            except KeyError as e:
-                # keypoint corresponding to 'index0' in 'viewpoint' is not
-                # triangulated yet
-                continue
+def generate_hashes(n_hashes, n_bytes=18):
+    return [random_bytes(n_bytes) for i in range(n_hashes)]
 
-            points.append(point)
-            keypoints_.append(keypoints[index1])
 
-    return np.array(points), np.array(keypoints_)
+def init_from_two_views(matcher, kd0, kd1, min_matches=60):
+    matches01 = matcher(kd0, kd1)
+    if len(matches01) < min_matches:
+        raise NotEnoughInliersException("Not enough matches found")
+
+    pose0 = Pose.identity()
+    pose1 = estimate_pose_change(kd0.keypoints, kd1.keypoints, matches01)
+
+    triangulator = Triangulation(pose0, pose1, kd0.keypoints, kd1.keypoints)
+    point_array, depth_mask = triangulator.triangulate(matches01)
+    matches01 = matches01[depth_mask]
+
+    point_hashes = generate_hashes(len(point_array))
+    point_dict = dict(zip(point_hashes, point_array))
+
+    point_keypoint_map0 = init_point_keypoint_map()
+    point_keypoint_map1 = init_point_keypoint_map()
+
+    for (index0, index1), point_hash in zip(matches01, point_hashes):
+        point_keypoint_map0[point_hash] = index0
+        point_keypoint_map1[point_hash] = index1
+
+    return point_keypoint_map0, point_keypoint_map1, pose0, pose1, point_dict
+
+
+def point_array(point_dict, point_keys):
+    return np.array([point_dict[k] for k in point_keys])
+
+
+def try_run_ba(self, viewpoints):
+    poses = [self.poses[v] for v in viewpoints]
+    keypoints = [self.kds[v].keypoints for v in viewpoints]
+
+    try:
+        poses, points, point_indices = try_run_ba(
+            self.point_manager.index_map,
+            self.point_manager.points,
+            poses, keypoints, viewpoints
+        )
+    except ValueError as e:
+        print_error(e)
+        return
+
+    assert(len(point_indices) == len(points))
+
+    for j, pose in zip(viewpoints, poses):
+        self.poses[j] = pose
+
+    for i, point in zip(point_indices, points):
+        self.point_manager.overwrite(i, point)
 
 
 def match(matcher, viewpoints, kds, kd1, min_matches=60):
@@ -50,15 +96,28 @@ def match(matcher, viewpoints, kds, kd1, min_matches=60):
     return matches, viewpoints_
 
 
-def triangulation(point_manager, matches, viewpoints, viewpoint1, kds, kd1, poses, pose1):
-    for viewpoint0, matches01 in zip(viewpoints, matches):
-        kd0 = kds[viewpoint0]
-        pose0 = poses[viewpoint0]
-        triangulator = Triangulation(pose0, pose1,
-                                     kd0.keypoints, kd1.keypoints)
-        point_manager.triangulate(triangulator, matches01,
-                                  viewpoint0, viewpoint1)
-    return point_manager
+def value_list(dict_, keys):
+    return [dict_[k] for k in keys]
+
+
+def update(matches01, point_keypoint_map0, point_keypoint_map1,
+           pose0, pose1, kd0, kd1):
+
+    point_keypoint_map0, point_keypoint_map1, matches01 = associate_existing(
+        point_keypoint_map0, point_keypoint_map1, matches01)
+
+    point_array, matches01 = triangulation(pose0, pose1,
+                                           kd0.keypoints, kd1.keypoints,
+                                           matches01)
+
+    point_hashes = generate_hashes(len(point_array))
+    point_dict = dict(zip(point_hashes, point_array))
+
+    for (index0, index1), point_hash in zip(matches01, point_hashes):
+        point_keypoint_map0[point_hash] = index0
+        point_keypoint_map1[point_hash] = index1
+
+    return point_keypoint_map0, point_keypoint_map1, point_dict
 
 
 class VisualOdometry(object):
@@ -66,19 +125,22 @@ class VisualOdometry(object):
                  matcher=Matcher(enable_ransac=True,
                                  enable_homography_filter=True),
                  max_active_keyframes=8):
+
         self.matcher = matcher
         self.max_active_keyframes = max_active_keyframes
         self.camera_model = CameraModel(camera_parameters, distortion_model)
+
         self.active_viewpoints = KeyframeIndices()
-        self.point_manager = PointManager()
+        self.point_keypoint_maps = dict()
+
+        self.point_dict = dict()
         self.kds = dict()
         self.poses = dict()
         self.images = dict()
-
         self.kds_ = dict()
 
     def export_points(self):
-        return self.point_manager.export_points()
+        return np.array(list(self.point_dict.values()))
 
     def export_poses(self):
         poses = [self.poses[v] for v in sorted(self.poses.keys())]
@@ -98,84 +160,99 @@ class VisualOdometry(object):
         viewpoint = self.active_viewpoints.get_next()
         kd = KD(self.camera_model.undistort(keypoints), descriptors)
 
-        pose = self.try_add_keyframe(viewpoint, kd, image)
-        if pose is None:
-            print_error("Pose estimation failed")
-            return -1
+        self.try_add_keyframe(viewpoint, kd, image)
 
         self.active_viewpoints.add_new(viewpoint)
         self.kds[viewpoint] = kd
-        self.poses[viewpoint] = pose
         self.images[viewpoint] = image
         return viewpoint
-
-    def try_run_ba(self, viewpoints):
-        poses = [self.poses[v] for v in viewpoints]
-        keypoints = [self.kds[v].keypoints for v in viewpoints]
-
-        try:
-            poses, points, point_indices = try_run_ba(
-                self.point_manager.index_map,
-                self.point_manager.points,
-                poses, keypoints, viewpoints
-            )
-        except ValueError as e:
-            print_error(e)
-            return
-
-        assert(len(point_indices) == len(points))
-
-        for j, pose in zip(viewpoints, poses):
-            self.poses[j] = pose
-
-        for i, point in zip(point_indices, points):
-            self.point_manager.overwrite(i, point)
 
     def try_add_keyframe(self, viewpoint1, kd1, image1):
         min_matches = 60
         if len(self.active_viewpoints) == 0:
-            return Pose.identity()
+            self.poses[viewpoint1] = Pose.identity()
+            return
 
         if len(self.active_viewpoints) == 1:
             viewpoint0 = self.active_viewpoints[0]
-            pose0 = self.poses[viewpoint0]
             kd0 = self.kds[viewpoint0]
 
             matches01 = self.matcher(kd0, kd1)
-            pose1 = estimate_pose_change(kd0.keypoints, kd1.keypoints, matches01)
-
             if len(matches01) < min_matches:
                 raise NotEnoughInliersException("Not enough matches found")
 
-            triangulator = Triangulation(pose0, pose1,
-                                         kd0.keypoints, kd1.keypoints)
-            for index0, index1 in matches01:
-                point = triangulator.triangulate(index0, index1)
-                self.point_manager.add_point(point, viewpoint0, viewpoint1,
-                                             index0, index1)
+            pose0 = Pose.identity()
+            pose1 = estimate_pose_change(kd0.keypoints, kd1.keypoints, matches01)
 
-            return pose1
+            triangulator = Triangulation(pose0, pose1, kd0.keypoints, kd1.keypoints)
+            point_array, depth_mask = triangulator.triangulate(matches01)
+            matches01 = matches01[depth_mask]
 
-        matches, viewpoints = match(self.matcher, self.active_viewpoints, self.kds, kd1)
-        assert(len(matches) == len(viewpoints))
+            point_hashes = generate_hashes(len(point_array))
+            point_dict = dict(zip(point_hashes, point_array))
+
+            point_keypoint_map0 = init_point_keypoint_map()
+            point_keypoint_map1 = init_point_keypoint_map()
+            for (index0, index1), point_hash in zip(matches01, point_hashes):
+                point_keypoint_map0[point_hash] = index0
+                point_keypoint_map1[point_hash] = index1
+
+            self.point_keypoint_maps[viewpoint0] = point_keypoint_map0
+            self.point_keypoint_maps[viewpoint1] = point_keypoint_map1
+            self.point_dict = point_dict
+            self.poses[viewpoint0] = pose0
+            self.poses[viewpoint1] = pose1
+            return
+
+        viewpoints = []
+        matches = []
+        for viewpoint0 in self.active_viewpoints:
+            matches01 = self.matcher(self.kds[viewpoint0], kd1)
+            if len(matches01) < min_matches:
+                continue
+            viewpoints.append(viewpoint0)
+            matches.append(matches01)
 
         if len(matches) == 0:
             raise NotEnoughInliersException("No matches found")
             return None
 
-        points, keypoints = accumulate_correspondences(
-            self.point_manager, kd1.keypoints,
-            matches, viewpoints
-        )
-        pose1 = solve_pnp(points, keypoints)
+        maps = value_list(self.point_keypoint_maps, viewpoints)
+        point_keys, keypoint_indices = correspondences(maps, matches)
+        pose1 = solve_pnp(np.array(value_list(self.point_dict, point_keys)),
+                          kd1.keypoints[keypoint_indices])
 
-        self.point_manager = triangulation(self.point_manager, matches,
-                                           viewpoints, viewpoint1,
-                                           self.kds, kd1, self.poses, pose1)
+        point_dict = dict()
+        point_keypoint_map1 = init_point_keypoint_map()
+        for viewpoint0, matches01 in zip(viewpoints, matches):
+            point_keypoint_map0 = self.point_keypoint_maps[viewpoint0]
+            pose0 = self.poses[viewpoint0]
+            kd0 = self.kds[viewpoint0]
 
-        self.try_run_ba(self.active_viewpoints)
+            mask = triangulation_required(point_keypoint_map0, point_keypoint_map1, matches01)
 
-        return pose1
+            point_keypoint_map0, point_keypoint_map1 = copy_existing_points(
+                point_keypoint_map0, point_keypoint_map1, matches01[~mask]
+            )
+
+            matches01 = matches01[mask]
+            triangulator = Triangulation(pose0, pose1, kd0.keypoints, kd1.keypoints)
+            point_array, depth_mask = triangulator.triangulate(matches01)
+
+            point_hashes = generate_hashes(len(point_array))
+
+            assert(len(matches01[depth_mask]) == len(point_hashes))
+            for (index0, index1), point_hash in zip(matches01[depth_mask], point_hashes):
+                point_keypoint_map0[point_hash] = index0
+                point_keypoint_map1[point_hash] = index1
+
+            for point_hash, point in zip(point_hashes, point_array):
+                point_dict[point_hash] = point
+
+        self.point_keypoint_maps[viewpoint1] = point_keypoint_map1
+        self.point_dict = merge_dicts(self.point_dict, point_dict)
+        self.poses[viewpoint1] = pose1
+        # self.try_run_ba(self.active_viewpoints)
 
     def try_remove(self):
         if self.n_active_keyframes <= self.max_active_keyframes:
