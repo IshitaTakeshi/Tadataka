c0b05ca0bbcdafc1a09a439a3b37a1c4257d1852
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-11-17 00:59:07 +0900

    Make 'plot_matches' work

---
+++ tadataka/plot/__init__.py
@@ -0,0 +1,1 @@
+from tadataka.plot.matches import plot_matches
--- tadataka/plot/matches.py
+++ tadataka/plot/matches.py
@@ -1,8 +1,11 @@
+from matplotlib import pyplot as plt
+from skimage.feature import plot_matches as plot_matches_
+
 from tadataka.coordinates import xy_to_yx
 
-from skimage.feature import plot_matches
 
-
-def plot_matches(ax, image1, image2, keypoints1, keypoints2):
+def plot_matches(image1, image2, keypoints1, keypoints2, matches12):
+    fig, ax = plt.subplots()
     plot_matches_(ax, image1, image2,
                   xy_to_yx(keypoints1), xy_to_yx(keypoints2), matches12)
+    plt.show()
