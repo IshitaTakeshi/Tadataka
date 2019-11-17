c57b166e4cd45e42e6bdadc02efb8d465b5b82eb
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-11-18 00:37:35 +0900

    Plot map after conustruction loop ended

--- nikkei/vo.py
+++ nikkei/vo.py
@@ -19,26 +19,23 @@ vo = VisualOdometry(
     max_active_keyframes=6
 )
 
-
-# とりあえずこれを動かしてもらう
-
 filenames = sorted(Path("./datasets/saba/").glob("*.jpg"))
 filenames = [filenames[0]] + filenames[4:]
-filenames = filenames[:4]
 
 for i, filename in enumerate(filenames):
     print("filename = {}".format(filename))
+    # 画像の読み込み
     image = imread(filename)
 
+    # フレームの追加
     viewpoint = vo.add(image)
 
+    # フレームの追加に失敗した場合負の数が返る
     if viewpoint < 0:
-        continue  # フレームの追加に失敗した場合
-
-    vo.try_remove()
-
-    if i == 0:
         continue
 
-    points, colors = vo.export_points()
-    plot_map(vo.export_poses(), points, colors)
+    # 不要なフレームを削除する
+    vo.try_remove()
+
+points, colors = vo.export_points()
+plot_map(vo.export_poses(), points, colors)
