e9e1b658e3f5a19f1963a28393260fd9cd3e8c05
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-11-22 16:41:46 +0900

    Remove unused functions

--- tadataka/point_keypoint_map.py
+++ tadataka/point_keypoint_map.py
@@ -19,30 +19,6 @@ def point_exists(point_keypoint_map, keypoint_index):
     return keypoint_index in point_keypoint_map.values()
 
 
-def associate_new(point_keypoint_map0, point_keypoint_map1,
-                  point_hashes, matches01):
-    assert(len(matches01) == len(point_hashes))
-    for (index0, index1), point_hash in zip(matches01, point_hashes):
-        point_keypoint_map0[point_hash] = index0
-        point_keypoint_map1[point_hash] = index1
-    return point_keypoint_map0, point_keypoint_map1
-
-
-def warn_if_incorrect_match(point_keypoint_map0, point_keypoint_map1,
-                            keypoint_index0, keypoint_index1):
-    point_hash0 = point_by_keypoint(point_keypoint_map0, keypoint_index0)
-    point_hash1 = point_by_keypoint(point_keypoint_map1, keypoint_index1)
-    if point_hash0 != point_hash1:
-        message = (
-            "Wrong match! "
-            "Keypoint index {} in viewpoint 0 and "
-            "keypoint index {} in viewpoint 1 have matched, "
-            "but indicate different 3D points!"
-        )
-        warnings.warn(message.format(keypoint_index0, keypoint_index1),
-                      RuntimeWarning)
-
-
 def triangulation_required(map_, keypoint_indices):
     return np.array([not point_exists(map_, i) for i in keypoint_indices])
 
