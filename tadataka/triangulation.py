e7a33ca753d63c2cb5ff8d6b2a3ceeddad7ffc4d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-11-28 17:59:55 +0900

    Pass only keypoints to 'Triangulation' class

--- tadataka/triangulation.py
+++ tadataka/triangulation.py
@@ -27,27 +27,27 @@ def linear_triangulation(pose0, pose1, keypoint0, keypoint1, min_depth=0.0):
 
 
 class Triangulation(object):
-    def __init__(self, pose0, pose1, keypoints0, keypoints1):
+    def __init__(self, pose0, pose1):
         self.pose0, self.pose1 = pose0, pose1
-        self.keypoints0, self.keypoints1 = keypoints0, keypoints1
 
-    def triangulate_(self, index0, index1):
-        keypoint0 = self.keypoints0[index0]
-        keypoint1 = self.keypoints1[index1]
+    def triangulate_(self, keypoint0, keypoint1):
         return linear_triangulation(self.pose0, self.pose1, keypoint0, keypoint1)
 
-    def triangulate(self, matches01):
+    def triangulate(self, keypoints0, keypoints1):
+        assert(keypoints0.shape == keypoints1.shape)
         points = []
         depth_mask = []
-        for index0, index1 in matches01:
-            point, depth_is_positive = self.triangulate_(index0, index1)
+        for keypoint0, keypoint1 in zip(keypoints0, keypoints1):
+            point, depth_is_positive = self.triangulate_(keypoint0, keypoint1)
             points.append(point)
             depth_mask.append(depth_is_positive)
         return np.array(points), np.array(depth_mask)
 
 
 def triangulate(pose0, pose1, keypoints0, keypoints1, matches01):
-    t = Triangulation(pose0, pose1, keypoints0, keypoints1)
-    point_array, depth_mask = t.triangulate(matches01)
+    keypoints0 = keypoints0[matches01[:, 0]]
+    keypoints1 = keypoints1[matches01[:, 1]]
+    t = Triangulation(pose0, pose1)
+    point_array, depth_mask = t.triangulate(keypoints0, keypoints1)
     # preserve points that have positive depths
     return point_array[depth_mask], matches01[depth_mask]
