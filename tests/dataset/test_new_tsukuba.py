7e240bf1c34b31c0773d6251d74613eed2af5fc6
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-04 20:40:49 +0900

    Check each camera location of stereo camera

--- tests/dataset/test_new_tsukuba.py
+++ tests/dataset/test_new_tsukuba.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-from numpy.testing import assert_equal
+from numpy.testing import (assert_array_almost_equal,
+                           assert_array_equal, assert_equal)
 
 from tadataka.dataset.new_tsukuba import NewTsukubaDataset
 
@@ -19,16 +20,15 @@ def test_new_tsukuba():
     assert_equal(frame.depth_map_left.shape, image_shape[0:2])
     assert_equal(frame.depth_map_right.shape, image_shape[0:2])
 
-    frames = dataset[1:4:2]
+    assert_array_equal(frame.position_left, [-5, 0, 0])
+    assert_array_equal(frame.position_right, [5, 0, 0])
 
-    assert_equal(frames[0].image_left.shape, image_shape)
-    assert_equal(frames[0].image_right.shape, image_shape)
-    assert_equal(frames[0].depth_map_left.shape, image_shape[0:2])
-    assert_equal(frames[0].depth_map_right.shape, image_shape[0:2])
-
-    assert_equal(frames[1].image_left.shape, image_shape)
-    assert_equal(frames[1].image_right.shape, image_shape)
-    assert_equal(frames[1].depth_map_left.shape, image_shape[0:2])
-    assert_equal(frames[1].depth_map_right.shape, image_shape[0:2])
-
-    assert_equal(len(frames), 2)
+    frame = dataset[4]
+    assert_array_almost_equal(
+        frame.position_left,
+        [-4.98281912e+00, -3.48797408e-05, 3.71711033e-01]
+    )
+    assert_array_almost_equal(
+        frame.position_right,
+        [4.98282112e+00, 3.48797408e-05, -4.56549033e-01]
+    )
