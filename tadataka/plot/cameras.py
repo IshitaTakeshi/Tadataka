389b58eb7976e7fc3e93ed92a4c237ba7e31afea
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-07 22:23:09 +0900

    Simplify camera plot

--- tadataka/plot/cameras.py
+++ tadataka/plot/cameras.py
@@ -3,44 +3,41 @@ import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-from tadataka.rigid_transform import transform_all
+from tadataka.rigid_transform import transform
 
 
-def plot_cameras(ax, camera_rotations, camera_locations):
-    ax.add_collection3d(
-        cameras_poly3d(
-            np.array(camera_rotations),
-            np.array(camera_locations)
-        )
-    )
+vertices = np.array([
+    [-0.5, -0.5, 1.5],
+    [+0.5, -0.5, 1.5],
+    [+0.5, +0.5, 1.5],
+    [-0.5, +0.5, 1.5],
+    [0, 0, 0]
+])
+
+
+def plot_cameras_(ax, poses):
+    ax.add_collection3d(cameras_poly3d(poses))
     return ax
 
 
-def cameras_poly3d(camera_rotations, camera_locations, scale=1.0):
+def cameras_poly3d(poses, scale=1.0):
     # this code is the modified version of
     # [an answer](https://stackoverflow.com/a/44920709)
     # by [serenity](https://stackoverflow.com/users/2666859/serenity)
 
-    v = np.array([
-        [-0.5, -0.5, 1.5],
-        [+0.5, -0.5, 1.5],
-        [+0.5, +0.5, 1.5],
-        [-0.5, +0.5, 1.5],
-        [0, 0, 0]
-    ])
+    V = []
+    for pose in poses:
+        pose = pose.local_to_world()
+        v = transform(pose.rotation.as_dcm(), pose.t, vertices * scale)
 
-    V = transform_all(camera_rotations, camera_locations, v * scale)
-
-    verts = []
-    for v in V:
         P = np.array([
             [v[0], v[1], v[4]],
             [v[0], v[3], v[4]],
             [v[2], v[1], v[4]],
             [v[2], v[3], v[4]],
         ])
-        verts.append(P)
-    verts = np.vstack(verts)
+        V.append(P)
+    V = np.vstack(V)
 
-    return Poly3DCollection(verts, facecolors='cyan', linewidths=1,
+    return Poly3DCollection(V, facecolors='cyan', linewidths=1,
                             edgecolors='r', alpha=.25)
--- tadataka/plot/map.py
+++ tadataka/plot/map.py
@@ -4,20 +4,12 @@ from matplotlib import pyplot as plt
 from tadataka.coordinates import local_to_world
 from tadataka.plot.common import axis3d
 from tadataka.plot.visualizers import plot3d_
-from tadataka.plot.cameras import plot_cameras
+from tadataka.plot.cameras import plot_cameras_
 from tadataka.so3 import rodrigues
 
 
-def plot_map_(camera_omegas, camera_locations, points, color=None):
-    ax = axis3d()
-    plot3d_(ax, points, color)
-    plot_cameras(ax, rodrigues(camera_omegas), camera_locations)
-    plt.show()
-
-
 def plot_map(poses, points, colors=None):
-    omegas, translations = zip(*[[p.omega, p.t] for p in poses])
-    omegas = np.array(omegas)
-    translations = np.array(translations)
-    omegas, translations = local_to_world(omegas, translations)
-    plot_map_(omegas, translations, points, colors)
+    ax = axis3d()
+    plot3d_(ax, points, colors)
+    plot_cameras_(ax, poses)
+    plt.show()
