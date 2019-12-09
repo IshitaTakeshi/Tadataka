25ae36727f8ceff46cc5b1897a5f3759ec9bec98
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-09 17:54:13 +0900

    Remove unused functions

--- tadataka/flow_estimation/flow_estimation.py
+++ tadataka/flow_estimation/flow_estimation.py
@@ -1,34 +1,13 @@
 from autograd import numpy as np
 
-from tadataka.optimization.functions import Function
 from tadataka.transform import AffineTransform
-from tadataka.optimization.residuals import BaseResidual
-from tadataka.optimization.robustifiers import (
-    GemanMcClureRobustifier, SquaredRobustifier)
-from tadataka.optimization.updaters import GaussNewtonUpdater
-from tadataka.optimization.optimizers import Optimizer
-from tadataka.optimization.transformers import BaseTransformer
-from tadataka.optimization.errors import SumRobustifiedNormError
 from tadataka import irls
-# we handle point coordinates P in a format:
-# P[:, 0] contains x coordinates
-# P[:, 1] contains y coordinates
 
 
-def affine_params_to_theta(A, b):
-    return np.concatenate((A.flatten(), b.flatten()))
-
-
-def theta_to_affine_params(theta):
-    A = np.reshape(theta[0:4], (2, 2))
-    b = theta[4:6]
-    return A, b
-
-
-def estimate_affine_transform(keypoints1, keypoints2):
-    keypoints1 = np.column_stack((keypoints1, np.ones(keypoints1.shape[0])))
-    params0 = irls.fit(keypoints1, keypoints2[:, 0])
-    params1 = irls.fit(keypoints1, keypoints2[:, 1])
+def estimate_affine_transform(keypoints0, keypoints1):
+    keypoints0 = np.column_stack((keypoints0, np.ones(keypoints0.shape[0])))
+    params0 = irls.fit(keypoints0, keypoints1[:, 0])
+    params1 = irls.fit(keypoints0, keypoints1[:, 1])
 
     A0, b0 = params0[0:2], params0[2]
     A1, b1 = params1[0:2], params1[2]
