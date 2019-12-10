0dd2476c956a1a0cec6bf5693b4f29142e8117b1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-10 21:47:21 +0900

    Make kwargs of skimage's 'plot_matches' accesible

--- tadataka/plot/matches.py
+++ tadataka/plot/matches.py
@@ -4,8 +4,9 @@ from skimage.feature import plot_matches as plot_matches_
 from tadataka.coordinates import xy_to_yx
 
 
-def plot_matches(image1, image2, keypoints1, keypoints2, matches12):
+def plot_matches(image1, image2, keypoints1, keypoints2, matches12, **kwargs):
     fig, ax = plt.subplots()
     plot_matches_(ax, image1, image2,
-                  xy_to_yx(keypoints1), xy_to_yx(keypoints2), matches12)
+                  xy_to_yx(keypoints1), xy_to_yx(keypoints2), matches12,
+                  **kwargs)
     plt.show()
