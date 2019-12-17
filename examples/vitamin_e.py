986764a373a4ab9b9b6d9bf7fae6d63a76306d67
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-17 21:44:32 +0900

    Implement VO on dense keypoints

--- examples/vitamin_e.py
+++ examples/vitamin_e.py
@@ -1,16 +1,45 @@
 from pathlib import Path
 
 import numpy as np
+from matplotlib import pyplot as plt
 from skimage.color import rgb2gray
+
 from tadataka.feature import extract_features, Matcher
 from tadataka.flow_estimation.flow_estimation import estimate_affine_transform
 from tadataka.flow_estimation.image_curvature import (
     extract_curvature_extrema, compute_image_curvature)
 from tadataka.pose import Pose, estimate_pose_change, solve_pnp
-from tadataka.triangulation import TwoViewTriangulation
+from tadataka.triangulation import Triangulation, TwoViewTriangulation
 from tadataka.flow_estimation.extrema_tracker import ExtremaTracker
 from tadataka.dataset.tum_rgbd import TumRgbdDataset
 from tadataka.utils import is_in_image_range
+
+
+def keypoints_from_new_area(image1, flow01):
+    """Extract keypoints from newly observed image rea"""
+    keypoints1 = extract_curvature_extrema(image1)
+    # out of image range after transforming from frame1 to frame0
+    # we assume image1.shape == image0.shape
+    mask = ~is_in_image_range(flow01.inverse(keypoints1), image1.shape)
+    return keypoints1[mask]
+
+
+
+def track(keypoints0, image1, flow01):
+    curvature = compute_image_curvature(rgb2gray(image1))
+    tracker = ExtremaTracker(curvature, lambda_=10.0)
+    return tracker.optimize(flow01(keypoints0))
+
+
+def triangulate(keypoint_node0, keypoint_node1):
+    tringulator = TwoViewTriangulation(pose0, pose1)
+    points, depths = triangulator.triangulate(
+        keypoint_node0.keypoints[keypoint_node1.tracked_indices],
+        keypoint_node1.tracked_keypoints
+    )
+
+    # TODO check if depths are positive
+    return points
 
 
 def grow(keypoints, keypoints_tracked, keypoints_new):
@@ -20,7 +49,14 @@ def grow(keypoints, keypoints_tracked, keypoints_new):
     upper = np.concatenate((keypoints, nans), axis=1)
     bottom = np.concatenate((keypoints_tracked, keypoints_new), axis=0)
     lower = bottom.reshape(1, *bottom.shape)
-    return np.concatenate((upper, lower))
+    return np.concatenate((upper, lower), axis=0)
+
+
+def estimate_flow(features0, features1):
+    matches01 = match(features0, features1)
+    keypoints0 = features0.keypoints[matches01[:, 0]]
+    keypoints1 = features1.keypoints[matches01[:, 1]]
+    return estimate_affine_transform(keypoints0, keypoints1)
 
 
 def allocate(points, n_keypoints):
@@ -28,26 +64,20 @@ def allocate(points, n_keypoints):
     return np.vstack((points, nans))
 
 
-def keypoints_from_new_area(image1, flow01):
-    keypoints1 = extract_curvature_extrema(image1)
-    # out of image range after transforming from frame1 to frame0
-    # we assume image1.shape == image0.shape
-    mask = ~is_in_image_range(flow01.inverse(keypoints1), image1.shape)
-    return keypoints1[mask]
-
-
-def track(keypoints0, flow01, image1):
-    curvature = compute_image_curvature(rgb2gray(image1))
-    tracker = ExtremaTracker(curvature, lambda_=10.0)
-    keypoints1 = flow01(keypoints0)
-    mask = is_in_image_range(keypoints1, curvature.shape)
-    keypoints1[mask] = tracker.optimize(keypoints1[mask])
-    return keypoints1
+def plot_track():
+    fig = plt.figure()
+    ax = fig.add_subplot(121)
+    ax.imshow(image1)
+    ax.scatter(keypoints[1, :, 0], keypoints[1, :, 1], s=0.1, c='red')
+    ax = fig.add_subplot(122)
+    ax.imshow(image2)
+    ax.scatter(keypoints[2, :, 0], keypoints[2, :, 1], s=0.1, c='red')
+    plt.show()
 
 
 dataset = TumRgbdDataset(Path("datasets", "rgbd_dataset_freiburg1_xyz"))
 camera_model = dataset.camera_model
-frames = dataset[270:274]
+frames = dataset[270:273]
 
 
 image0 = frames[0].image
@@ -62,78 +92,60 @@ features0 = extract_features(image0)
 features1 = extract_features(image1)
 features2 = extract_features(image2)
 
-matches01 = match(features0, features1)
-flow01 = estimate_affine_transform(
-    features0.keypoints[matches01[:, 0]],
-    features1.keypoints[matches01[:, 1]]
+pose0 = Pose.identity()
+
+
+# HACK is it better to drop keypoints0 that are out of the next image range ?
+tracked_keypoints0 = extract_curvature_extrema(image0)
+
+flow01 = estimate_flow(features0, features1)
+tracked_keypoints1 = track(tracked_keypoints0, image1, flow01)
+tracked_mask1 = is_in_image_range(tracked_keypoints1, image1.shape)
+new_keypoints1 = keypoints_from_new_area(image1, flow01)
+
+pose1 = estimate_pose_change(
+    camera_model.undistort(tracked_keypoints0[tracked_mask1]),
+    camera_model.undistort(tracked_keypoints1[tracked_mask1])
 )
 
-keypoints0 = extract_curvature_extrema(image0)
-keypoints = keypoints0.reshape(1, *keypoints0.shape)
-points = np.full((keypoints.shape[1], 3), np.nan)
-
-keypoints1_tracked = track(keypoints[-1], flow01, image1)
-mask = is_in_image_range(keypoints1_tracked, image1.shape)
-keypoints1_tracked[~mask] = np.nan
-keypoints1_new = keypoints_from_new_area(image1, flow01)
-keypoints = grow(keypoints, keypoints1_tracked, keypoints1_new)
-points = allocate(points, keypoints1_new.shape[0])
-assert(points.shape[0] == keypoints.shape[1])
-
-mask = np.all(~np.isnan(keypoints), axis=(0, 2))
-
-poses = [None] * 3
-poses[0] = Pose.identity()
-poses[1] = estimate_pose_change(
-    camera_model.undistort(keypoints[0, mask]),
-    camera_model.undistort(keypoints[1, mask])
+triangulator = TwoViewTriangulation(pose0, pose1)
+points01, depths = triangulator.triangulate(
+    camera_model.undistort(tracked_keypoints0[tracked_mask1]),
+    camera_model.undistort(tracked_keypoints1[tracked_mask1])
 )
 
-# poses[0], poses[1] = poses[0], poses[1]
-triangulator = TwoViewTriangulation(poses[0], poses[1])
-points[mask], depth_mask = triangulator.triangulate(
-    camera_model.undistort(keypoints[0, mask]),
-    camera_model.undistort(keypoints[1, mask])
-)
+tracked_keypoints1 = tracked_keypoints1[tracked_mask1]
 
-from tadataka.plot import plot3d
-point_mask = np.all(~np.isnan(points), axis=1)
-plot3d(points[point_mask])
+points = points01
 
-matches12 = match(features1, features2)
-
-flow12 = estimate_affine_transform(
-    features1.keypoints[matches12[:, 0]],
-    features2.keypoints[matches12[:, 1]]
-)
-
-keypoints2_tracked = track(keypoints[-1], flow12, image2)
-mask = is_in_image_range(keypoints2_tracked, image2.shape)
-keypoints2_tracked[~mask] = np.nan
-keypoints2_new = keypoints_from_new_area(image2, flow12)
-keypoints = grow(keypoints, keypoints2_tracked, keypoints2_new)
-points = allocate(points, keypoints2_new.shape[0])
-assert(points.shape[0] == keypoints.shape[1])
-
-keypoints_ = keypoints[-1]
-
-point_mask = np.all(~np.isnan(points), axis=1)
-keypoint_mask = np.all(~np.isnan(keypoints_), axis=1)
-mask = point_mask & keypoint_mask
-poses[2] = solve_pnp(points[mask], camera_model.undistort(keypoints_[mask]))
+assert(points01.shape[0] == tracked_keypoints1.shape[0])
 
 from tadataka.plot import plot_map
-plot_map(poses, points[point_mask])
+plot_map([pose0, pose1], points01)
 
-keypoint_mask = np.all(~np.isnan(keypoints[-2:]), axis=(0, 2))
-point_mask = np.all(~np.isnan(points), axis=1)
+flow12 = estimate_flow(features1, features2)
 
-mask = keypoint_mask & point_mask
-triangulator = TwoViewTriangulation(poses[0], poses[2])
-points[mask], depth_mask = triangulator.triangulate(
-    camera_model.undistort(keypoints[0, mask]),
-    camera_model.undistort(keypoints[2, mask])
+tracked_keypoints2 = track(tracked_keypoints1, image2, flow12)
+tracked_mask2 = is_in_image_range(tracked_keypoints2, image2.shape)
+pose2 = solve_pnp(
+    points01[tracked_mask2],
+    camera_model.undistort(tracked_keypoints2[tracked_mask2])
 )
 
-point_mask = np.all(~np.isnan(points), axis=1)
-plot_map(poses, points[point_mask])
+tracked_new_keypoints2 = track(new_keypoints1, image2, flow12)
+new_mask2 = is_in_image_range(tracked_new_keypoints2, image2.shape)
+
+triangulator = TwoViewTriangulation(pose1, pose2)
+points12, depths = triangulator.triangulate(
+    camera_model.undistort(new_keypoints1[new_mask2]),
+    camera_model.undistort(tracked_new_keypoints2[new_mask2])
+)
+
+tracked_keypoints2 = np.vstack((
+    tracked_keypoints2[tracked_mask2],
+    tracked_new_keypoints2[new_mask2]
+))
+new_keypoints2 = keypoints_from_new_area(image2, flow12)
+
+points = np.vstack((points, points12))
+plot_map([pose0, pose1, pose2], points)
