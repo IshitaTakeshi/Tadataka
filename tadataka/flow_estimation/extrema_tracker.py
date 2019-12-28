5e9a5a112e58af5e19343cecc65ba76eb3bb9b88
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-12-29 01:04:12 +0900

    Fix a bug that regularizer term is not calculated properly The argument should be the difference between the initial and current coordinates but coordinate of last and current were used

--- tadataka/flow_estimation/extrema_tracker.py
+++ tadataka/flow_estimation/extrema_tracker.py
@@ -3,7 +3,6 @@ import numpy as np
 import numba
 from numba import njit
 
-
 from tadataka.flow_estimation.regularizer import get_geman_mcclure
 from tadataka.utils import is_in_image_range
 
@@ -15,63 +14,61 @@ diff_to_neighbors_ = np.array([
 ])
 
 
-# regularizer term 'w'
-def compute_regularizer_map(regularizer):
-    D = np.empty((3, 3))
-    for y in range(3):
-        for x in range(3):
-            D[y, x] = regularizer(diff_to_neighbors_[y * 3 + x])
-    return 1 - D
-
-
-@njit
 def step(energy_map):
     return diff_to_neighbors_[np.argmax(energy_map)]
 
 
-@njit
-def maximize_one(curvature, regularizer_map, p0, max_iter=20):
-    px, py = p0
-    for i in range(max_iter):
-        C = curvature[py-1:py+2, px-1:px+2]
+# regularizer term 'w'
+def compute_regularizer_map(regularizer, dp):
+    xs, ys = np.meshgrid([-1, 0, 1], [-1, 0, 1])
+    DP = np.column_stack((xs.flatten(), ys.flatten()))
 
-        dpx, dpy = step(C + regularizer_map)
-
-        if dpx == 0 and dpy == 0:
-            return [px, py]
-
-        px, py = px + dpx, py + dpy
-    return [px, py]
+    D = np.array([regularizer(dp + ddp) for ddp in DP])
+    D = D.reshape(3, 3)
+    return 1 - D
 
 
-@njit
-def maximize_(curvature, regularizer_map, coordinates):
-    for i in range(coordinates.shape[0]):
-        coordinates[i] = maximize_one(curvature, regularizer_map,
-                                      coordinates[i])
-    return coordinates
+def get_patch(curvature, p):
+    px, py = p
+    return curvature[py-1:py+2, px-1:px+2]
 
 
-def maximize(curvature, regularizer_map, coordinates):
-    C = np.pad(curvature, ((1, 1), (1, 1)),
-               mode="constant", constant_values=-np.inf)
-    offset = np.array([1, 1])
-    P = coordinates + offset
+class Maximizer(object):
+    def __init__(self, curvature, regularizer, lambda_, max_iter=20):
+        # fill the border with -inf
+        self.curvature = np.pad(curvature, ((1, 1), (1, 1)),
+                                mode="constant", constant_values=-np.inf)
+        self.regularizer = regularizer
+        self.lambda_ = lambda_
+        self.max_iter = max_iter
 
-    P = maximize_(C, regularizer_map, P)
+    def _maximize(self, p0):
+        p = np.copy(p0)
+        for i in range(self.max_iter):
+            C = get_patch(self.curvature, p)
+            R = compute_regularizer_map(self.regularizer, p - p0)
+            dp = step(C + self.lambda_ * R)
 
-    return P - offset
+            if dp[0] == 0 and dp[1] == 0:
+                return p
+
+            p = p + dp
+        return p
+
+    def __call__(self, coordinates):
+        offset = np.array([1, 1])
+        P = coordinates + offset
+        P = self._maximize(P)
+        return P - offset
 
 
 # FIXME rename to 'LocalExtremaCorrection'
 class ExtremaTracker(object):
     """ Optimize equation (5) """
     def __init__(self, image_curvature, lambda_,
-                 regularizer=get_geman_mcclure(1.0)):
-        self.curvature = image_curvature
-
-        R = compute_regularizer_map(regularizer)
-        self.regularizer_map = lambda_ * R
+                 regularizer=get_geman_mcclure(10.0)):
+        self.maximizer = Maximizer(image_curvature, regularizer, lambda_)
+        self.image_shape = image_curvature.shape
 
     def optimize(self, initial_coordinates):
         """
@@ -81,14 +78,17 @@ class ExtremaTracker(object):
         assert(np.ndim(initial_coordinates) == 2)
         assert(initial_coordinates.shape[1] == 2)
 
+        coordinates = initial_coordinates
         coordinates = np.round(initial_coordinates)
         after_decimal = initial_coordinates - coordinates
 
         coordinates = coordinates.astype(np.int64)
 
-        mask = is_in_image_range(coordinates, self.curvature.shape)
+        mask = is_in_image_range(coordinates, self.image_shape)
 
-        coordinates[mask] = maximize(self.curvature, self.regularizer_map,
-                                     coordinates[mask])
+        P = coordinates[mask]
+        for i in range(P.shape[0]):
+            P[i] = self.maximizer(P[i])
+        coordinates[mask] = P
 
-        return coordinates.astype(np.float64) + after_decimal
+        return coordinates + after_decimal
