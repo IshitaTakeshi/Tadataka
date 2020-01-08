a1b647f6376a73efc47a17f595cf26063be7f097
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-01-08 21:20:35 +0900

    Camera parameters differ in each 'freiburg' in TUM RGB-D dataset

--- tadataka/dataset/tum_rgbd.py
+++ tadataka/dataset/tum_rgbd.py
@@ -5,12 +5,15 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 from skimage.io import imread
 
-from tadataka.camera import CameraModel, CameraParameters, FOV
+from tadataka.camera import CameraModel, CameraParameters, RadTan
 from tadataka.dataset.frame import Frame
 from tadataka.dataset.base import BaseDataset
 from tadataka.dataset.tum import load_image_paths, synchronize
 from tadataka.utils import value_list
 from tadataka.pose import Pose
+
+
+DEPTH_FACTOR = 5000.
 
 
 def load_depth_image_paths(dataset_root):
@@ -36,14 +39,74 @@ def load_ground_truth_poses(dataset_root):
     return load_poses(Path(dataset_root, "groundtruth.txt"), delimiter=None)
 
 
-# TODO download and set dataset_root automatically
-class TumRgbdDataset(BaseDataset):
-    def __init__(self, dataset_root, depth_factor=5000.):
-        self.depth_factor = depth_factor
-        self.camera_model = CameraModel(
-            CameraParameters(focal_length=[525., 525.], offset=[319.5, 239.5]),
-            FOV(0.0)
+def no_such_sequence_message(freiburg):
+    return "No such sequence 'freiburg{}'".format(freiburg)
+
+
+# TODO better to move these camera model definitions to text files
+def get_camera_model_depth(freiburg):
+    if freiburg == 1:
+        return CameraModel(
+            CameraParameters(focal_length=[591.1, 590.1],
+                             offset=[331.0, 234.0]),
+            RadTan([-0.0410, 0.3286, 0.0087, 0.0051, -0.5643])
         )
+    if freiburg == 2:
+        return CameraModel(
+            CameraParameters(focal_length=[580.8, 581.8],
+                             offset=[308.8, 253.0]),
+            RadTan([-0.2297, 1.4766, 0.0005, -0.0075, -3.4194])
+        )
+    if freiburg == 3:
+        return CameraModel(
+            CameraParameters(focal_length=[567.6, 570.2],
+                             offset=[324.7, 250.1]),
+            RadTan([0, 0, 0, 0, 0])
+        )
+    raise ValueError(no_such_sequence_message(freiburg))
+
+
+def get_camera_model_rgb(freiburg):
+    if freiburg == 1:
+        return CameraModel(
+            CameraParameters(focal_length=[517.3, 516.5],
+                             offset=[318.6, 255.3]),
+            RadTan([0.2624, -0.9531, -0.0054, 0.0026, 1.1633])
+        )
+    if freiburg == 2:
+        return CameraModel(
+            CameraParameters(focal_length=[520.9, 521.0],
+                             offset=[325.1, 249.7]),
+            RadTan([0.2312, -0.7849, -0.0033, -0.0001, 0.9172])
+        )
+    if freiburg == 3:
+        return CameraModel(
+            CameraParameters(focal_length=[535.4, 539.2],
+                             offset=[320.1, 247.6]),
+            RadTan([0, 0, 0, 0, 0])
+        )
+    raise ValueError(no_such_sequence_message(freiburg))
+
+
+def get_depth_scale(freiburg):
+    if freiburg == 1:
+        return 1.035
+    if freiburg == 2:
+        return 1.031
+    if freiburg == 3:
+        return 1.000
+    raise ValueError(no_such_sequence_message(freiburg))
+
+
+# TODO download and set dataset_root and which_freiburg automatically
+class TumRgbdDataset(BaseDataset):
+    def __init__(self, dataset_root, which_freiburg):
+        # there are 3 types of camera model
+        # specify which to use by setting 'which_freiburg'
+
+        self.depth_factor = DEPTH_FACTOR * get_depth_scale(which_freiburg)
+        self.camera_model = get_camera_model_rgb(which_freiburg)
+        self.camera_model_depth = get_camera_model_depth(which_freiburg)
 
         timestamps_gt, rotations, positions =\
             load_ground_truth_poses(dataset_root)
