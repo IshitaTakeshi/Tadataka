d80652a3f071ad4d78d6f7b22f1b65c7d24e1c8e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-01-14 19:37:11 +0900

    Add tests for camera normalizer

---
+++ tests/camera/test_normalizer.py
@@ -0,0 +1,35 @@
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from tadataka.camera.parameters import CameraParameters
+from tadataka.camera.normalizer import Normalizer
+
+
+camera_parameters = CameraParameters(focal_length=[10, 20], offset=[2, 4])
+
+unnormalized = np.array([
+    [12, 24],
+    [0, 0],
+    [8, 10]
+])
+
+normalized = np.array([
+    [1, 1.0],
+    [-0.2, -0.2],
+    [0.6, 0.3]
+])
+
+
+def test_normalize():
+    assert_array_equal(
+        Normalizer(camera_parameters).normalize(unnormalized),
+        normalized
+    )
+
+
+def test_unnormalize():
+
+    assert_array_equal(
+        Normalizer(camera_parameters).unnormalize(normalized),
+        unnormalized
+    )
