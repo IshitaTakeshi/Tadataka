57a1164e8e0f381911ebddca9ed235d170e73bb2
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-01-17 15:11:00 +0900

    Make interpolation accept multi-channel images

--- tadataka/interpolate.py
+++ tadataka/interpolate.py
@@ -6,10 +6,15 @@ def interpolate(image, coordinates):
     assert(is_in_image_range(coordinates, image.shape).all())
     P = coordinates.astype(np.int64)
     D = coordinates - P
-    dx, dy = D[:, 0], D[:, 1]
+    dx, dy = D[:, [0]], D[:, [1]]
     ixs, iys = P[:, 0], P[:, 1]
 
-    return (dx * dy * image[iys+1, ixs+1] +
-            dy * (1 - dx) * image[iys+1, ixs] +
-            dx * (1 - dy) * image[iys, ixs+1] +
-            (1 - dx) * (1 - dy) * image[iys, ixs])
+    br = image[iys+1, ixs+1]
+    bl = image[iys+1, ixs]
+    tr = image[iys, ixs+1]
+    tl = image[iys, ixs]
+
+    return (dx * dy * br +
+            dy * (1 - dx) * bl +
+            dx * (1 - dy) * tr +
+            (1 - dx) * (1 - dy) * tl)
--- tests/test_inetrpolate.py
+++ tests/test_inetrpolate.py
@@ -4,7 +4,7 @@ from tadataka.interpolate import interpolate
 
 
 def test_interpolate():
-    # image.shape == (2, 2, 4)
+    # image.shape == (2, 2, 4), 4 channels 2 x 2 image
 
     image = np.array([
         [[0, 0, 0, 0], [1, 0, 1, 0]],
@@ -15,7 +15,8 @@ def test_interpolate():
 
     coordinates = np.array([
     #      x    y
-        [0.3, 0.6]
+        [0.3, 0.6],
+        [0.1, 0.8]
     ])
     intensities = interpolate(image, coordinates)
 
@@ -24,3 +25,9 @@ def test_interpolate():
                 image[1, 0] * 0.6 * (1 - 0.3) +         # x, y = 0, 1
                 image[1, 1] * 0.3 * 0.6)                # x, y = 1, 1
     assert_array_almost_equal(intensities[0], expected)
+
+    expected = (image[0, 0] * (1 - 0.1) * (1 - 0.8) +   # x, y = 0, 0
+                image[0, 1] * 0.1 * (1 - 0.8) +         # x, y = 1, 0
+                image[1, 0] * 0.8 * (1 - 0.1) +         # x, y = 0, 1
+                image[1, 1] * 0.1 * 0.8)                # x, y = 1, 1
+    assert_array_almost_equal(intensities[1], expected)
