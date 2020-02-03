f11da58b0d68531b14369d1f966ac26e867b61eb
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-03 21:30:22 +0900

    Implement image interpolation in SciPy's 'map_coordinates'

--- tadataka/interpolation.py
+++ tadataka/interpolation.py
@@ -1,20 +1,28 @@
 import numpy as np
-from tadataka.utils import is_in_image_range
+from scipy.ndimage import map_coordinates
 
 
-def interpolate(image, coordinates):
-    assert(is_in_image_range(coordinates, image.shape).all())
-    P = coordinates.astype(np.int64)
-    D = coordinates - P
-    dx, dy = D[:, [0]], D[:, [1]]
-    ixs, iys = P[:, 0], P[:, 1]
+def interpolation(I, Q, order=3):
 
-    br = image[iys+1, ixs+1]
-    bl = image[iys+1, ixs]
-    tr = image[iys, ixs+1]
-    tl = image[iys, ixs]
+    # Because 'map_coordinates' requires indices of
+    # [row, column] order, the 2nd axis have to be reversed
+    # so that it becomes [y, x]
 
-    return (dx * dy * br +
-            dy * (1 - dx) * bl +
-            dx * (1 - dy) * tr +
-            (1 - dx) * (1 - dy) * tl)
+    Q = Q[:, [1, 0]].T
+
+    # Here,
+    # Q = [
+    #     [y0 y0... y0 ... yi yi... yi ... ym ym ... ym]
+    #     [x0 x1... xn ... x0 x1... xn ... x0 x1 ... xn]
+    # ]
+
+    # sample pixel sequences from the given image I1
+    # warped_image = [
+    #     I[y0, x0]  I[y0, x1]  ...  I[y0, xn]
+    #     I[y1, x0]  I[y1, x1]  ...  I[y1, xn]
+    #     ...
+    #     I[ym, x0]  I[ym, x1]  ...  I[ym, xn]
+    # ]
+
+    return map_coordinates(I.astype(np.float64), Q,
+                           mode="constant", cval=np.nan, order=order)
