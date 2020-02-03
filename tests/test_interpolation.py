4426db44d518ba1c4f83fc0fb83026f2daa3385c
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-03 21:32:03 +0900

    Add more test cases for linear interpolation

--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,31 +1,45 @@
 import numpy as np
-from numpy.testing import assert_array_almost_equal
-from tadataka.interpolate import interpolate
+from numpy.testing import assert_almost_equal
+from tadataka.interpolation import interpolation
 
 
-def test_interpolate():
-    # image.shape == (2, 2, 4), 4 channels 2 x 2 image
+def test_interpolation():
+    # image.shape == (2, 2), 4 channels 2 x 2 image
 
     image = np.array([
-        [[0, 0, 0, 0], [1, 0, 1, 0]],
-        [[0, 1, 1, 0], [0, 0, 0, 1]]
-    ])
+        [0, 1, 5],
+        [0, 0, 2],
+        [4, 3, 2]
+    ], dtype=np.float64)
 
     coordinates = np.array([
     #      x    y
         [0.3, 0.6],
-        [0.1, 0.8]
+        [0.1, 0.8],
+        [1.4, 0.2],
+        [1.2, 4.8]
     ])
-    intensities = interpolate(image, coordinates)
+    intensities = interpolation(image, coordinates, order=1)
 
-    expected = (image[0, 0] * (1 - 0.3) * (1 - 0.6) +   # x, y = 0, 0
-                image[0, 1] * 0.3 * (1 - 0.6) +         # x, y = 1, 0
-                image[1, 0] * 0.6 * (1 - 0.3) +         # x, y = 0, 1
-                image[1, 1] * 0.3 * 0.6)                # x, y = 1, 1
-    assert_array_almost_equal(intensities[0], expected)
+    #                 y  x
+    expected = (image[0, 0] * (1.0 - 0.3) * (1.0 - 0.6) +
+                image[0, 1] * (0.3 - 0.0) * (1.0 - 0.6) +
+                image[1, 0] * (0.6 - 0.0) * (1.0 - 0.3) +
+                image[1, 1] * (0.3 - 0.0) * (0.6 - 0.0))
+    assert_almost_equal(intensities[0], expected)
 
-    expected = (image[0, 0] * (1 - 0.1) * (1 - 0.8) +   # x, y = 0, 0
-                image[0, 1] * 0.1 * (1 - 0.8) +         # x, y = 1, 0
-                image[1, 0] * 0.8 * (1 - 0.1) +         # x, y = 0, 1
-                image[1, 1] * 0.1 * 0.8)                # x, y = 1, 1
-    assert_array_almost_equal(intensities[1], expected)
+    #                 y  x
+    expected = (image[0, 0] * (1.0 - 0.1) * (1.0 - 0.8) +
+                image[0, 1] * (0.1 - 0.0) * (1.0 - 0.8) +
+                image[1, 0] * (0.8 - 0.0) * (1.0 - 0.1) +
+                image[1, 1] * (0.1 - 0.0) * (0.8 - 0.0))
+    assert_almost_equal(intensities[1], expected)
+
+    #                 y  x
+    expected = (image[0, 1] * (2.0 - 1.4) * (1.0 - 0.2) +
+                image[0, 2] * (1.4 - 1.0) * (1.0 - 0.2) +
+                image[1, 1] * (0.2 - 0.0) * (2.0 - 1.4) +
+                image[1, 2] * (1.4 - 1.0) * (0.2 - 0.0))
+
+    assert_almost_equal(intensities[2], expected)
+    assert(np.isnan(intensities[3]))
