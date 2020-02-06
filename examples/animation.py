5c4c02e84daa3a30bc5045202396c85dd7ef7a6d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-06 16:32:27 +0900

    Adaptively change ax range Add comparison with ground truth

--- examples/animation.py
+++ examples/animation.py
@@ -10,6 +10,8 @@ from tadataka.pose import Pose
 from tadataka.vo.dvo import DVO
 from tadataka.dataset.tum_rgbd import TumRgbdDataset
 from tadataka.camera import CameraParameters
+from tadataka.rigid_motion import LeastSquaresRigidMotion
+from tadataka.rigid_transform import Transform
 
 
 def set_line_3d(line, data):
@@ -21,27 +23,31 @@ def set_image(image_axis, image):
     image_axis.set_array(image)
 
 
-fig = plt.figure(figsize=(6, 6))
-ax1 = fig.add_subplot(121, projection='3d')
-ax1.set_xlim([-2, 2])
-ax1.set_ylim([-2, 2])
-ax1.set_zlim([-2, 2])
-
-ax2 = fig.add_subplot(122)
-
 camera_parameters = CameraParameters(
     focal_length=[525.0, 525.0],
     offset=[319.5, 239.5]
 )
 
 
+def set_ax_range(ax, data):
+    min_ = np.min(data, axis=0)
+    max_ = np.max(data, axis=0)
+    ax.set_xlim([min_[0], max_[0]])
+    ax.set_ylim([min_[1], max_[1]])
+    ax.set_zlim([min_[2], max_[2]])
+
+
 class Drawer(object):
-    def __init__(self, dataset):
+    def __init__(self, fig, dataset):
+        self.ax1 = fig.add_subplot(121, projection='3d')
+        self.ax2 = fig.add_subplot(122)
+
         self.pose = Pose.identity()
         self.dataset = dataset
-        self.trajectory = np.zeros((1, 3))
-        self.line = ax1.plot([0], [0], [0])[0]
-        self.image_axis = ax2.imshow(dataset[0].image)
+        self.trajectory_pred = self.pose.t
+        self.trajectory_true = self.dataset[0].pose.t
+        self.line = self.ax1.plot([0], [0], [0])[0]
+        self.image_axis = self.ax2.imshow(dataset[0].image)
 
     def update(self, i):
         frame0, frame1 = self.dataset[i+0], self.dataset[i+1]
@@ -52,18 +58,36 @@ class Drawer(object):
         dpose = vo.estimate_motion(n_coarse_to_fine=6)
 
         self.pose = self.pose * dpose.inv()
-        self.trajectory = np.vstack((self.trajectory, self.pose.t))
+        self.trajectory_pred = np.vstack((self.trajectory_pred, self.pose.t))
+        self.trajectory_true = np.vstack((self.trajectory_true, frame1.pose.t))
 
-        set_line_3d(self.line, self.trajectory)
+        set_line_3d(self.line, self.trajectory_pred)
+        set_ax_range(self.ax1, self.trajectory_pred)
         set_image(self.image_axis, frame1.image)
+
 
 
 dataset = TumRgbdDataset(Path("datasets/rgbd_dataset_freiburg1_desk"),
                          which_freiburg=1)
 
-drawer = Drawer(dataset)
+fig = plt.figure(figsize=(16, 10))
+drawer = Drawer(fig, dataset)
 
-anim = animation.FuncAnimation(fig, drawer.update, 25,
+anim = animation.FuncAnimation(fig, drawer.update, len(dataset)-1,
                                interval=50, blit=False)
+anim.save("dvo-freiburg1-desk.mp4")
+# plt.show()
 
+m = LeastSquaresRigidMotion(drawer.trajectory_pred, drawer.trajectory_true)
+R, t, s = m.solve()
+Q = drawer.trajectory_true
+P = Transform(R, t, s)(drawer.trajectory_pred)
+
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(Q[:, 0], Q[:, 1], Q[:, 2], label="true")
+ax.plot(P[:, 0], P[:, 1], P[:, 2], label="pred")
+
+plt.legend()
 plt.show()
+
