404a8cadadb7bd1d334c024299a9d7098bb32066
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-06 18:31:17 +0900

    Change the argument order

--- tadataka/plot/map.py
+++ tadataka/plot/map.py
@@ -5,7 +5,7 @@ from tadataka.plot.visualizers import plot3d_
 from tadataka.plot.cameras import plot_cameras_
 
 
-def plot_map(poses, points, camera_scale=1.0, colors=None):
+def plot_map(poses, points, colors=None, camera_scale=1.0):
     ax = axis3d()
     plot3d_(ax, points, colors)
     plot_cameras_(ax, poses, camera_scale)
