a5680e5bfc3ec83a6c4cdc0972907f28b247f80a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-14 19:40:12 +0900

    Add a file to specify test dataset paths

---
+++ tests/dataset/path.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+dataset_root = Path(__file__).parent
+new_tsukuba = Path(dataset_root, "new_tsukuba")
+euroc = Path(dataset_root, "euroc",  "mav0")
+tum_rgbd = Path(dataset_root, "tum_rgbd")
