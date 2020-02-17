0f70f8e4b5e08a88335797af053c057abdeae1f1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-17 18:43:11 +0900

    Add image warp function

--- tadataka/vo/dvo/projection.py
+++ tadataka/vo/dvo/projection.py
@@ -6,6 +6,8 @@ from tadataka.rigid_transform import transform
 from tadataka.se3 import get_rotation, get_translation, log_se3
 from tadataka.vo.dvo.mask import compute_mask
 
+from tadataka.interpolation import interpolation
+
 
 def inverse_projection(camera_parameters, depth_map):
     """
@@ -89,3 +91,26 @@ def reprojection(camera_parameters, depth_map, G):
     P = transform(get_rotation(G), get_translation(G), S)
     Q = projection(camera_parameters, P)
     return Q, compute_mask(depth_map, Q)
+
+
+def warp(camera_parameters, I1, D0, G):
+    # this function samples pixels in I1 and project them to
+    # I0 coordinate system
+
+    # 'reprojection' transforms I0 coordinates to
+    # the corresponding coordinates in I1
+
+    # 'Q' has pixel coordinates in I1 coordinate system, but each pixel
+    # coordinate is corresponding to the one in I0
+    # Therefore image pixels sampled by 'Q' represents I1 transformed into
+    # I0 coordinate system
+
+    # 'G' describes the transformation from I0 coordinate system to
+    # I1 coordinate system
+
+    Q, mask = reprojection(camera_parameters, D0, G)
+
+    warped_image = interpolation(I1, Q)
+    warped_image = warped_image.reshape(D0.shape)
+
+    return warped_image, mask
