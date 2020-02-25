14a79b5b6566a12df04c194f0752364d5a9c039b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-25 21:41:43 +0900

    Make classes to calculate coordinates on epipolar line simpler

--- tadataka/vo/semi_dense/epipolar.py
+++ tadataka/vo/semi_dense/epipolar.py
@@ -24,21 +24,16 @@ def calc_coordinates(x_min, x_max, step_size):
 
 
 class ReferenceCoordinates(object):
-    def __init__(self, camera_model, image_shape, step_size):
-        self.camera_model = camera_model
-        self.image_shape = image_shape
+    def __init__(self, step_size):
         self.step_size = step_size
 
     def __call__(self, x_range):
         x_min, x_max = x_range
-        xs = calc_coordinates(x_min, x_max, self.step_size)
-        us = self.camera_model.unnormalize(xs)
-        mask = is_in_image_range(us, self.image_shape)
-        return xs[mask], us[mask]
+        return calc_coordinates(x_min, x_max, self.step_size)
 
 
-class NormalizedKeyCoordinates(object):
-    def __init__(self, epipolar_direction, sampling_steps):
+class KeyCoordinates(object):
+    def __init__(self, epipolar_direction, sampling_steps=[-2, -1, 0, 1, 2]):
         self.epipolar_direction = epipolar_direction
         self.sampling_steps = sampling_steps
 
@@ -46,15 +41,3 @@ class NormalizedKeyCoordinates(object):
         direction = normalize_length(self.epipolar_direction(x))
         step = step_size * direction
         return coordinates_along_line(x, step, self.sampling_steps)
-
-
-class KeyCoordinates(object):
-    def __init__(self, camera_model, epipolar_direction,
-                 sampling_steps=[-2, -1, 0, 1, 2]):
-        self.camera_model = camera_model
-        self.normalized = NormalizedKeyCoordinates(epipolar_direction,
-                                                   sampling_steps)
-
-    def __call__(self, x, step_size):
-        xs = self.normalized(x, step_size)
-        return self.camera_model.unnormalize(xs)
--- tests/vo/semi_dense/test_epipolar.py
+++ tests/vo/semi_dense/test_epipolar.py
@@ -4,7 +4,7 @@ from numpy.testing import assert_array_almost_equal
 
 from tadataka.camera import CameraModel, CameraParameters, FOV
 from tadataka.vo.semi_dense.epipolar import (
-    ReferenceCoordinates, NormalizedKeyCoordinates, EpipolarDirection)
+    ReferenceCoordinates, KeyCoordinates, EpipolarDirection)
 
 
 def test_reference_coordinates():
@@ -12,17 +12,15 @@ def test_reference_coordinates():
     image_shape = [height, width]
     camera_model = CameraModel(
         CameraParameters(focal_length=[10, 10], offset=[80, 100]),
-        FOV(0.00)
+        distortion_model=None
     )
 
     search_step = 5.0
-    coordinates = ReferenceCoordinates(camera_model, image_shape, search_step)
+    coordinates = ReferenceCoordinates(search_step)
 
     x_min = np.array([-15.0, -20.0])
     x_max = np.array([15.0, 20.0])
-    xs, us = coordinates((x_min, x_max))
-
-    mask = [3, 4, 5, 6, 7]  # only masked coordinates are in the image
+    xs = coordinates((x_min, x_max))
 
     xs_true = np.array([
         [-15, -20],
@@ -36,21 +34,7 @@ def test_reference_coordinates():
         [9, 12],
         [12, 16]
     ])
-    assert_array_equal(xs, xs_true[mask])
-
-    us_true = np.array([
-        [-70, -100],
-        [-40, -60],
-        [-10, -20],
-        [20, 20],
-        [50, 60],
-        [80, 100],
-        [110, 140],
-        [140, 180],
-        [170, 220],
-        [200, 260]
-    ])
-    assert_array_equal(us, us_true[mask])
+    assert_array_equal(xs, xs_true)
 
 
 def test_normalized_key_intensities():
@@ -60,7 +44,7 @@ def test_normalized_key_intensities():
     sampling_steps = [-2, -1, 0, 1, 2]
 
     direction = EpipolarDirection(t)
-    coordinates = NormalizedKeyCoordinates(direction, sampling_steps)
+    coordinates = KeyCoordinates(direction, sampling_steps)
 
     # step should be [3, 4]
     assert_array_almost_equal(
