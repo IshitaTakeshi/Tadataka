7a42c403d5310157b265d91c35a13e2e02aaf4df
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-02-26 21:35:52 +0900

    Remove unused functions

--- tadataka/vo/semi_dense/semi_dense.py
+++ tadataka/vo/semi_dense/semi_dense.py
@@ -21,45 +21,8 @@ def intensity_gradient(intensities, interval):
     return np.linalg.norm(intensities[1:] - intensities[:-1]) / interval
 
 
-def calc_inv_depths(ref_coordinate, key_coordinate,
-                    R_key_to_ref, t_key_to_ref):
-    rot_inv_x = np.dot(R_key_to_ref, x_key)
-
-    n = x_ref[0:2] * t_key_to_ref - t_key_to_ref[0:2]
-    d = rot_inv_x[0:2] - ref_coordinate * rot_inv_x[2]
-    return d / n
-
-
-def depth_coordinate(search_step):
-    return 0 if np.abs(search_step[0]) > np.abs(search_step[1]) else 1
-
-
-class InsufficientCoordinatesError(Exception):
-    pass
-
-
-def error_if_insufficient_coordinates(mask, min_coordinates):
-    n_in_image = np.sum(mask)
-    if n_in_image < min_coordinates:
-        raise InsufficientCoordinatesError(
-            "Insufficient number of coordinates "
-            "sampled from the epipolar line. "
-            "Required {}, but found {}.".format(min_coordinates, n_in_image)
-        )
-
-
-def update(d1, d2, var1, var2):
-    d = (d1 * var2 + d2 * var1) / (var1 + var2)
-    var = (var1 * var2) / (var1 + var2)
-    return d, var
-
-
 def calc_observation_variance(alpha, geo_variance, photo_variance):
     return alpha * alpha * (geo_variance + photo_variance)
-
-
-def invert_depth(depth, EPSILON=1e-16):
-    return 1 / (depth + EPSILON)
 
 
 class InverseDepthSearchRange(object):
