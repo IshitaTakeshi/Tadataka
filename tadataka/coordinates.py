5ab69c1ac92cb1c7c2da1528297a855405ed66e7
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-03 21:22:45 +0900

    Add docstring to 'image_coordinates'

--- tadataka/coordinates.py
+++ tadataka/coordinates.py
@@ -5,6 +5,15 @@ from tadataka.so3 import exp_so3
 
 
 def image_coordinates(image_shape):
+    """
+    Returns:
+        If image_shape is (m+1, n+1), returns np.ndarray of shape
+        (2, m+1 * n+1) in the form
+        [[x0, y0], [x1, y0], [x2, y0], ..., [xn, y0],
+         [x0, y1], [x1, y1], [x2, y1], ..., [xn, y1],
+         ...,
+         [x0, ym], [x1, ym], [x2, ym], ..., [xn, ym]]
+    """
     height, width = image_shape[0:2]
     xs, ys = np.meshgrid(np.arange(width), np.arange(height))
     return np.column_stack((xs.flatten(), ys.flatten()))
