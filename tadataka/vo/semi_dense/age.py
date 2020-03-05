a7062e47b8e087256ffc1f2caf4272b88034085f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-05 20:04:03 +0900

    Cleanup pixel age calculation and clean up test

--- tadataka/vo/semi_dense/age.py
+++ tadataka/vo/semi_dense/age.py
@@ -4,17 +4,20 @@ from tadataka.utils import is_in_image_range
 from tadataka.coordinates import image_coordinates
 
 
-def increment_age(age0, warp01, depth_map0):
-    image_shape = depth_map0.shape
+def increment_(age0, us0, us1):
+    xs0, ys0 = us0[:, 0], us0[:, 1]
+    xs1, ys1 = us1[:, 0], us1[:, 1]
 
-    us0 = image_coordinates(image_shape)
-    depths0 = depth_map0.flatten()
-    us1 = warp01(us0, depths0)
-    mask = is_in_image_range(us1, image_shape)
-
-    xs0, ys0 = us0[mask, 0], us0[mask, 1]
-    xs1, ys1 = us1[mask, 0], us1[mask, 1]
-
-    age1 = np.zeros(image_shape, dtype=age0.dtype)
+    age1 = np.zeros(age0.shape, dtype=age0.dtype)
     age1[ys1, xs1] = age0[ys0, xs0] + 1
     return age1
+
+
+def increment_age(age0, depth_map0, warp01):
+    assert(age0.shape == depth_map0.shape)
+    image_shape = age0.shape
+    depths0 = depth_map0.flatten()
+    us0 = image_coordinates(image_shape)
+    us1 = np.round(warp01(us0, depths0)).astype(np.int64)
+    mask = is_in_image_range(us1, image_shape)
+    return increment_(age0, us0[mask], us1[mask])
--- tests/vo/semi_dense/test_age.py
+++ tests/vo/semi_dense/test_age.py
@@ -1,58 +1,32 @@
+from numpy.testing import assert_array_equal
 import numpy as np
-from matplotlib import pyplot as plt
 
-from tadataka.coordinates import image_coordinates
-from tadataka.utils import is_in_image_range
-from tadataka.projection import Warp
-from tadataka.pose import calc_relative_pose
-from tadataka.dataset import NewTsukubaDataset, TumRgbdDataset
+from tadataka.vo.semi_dense.age import increment_
 
 
-dataset = TumRgbdDataset("datasets/rgbd_dataset_freiburg1_xyz",
-                         which_freiburg=1)
+def test_increment_():
+    age0 = np.array([
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ])
+    us0 = np.array([
+    #    x  y
+        [0, 1],
+        [1, 2],
+        [2, 2]
+    ])
+    us1 = np.array([
+    #    x  y
+        [0, 0],
+        [1, 2],
+        [2, 0]
+    ])
 
-dataset = NewTsukubaDataset("datasets/NewTsukubaStereoDataset")
-
-
-def test_pixel_age():
-    start, end = 280, 289
-    frames = [fl for fl, fr in dataset[start:end]]
-    image_shape = frames[0].image.shape[0:2]
-    age0 = np.zeros(image_shape, dtype=np.uint8)
-
-    for i in range(len(frames)-1):
-        frame0, frame1 = frames[i], frames[i+1]
-        pose01 = calc_relative_pose(frame0.pose.to_local(),
-                                    frame1.pose.to_local())
-
-        warp = Warp(frame0.camera_model, frame1.camera_model, pose01)
-        us0 = image_coordinates(frame0.image.shape)
-        depths0 = frame0.depth_map.flatten()
-        us1 = warp(us0, depths0)
-        mask = is_in_image_range(us1, frame0.image.shape)
-        age1 = np.zeros(image_shape, dtype=np.int8)
-
-        xs0, ys0 = us0[mask, 0], us0[mask, 1]
-        xs1, ys1 = us1[mask, 0], us1[mask, 1]
-        age1[ys1, xs1] = age0[ys0, xs0] + 1
-
-        age0 = age1
-
-
-    plt.subplot(2, 2, 1)
-    plt.title(f"frame {len(frames)-1}")
-    plt.axis("off")
-    plt.imshow(frames[-1].image, cmap="gray")
-
-    plt.subplot(2, 2, 2)
-    plt.title("History (the more bright, the older the pixel)")
-    plt.axis("off")
-    plt.imshow(age0, cmap="gray")
-
-    for i in range(len(frames)-1):
-        plt.subplot(4, 4, i+9)
-        plt.title(f"frame {i}")
-        plt.axis("off")
-        plt.imshow(frames[i].image)
-
-    plt.show()
+    age1 = increment_(age0, us0, us1)
+    assert_array_equal(
+        age1,
+        [[4, 0, 9],
+         [0, 0, 0],
+         [0, 8, 0]]
+    )
