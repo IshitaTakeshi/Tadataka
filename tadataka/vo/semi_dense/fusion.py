f574ad9becb90bbf9f0caf401e6e76aba17113f8
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-06 15:01:57 +0900

    Summarize denomiator

--- tadataka/vo/semi_dense/fusion.py
+++ tadataka/vo/semi_dense/fusion.py
@@ -1,4 +1,5 @@
 def fusion(d1, d2, var1, var2):
-    d = (d1 * var2 + d2 * var1) / (var1 + var2)
-    var = (var1 * var2) / (var1 + var2)
+    V = (var1 + var2)
+    d = (d1 * var2 + d2 * var1) / V
+    var = (var1 * var2) / V
     return d, var
