c1929041ce16662d392886d38fc85a720e646812
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-11 20:19:33 +0900

    Implement depth propagation

--- tadataka/vo/semi_dense/propagation.py
+++ tadataka/vo/semi_dense/propagation.py
@@ -2,26 +2,62 @@ import numpy as np
 
 from tadataka.pose import WorldPose
 from tadataka.vo.semi_dense.common import invert_depth
+from tadataka.coordinates import image_coordinates
+from tadataka.utils import is_in_image_range
 
 
 def propagate_variance(inv_depths0, inv_depths1, variances0, uncertaintity):
     # we assume few rotation change between timestamp 0 and timestamp 1
     # TODO accept the case rotations significantly change between 0 and 1
     ratio = inv_depths1 / inv_depths0
+    print("max ratio", np.max(ratio))
     variances1 = np.power(ratio, 4) * variances0 + uncertaintity
     return variances1
 
 
-class DepthMapPropagation(object):
-    def __init__(self, tz01, uncertaintity):
-        self.tz = tz01
+def substitute(array2d, us, values):
+    assert(us.shape[0] == values.shape[0])
+    xs, ys = us[:, 0], us[:, 1]
+    array2d[ys, xs] = values
+    return array2d
+
+
+class Propagation(object):
+    def __init__(self, warp2d, uncertaintity=0.01,
+                 initial_inv_depth=1.0, initial_variance=1.0):
+        self.warp2d = warp2d
         self.uncertaintity = uncertaintity
+        self.initial_inv_depth = initial_inv_depth
+        self.initial_variance = initial_variance
+
+    def _warp(self, us0, inv_depths0):
+        depths0 = invert_depth(inv_depths0)
+        us1, depths1 = self.warp2d(us0, depths0)
+        inv_depths1 = invert_depth(depths1)
+        return np.round(us1).astype(np.int64), inv_depths1
 
     def __call__(self, inv_depth_map0, variance_map0):
-        depth_map0 = invert_depth(inv_depth_map0)
-        depth_map1 = depth_map0 + self.tz
-        inv_depth_map1 = invert_depth(depth_map1)
+        image_shape = inv_depth_map0.shape
 
-        ratio = inv_depth_map1 / inv_depth_map0
-        variance_map1 = np.power(ratio, 4) * variance_map0 + self.uncertaintity
+        us0 = image_coordinates(image_shape)
+        variances0 = variance_map0.flatten()
+        inv_depths0 = inv_depth_map0.flatten()
+
+        us1, inv_depths1 = self._warp(us0, inv_depths0)
+
+        mask = is_in_image_range(us1, image_shape)
+        us1 = us1[mask]
+        inv_depths0 = inv_depths0[mask]
+        inv_depths1 = inv_depths1[mask]
+        variances0 = variances0[mask]
+
+        variances1 = propagate_variance(inv_depths0, inv_depths1,
+                                        variances0, self.uncertaintity)
+
+        inv_depth_map1 = self.initial_inv_depth * np.ones(image_shape)
+        inv_depth_map1 = substitute(inv_depth_map1, us1, inv_depths1)
+
+        variance_map1 = self.initial_variance * np.ones(image_shape)
+        variance_map1 = substitute(variance_map1, us1, variances1)
+
         return inv_depth_map1, variance_map1
--- tests/vo/semi_dense/test_propagation.py
+++ tests/vo/semi_dense/test_propagation.py
@@ -9,27 +9,22 @@ from tadataka.vo.semi_dense.propagation import propagate_variance
 def test_propagation():
     # rotate 90 degrees around the y-axis
     rotation = Rotation.from_rotvec([0, np.pi / 2, 0])
-    # forward 2 along the x axis
+
     t0 = np.array([0, 0, 1])
     t1 = np.array([2, 0, 1])
-    # depth1 = depth0 - 2
+
     pose0 = WorldPose(rotation, t0)
     pose1 = WorldPose(rotation, t1)
-    assert_almost_equal(tz01, -2.0)
 
-    uncertaintity = 1.0
+    depths0 = np.array([4.0, 5.0])
+    us0 = np.array([
+        [3.0, 2.0],
+        [-1.0, 4.0]
+    ])
 
-    shape = (4, 3)
-    depth0 = 4.0
-    variance0 = 3.0
-    inv_depth_map0 = (1 / depth0) * np.ones(shape)
-    variance_map0 = variance0 * np.ones(shape)
-
-    propagate = DepthMapPropagation(tz01, uncertaintity)
-    inv_depth_map1, variance_map1 = propagate(inv_depth_map0, variance_map0)
-
-    depth1 = depth0 - 2.0
-    assert_array_almost_equal(inv_depth_map1, (1 / depth1) * np.ones(shape))
+    warp = Warp3D(pose0, pose1)
+    us1, inv_depths1 = propagate(us0, inv_depths0)
+    assert_array_almost_equal(inv_depths1, )
 
 
 def test_propagate_variance():
