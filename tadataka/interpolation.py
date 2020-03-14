80b73ac9f8e50b2348f29971b0814df3a0fc030a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-15 02:18:55 +0900

    Make it possible to take coordinates on the image edge

--- tadataka/interpolation.py
+++ tadataka/interpolation.py
@@ -1,6 +1,28 @@
+from numba import njit
 import numpy as np
 
 from tadataka.decorator import allow_1d
+from tadataka.utils import is_in_image_range
+
+
+def interpolation_(image, C):
+    image = np.pad(image, ((0, 1), (0, 1)), mode='edge')
+
+    L = np.floor(C)
+    U = L + 1
+
+    LI = L.astype(np.int64)
+    UI = U.astype(np.int64)
+
+    CX, CY = C[:, 0], C[:, 1]
+    LX, LY = L[:, 0], L[:, 1]
+    UX, UY = U[:, 0], U[:, 1]
+    LXI, LYI = LI[:, 0], LI[:, 1]
+    UXI, UYI = UI[:, 0], UI[:, 1]
+    return (image[LYI, LXI] * (UX - CX) * (UY - CY) +
+            image[LYI, UXI] * (CX - LX) * (UY - CY) +
+            image[UYI, LXI] * (UX - CX) * (CY - LY) +
+            image[UYI, UXI] * (CX - LX) * (CY - LY))
 
 
 @allow_1d(which_argument=1)
@@ -10,18 +32,14 @@ def interpolation(image, C):
         image (np.ndarary): gray scale image
         coordinates (np.ndarray): coordinates of shape (n_coordinates, 2)
     """
-    L = np.floor(C)
-    U = L + 1
-    LI = L.astype(np.int64)
-    UI = U.astype(np.int64)
 
-    CX, CY = C[:, 0], C[:, 1]
-    LX, LY = L[:, 0], L[:, 1]
-    UX, UY = U[:, 0], U[:, 1]
-    LXI, LYI = LI[:, 0], LI[:, 1]
-    UXI, UYI = UI[:, 0], UI[:, 1]
+    if not np.ndim(image) == 2:
+        raise ValueError("Image have to be a two dimensional array")
 
-    return (image[LYI, LXI] * (UX - CX) * (UY - CY) +
-            image[LYI, UXI] * (CX - LX) * (UY - CY) +
-            image[UYI, LXI] * (UX - CX) * (CY - LY) +
-            image[UYI, UXI] * (CX - LX) * (CY - LY))
+    mask = is_in_image_range(C, image.shape)
+    if not mask.all():
+        raise ValueError(
+            "Coordinates {} out of image range".format(C[~mask])
+        )
+
+    return interpolation_(image, C)
--- tests/test_interpolation.py
+++ tests/test_interpolation.py
@@ -1,50 +1,63 @@
 import numpy as np
 from numpy.testing import assert_almost_equal
+
+import pytest
+
 from tadataka.interpolation import interpolation
 
 
 def test_interpolation():
-    # image.shape == (2, 2), 4 channels 2 x 2 image
-
     image = np.array([
         [0, 1, 5],
         [0, 0, 2],
         [4, 3, 2],
         [5, 6, 1]
     ], dtype=np.float64)
+    # width, height = (3, 4)
 
     coordinates = np.array([
     #      x    y
-        [0.3, 0.6],
-        [0.1, 0.8],
-        [0.4, 2.2],
-        [1.0, 2.0]
+        [0.0, 0.0],
+        [1.3, 2.6],  # ordinary
+        [2.0, 2.9],  # maximum x
+        [1.9, 3.0],  # maximum y
+        [2.0, 3.0],  # possible maximum coordinate
     ])
     intensities = interpolation(image, coordinates)
 
-    #                  y  x
-    expected0 = (image[0, 0] * (1.0 - 0.3) * (1.0 - 0.6) +
-                 image[0, 1] * (0.3 - 0.0) * (1.0 - 0.6) +
-                 image[1, 0] * (1.0 - 0.3) * (0.6 - 0.0) +
-                 image[1, 1] * (0.3 - 0.0) * (0.6 - 0.0))
-    assert_almost_equal(intensities[0], expected0)
+    assert_almost_equal(intensities[0], image[0, 0])
 
     #                  y  x
-    expected1 = (image[0, 0] * (1.0 - 0.1) * (1.0 - 0.8) +
-                 image[0, 1] * (0.1 - 0.0) * (1.0 - 0.8) +
-                 image[1, 0] * (1.0 - 0.1) * (0.8 - 0.0) +
-                 image[1, 1] * (0.1 - 0.0) * (0.8 - 0.0))
+    expected1 = (image[2, 1] * (2.0 - 1.3) * (3.0 - 2.6) +
+                 image[2, 2] * (1.3 - 1.0) * (3.0 - 2.6) +
+                 image[3, 1] * (2.0 - 1.3) * (2.6 - 2.0) +
+                 image[3, 2] * (1.3 - 1.0) * (2.6 - 2.0))
     assert_almost_equal(intensities[1], expected1)
 
     #                  y  x
-    expected2 = (image[2, 0] * (1.0 - 0.4) * (3.0 - 2.2) +
-                 image[2, 1] * (0.4 - 0.0) * (3.0 - 2.2) +
-                 image[3, 0] * (1.0 - 0.4) * (2.2 - 2.0) +
-                 image[3, 1] * (0.4 - 0.0) * (2.2 - 2.0))
-
+    expected2 = (image[2, 2] * (3.0 - 2.0) * (3.0 - 2.9) +
+                 image[3, 2] * (3.0 - 2.0) * (2.9 - 2.0))
     assert_almost_equal(intensities[2], expected2)
 
-    assert_almost_equal(intensities[3], image[2, 1])
+    #                  y  x
+    expected3 = (image[3, 1] * (2.0 - 1.9) * (4.0 - 3.0) +
+                 image[3, 2] * (1.9 - 1.0) * (4.0 - 3.0))
+    assert_almost_equal(intensities[3], expected3)
 
+    assert_almost_equal(intensities[4], image[3, 2])
+
+    # 1d coordinate input
     intensity = interpolation(image, coordinates[2])
     assert_almost_equal(intensity, expected2)
+
+    with pytest.raises(ValueError):
+        interpolation(image, [3.0, 2.01])
+
+    with pytest.raises(ValueError):
+        interpolation(image, [3.01, 2.0])
+
+    with pytest.raises(ValueError):
+        interpolation(image, [-0.01, 0.0])
+
+    with pytest.raises(ValueError):
+        interpolation(image, [0.0, -0.01])
