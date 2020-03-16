edc19c5b4156a1014cc511b1da47d24329feb1d6
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-16 21:25:15 +0900

    Make camera plot larger

--- tadataka/plot/cameras.py
+++ tadataka/plot/cameras.py
@@ -6,38 +6,44 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from tadataka.rigid_transform import transform
 
 
-vertices = np.array([
-    [-0.05, -0.05, 0.15],
-    [+0.05, -0.05, 0.15],
-    [+0.05, +0.05, 0.15],
-    [-0.05, +0.05, 0.15],
+vertices_ = np.array([
+    [-0.5, -0.5, 1.0],
+    [+0.5, -0.5, 1.0],
+    [+0.5, +0.5, 1.0],
+    [-0.5, +0.5, 1.0],
     [0, 0, 0]
 ])
 
 
+optical_axis_ = np.array([[0, 0, 0], [0, 0, 1]])
+
+
 def plot_cameras_(ax, poses, scale=1.0):
-    ax.add_collection3d(cameras_poly3d(poses, scale))
+    for pose in poses:
+        ax.add_collection3d(camera_poly3d(pose, scale))
+        ax.plot(*optical_axis(pose, scale), c='red')
     return ax
 
 
-def cameras_poly3d(poses, scale):
+def optical_axis(pose, scale):
+    V = transform(pose.R, pose.t, optical_axis_ * scale)
+    src, dst = V[0], V[1]
+    return [[src[0], dst[0]], [src[1], dst[1]], [src[2], dst[2]]]
+
+
+def camera_poly3d(pose, scale):
     # this code is the modified version of
     # [an answer](https://stackoverflow.com/a/44920709)
     # by [serenity](https://stackoverflow.com/users/2666859/serenity)
 
-    V = []
-    for pose in poses:
-        pose = pose.local_to_world()
-        v = transform(pose.R, pose.t, vertices * scale)
+    v = transform(pose.R, pose.t, vertices_ * scale)
 
-        P = np.array([
-            [v[0], v[1], v[4]],
-            [v[0], v[3], v[4]],
-            [v[2], v[1], v[4]],
-            [v[2], v[3], v[4]],
-        ])
-        V.append(P)
-    V = np.vstack(V)
+    P = np.array([
+        [v[0], v[1], v[4]],
+        [v[0], v[3], v[4]],
+        [v[2], v[1], v[4]],
+        [v[2], v[3], v[4]]
+    ])
 
-    return Poly3DCollection(V, facecolors='cyan', linewidths=1,
-                            edgecolors='r', alpha=.25)
+    return Poly3DCollection(P, facecolors='cyan', linewidths=1,
+                            edgecolors='red', alpha=.25)
