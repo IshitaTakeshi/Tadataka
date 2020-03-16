8e6780923a629fd97550f46485c3707b14973765
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-16 21:30:32 +0900

    Add smaller warp functions to reduce computation overhead

--- tadataka/warp.py
+++ tadataka/warp.py
@@ -1,26 +1,40 @@
+from numba import njit
 from tadataka.rigid_transform import transform
 from tadataka.decorator import allow_1d
 from tadataka.projection import inv_pi, pi
+from tadataka.rigid_transform import transform, inv_transform
+
+
+@njit
+def warp3d(T0, T1, P):
+    R0, t0 = T0
+    R1, t1 = T1
+
+    P = transform(R0, t0, P)  # camera 0 to world
+    P = inv_transform(R1, t1, P)  # world to camera 1
+    return P
+
+
+@njit
+def warp2d(T0, T1, x, depth):
+    p = inv_pi(x, depth)
+    q = warp3d(T0, T1, p)
+    return pi(q)
 
 
 class Warp3D(object):
     def __init__(self, pose0, pose1):
-        camera0_to_world = pose0
-        world_to_camera1 = pose1.to_local()
-
-        self.R0, self.t0 = camera0_to_world.R, camera0_to_world.t
-        self.R1, self.t1 = world_to_camera1.R, world_to_camera1.t
+        self.T0 = pose0.R, pose0.t
+        self.T1 = pose1.R, pose1.t
 
     @allow_1d(which_argument=1)
     def __call__(self, P):
-        P = transform(self.R0, self.t0, P)
-        P = transform(self.R1, self.t1, P)
-        return P
+        return warp3d(self.T0, self.T1, P)
 
 
-def warp(warp3d, xs0, depths0):
+def warp(warp3d_, xs0, depths0):
     P0 = inv_pi(xs0, depths0)
-    P1 = warp3d(P0)
+    P1 = warp3d_(P0)
     xs1, depths1 = pi(P1), P1[:, 2]
     return xs1, depths1
 
