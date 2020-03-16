73de3b468184b54e2fe915aebfc3f931003e8c03
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-16 21:25:44 +0900

    Make projection and inv projection run on numba jit

--- tadataka/projection.py
+++ tadataka/projection.py
@@ -1,3 +1,4 @@
+from numba import njit
 import numpy as np
 
 from tadataka.matrix import to_homogeneous
@@ -6,16 +7,24 @@ from tadataka.matrix import to_homogeneous
 EPSILON = 1e-16
 
 
+@njit
 def pi(P):
-    if np.ndim(P) == 1:
+    if P.ndim == 1:
         return P[0:2] / (P[2] + EPSILON)
 
-    Z = P[:, [2]]
+    # copy since numba doesn't support reshape of non-contiguous array
+    z = np.copy(P[:, 2])
+    Z = z.reshape(z.shape[0], 1)
+
     XY = P[:, 0:2]
+
     return XY / (Z + EPSILON)
 
 
+@njit
 def inv_pi(xs, depths):
+    if xs.ndim == 1:
+        return depths * to_homogeneous(xs)
     return depths.reshape(-1, 1) * to_homogeneous(xs)
 
 
