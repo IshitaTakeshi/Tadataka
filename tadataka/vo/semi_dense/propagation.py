1f43f0d738a1ad4cf78048921a095ee69d844756
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-16 21:41:22 +0900

    Make propagation much simpler

--- tadataka/vo/semi_dense/propagation.py
+++ tadataka/vo/semi_dense/propagation.py
@@ -10,7 +10,6 @@ def propagate_variance(inv_depths0, inv_depths1, variances0, uncertaintity):
     # we assume few rotation change between timestamp 0 and timestamp 1
     # TODO accept the case rotations significantly change between 0 and 1
     ratio = inv_depths1 / inv_depths0
-    print("max ratio", np.max(ratio))
     variances1 = np.power(ratio, 4) * variances0 + uncertaintity
     return variances1
 
@@ -22,42 +21,19 @@ def substitute(array2d, us, values):
     return array2d
 
 
-class Propagation(object):
-    def __init__(self, warp2d, uncertaintity=0.01,
-                 initial_inv_depth=1.0, initial_variance=1.0):
-        self.warp2d = warp2d
-        self.uncertaintity = uncertaintity
-        self.initial_inv_depth = initial_inv_depth
-        self.initial_variance = initial_variance
+def get(array2d, us):
+    xs, ys = us[:, 0], us[:, 1]
+    return array2d[ys, xs]
 
-    def _warp(self, us0, inv_depths0):
-        depths0 = invert_depth(inv_depths0)
-        us1, depths1 = self.warp2d(us0, depths0)
-        inv_depths1 = invert_depth(depths1)
-        return np.round(us1).astype(np.int64), inv_depths1
 
-    def __call__(self, inv_depth_map0, variance_map0):
-        image_shape = inv_depth_map0.shape
+def coordinates(warp01, depth_map0):
+    image_shape = depth_map0.shape
 
-        us0 = image_coordinates(image_shape)
-        variances0 = variance_map0.flatten()
-        inv_depths0 = inv_depth_map0.flatten()
+    us0 = image_coordinates(image_shape)
+    depths0 = get(depth_map0, us0)
 
-        us1, inv_depths1 = self._warp(us0, inv_depths0)
+    us1, depths1 = warp01(us0, depths0)
+    us1 = np.round(us1).astype(np.int64)
 
-        mask = is_in_image_range(us1, image_shape)
-        us1 = us1[mask]
-        inv_depths0 = inv_depths0[mask]
-        inv_depths1 = inv_depths1[mask]
-        variances0 = variances0[mask]
-
-        variances1 = propagate_variance(inv_depths0, inv_depths1,
-                                        variances0, self.uncertaintity)
-
-        inv_depth_map1 = self.initial_inv_depth * np.ones(image_shape)
-        inv_depth_map1 = substitute(inv_depth_map1, us1, inv_depths1)
-
-        variance_map1 = self.initial_variance * np.ones(image_shape)
-        variance_map1 = substitute(variance_map1, us1, variances1)
-
-        return inv_depth_map1, variance_map1
+    mask = is_in_image_range(us1, image_shape)
+    return us0[mask], us1[mask], depths0[mask], depths1[mask]
