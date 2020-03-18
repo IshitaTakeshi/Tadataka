ce137e94a2417c54a41afdfdfcc611285599f718
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-18 13:55:34 +0900

    Make depth estimation iterate over frames

--- examples/semi_dense_depth.py
+++ examples/semi_dense_depth.py
@@ -2,6 +2,7 @@ import numpy as np
 from skimage.color import rgb2gray, gray2rgb
 
 from tqdm import tqdm
+from tadataka.camera import CameraModel
 from tadataka.coordinates import image_coordinates
 from tadataka.dataset import TumRgbdDataset
 from tadataka.vo.semi_dense import SemiDenseVO
@@ -11,40 +12,61 @@ from tadataka.matrix import to_homogeneous
 from tadataka.rigid_transform import transform
 from tadataka.vo.semi_dense.flag import ResultFlag as FLAG
 from tadataka.vo.semi_dense.fusion import fusion
+from tadataka.vo.semi_dense.propagation import propagate
 from tests.dataset.path import new_tsukuba
 
-from examples.plot import plot_depth
+from examples.plot import plot_depth, plot_prior
 from matplotlib import pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-
-dataset = TumRgbdDataset(
-    "datasets/rgbd_dataset_freiburg1_desk",
-    which_freiburg=1
-)
-
 from tadataka.vo.semi_dense.semi_dense import InverseDepthMapEstimator
 
-kf = dataset[0]
-rf = dataset[5]
 
-keyframe = Frame(kf.camera_model, rgb2gray(kf.image), kf.pose)
-refframe = Frame(rf.camera_model, rgb2gray(rf.image), rf.pose)
-
-estimator = InverseDepthMapEstimator(
-    keyframe,
-    sigma_i=0.01, sigma_l=0.02,
-    step_size_ref=0.005, min_gradient=10.0
-)
-image_shape = keyframe.image.shape
-
-prior_inv_depth_map = np.random.uniform(0.8, 1.2, size=kf.depth_map.shape)
-prior_variance_map = 1.0 * np.ones(image_shape)
-
-inv_depth_map, variance_map, flag_map = estimator(
-    refframe, prior_inv_depth_map, prior_variance_map
-)
+def gray_frame(frame):
+    return Frame(frame.camera_model, rgb2gray(frame.image), frame.pose)
 
 
-plot_depth(kf.image, rf.image, flag_map,
-           kf.depth_map, invert_depth(inv_depth_map), variance_map)
+def update(keyframe, refframe, prior_inv_depth_map, prior_variance_map):
+    estimator = InverseDepthMapEstimator(
+        keyframe,
+        sigma_i=0.01, sigma_l=0.02,
+        step_size_ref=0.01, min_gradient=20.0
+    )
+
+    inv_depth_map, variance_map, flag_map = estimator(
+        refframe, prior_inv_depth_map, prior_variance_map
+    )
+
+    inv_depth_map, variance_map = fusion(prior_inv_depth_map, inv_depth_map,
+                                         prior_variance_map, variance_map)
+
+    return inv_depth_map, variance_map, flag_map
+
+
+dataset = TumRgbdDataset("datasets/rgbd_dataset_freiburg1_xyz",
+                         which_freiburg=1)
+
+frames = dataset[490:]
+
+inv_depth_map = np.random.uniform(0.8, 1.2, size=frames[0].depth_map.shape)
+# inv_depth_map = invert_depth(frames[0].depth_map)
+variance_map = 10.0 * np.ones(frames[0].depth_map.shape)
+
+for i in range(len(frames)):
+    kf0_ = frames[i]
+    kf1_ = frames[i+1]
+    keyframe0 = gray_frame(kf0_)
+    keyframe1 = gray_frame(kf1_)
+    refframe = gray_frame(frames[i+12])
+
+    plot_prior(keyframe0.image, kf0_.depth_map,
+               invert_depth(inv_depth_map), variance_map)
+
+    inv_depth_map, variance_map, flag_map = update(
+        keyframe0, refframe, inv_depth_map, variance_map
+    )
+
+    plot_depth(keyframe0.image, refframe.image, flag_map,
+               kf0_.depth_map, invert_depth(inv_depth_map), variance_map)
+
+    inv_depth_map, variance_map = propagate(keyframe0, keyframe1,
+                                            inv_depth_map, variance_map)
