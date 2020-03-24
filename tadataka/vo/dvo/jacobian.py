Fix a mistake in Jacobian 3D points in time 0 is not necessary

tadataka/vo/dvo/jacobian.py
@@ -5,50 +5,22 @@ import numpy as np
 # Master's Thesis, Technical University (2012).
 
 
-def calc_projection_jacobian(focal_lengths, P):
-    fx, fy = focal_lengths
-
-    x, y, z = P[:, 0], P[:, 1], P[:, 2]
-
-    z_squared = np.power(z, 2)
-    a = x * y / z_squared
-
-    JW = np.empty((P.shape[0], 2, 6))
-
-    JW[:, 0, 0] = +fx / z
-    JW[:, 0, 1] = 0
-    JW[:, 0, 2] = -fx * x / z_squared
-    JW[:, 0, 3] = -fx * a
-    JW[:, 0, 4] = +fx * (1 + x * x / z_squared)
-    JW[:, 0, 5] = -fx * y / z
-
-    JW[:, 1, 0] = 0
-    JW[:, 1, 1] = +fy / z
-    JW[:, 1, 2] = -fy * y / z_squared
-    JW[:, 1, 3] = -fy * (1 + y * y / z_squared)
-    JW[:, 1, 4] = +fy * a
-    JW[:, 1, 5] = +fy * x / z
-
-    return JW
-
-
-def calc_jacobian(focal_length, didx, didy, P0, P1):
+def calc_jacobian(focal_length, didx, didy, P):
     fx, fy = focal_length
     fgx, fgy = fx * didx, fy * didy
 
-    x0, y0, z0 = P0[:, 0], P0[:, 1], P0[:, 2]
-    x1, y1, z1 = P1[:, 0], P1[:, 1], P1[:, 2]
+    x, y, z = P[:, 0], P[:, 1], P[:, 2]
 
-    z12 = z1 * z1
-    x1y1 = x1 * y1
+    z2 = z * z
+    xy = x * y
 
-    J = np.empty((P0.shape[0], 6))
-    J[:, 0] = fgx / z0
-    J[:, 1] = fgy / z0
-    J[:, 2] = -(fgx * x0 + fgy * y0) / (z0 * z0)
-    J[:, 3] = -(fgx * x1y1 + fgy * (z12 + y1 * y1)) / z12
-    J[:, 4] = (fgx * (z12 + x1 * x1) + fgy * x1y1) / z12
-    J[:, 5] = (-fgx * y1 + fgy * x1) / z1
+    J = np.empty((P.shape[0], 6))
+    J[:, 0] = fgx / z
+    J[:, 1] = fgy / z
+    J[:, 2] = -(fgx * x + fgy * y) / (z * z)
+    J[:, 3] = -(fgx * xy + fgy * (z2 + y * y)) / z2
+    J[:, 4] = (fgx * (z2 + x * x) + fgy * xy) / z2
+    J[:, 5] = (-fgx * y + fgy * x) / z
     return J
 
 

tests/vo/dvo/test_jacobian.py
@@ -3,66 +3,36 @@ from numpy.testing import (assert_array_equal, assert_array_almost_equal,
                            assert_almost_equal)
 
 from tadataka.camera import CameraParameters
-from tadataka.vo.dvo.jacobian import (calc_projection_jacobian,
-                                      calc_image_gradient,
-                                      calc_jacobian)
-
-
-def test_calc_projection_jacobian():
-    camera_parameters = CameraParameters(
-        focal_length=[3, 2],
-        offset=[0, 0]
-    )
-
-    P = np.array([
-        [1, 6, 2],
-        [8, 5, 3],
-    ])
-
-    J = calc_projection_jacobian(camera_parameters, P)
-
-    GT = np.array([
-        # x' = 1, y' = 6, z' = 2, fx = 3, fy = 2
-        [[3 / 2, 0, -3 * 1 / 4, -3 * 1 * 6 / 4, 3 * (1 + 1 / 4), -3 * 6 / 2],
-         [0, 2 / 2, -2 * 6 / 4, -2 * (1 + 36 / 4), 2 * 1 * 6 / 4, 2 * 1 / 2]],
-        # x' = 8, y' = 5, z' = 3, fx = 3, fy = 2
-        [[3 / 3, 0, -3 * 8 / 9, -3 * 8 * 5 / 9, 3 * (1 + 64 / 9), -3 * 5 / 3],
-         [0, 2 / 3, -2 * 5 / 9, -2 * (1 + 25 / 9), 2 * 8 * 5 / 9, 2 * 8 / 3]]
-    ])
-
-    assert_array_equal(J, GT)
+from tadataka.vo.dvo.jacobian import calc_image_gradient, calc_jacobian
 
 
 def test_calc_jacobian():
-    def run(j, gx, gy, p0, p1):
-        x0, y0, z0 = p0
-        x1, y1, z1 = p1
-        assert_almost_equal(j[0], gx * (fx / z0))
-        assert_almost_equal(j[1], gy * (fy / z0))
+    def run(j, gx, gy, p1):
+        x, y, z = p1
+        assert_almost_equal(j[0], gx * (fx / z))
+        assert_almost_equal(j[1], gy * (fy / z))
         assert_almost_equal(j[2],
-                            (gx * (-fx * x0 / (z0 * z0)) +
-                             gy * (-fy * y0 / (z0 * z0))))
+                            (gx * (-fx * x / (z * z)) +
+                             gy * (-fy * y / (z * z))))
         assert_almost_equal(j[3],
-                            (gx * (-fx * x1 * y1 / (z1 * z1)) +
-                             gy * (-fy * (1 + (y1 * y1) / (z1 * z1)))))
+                            (gx * (-fx * x * y / (z * z)) +
+                             gy * (-fy * (1 + (y * y) / (z * z)))))
         assert_almost_equal(j[4],
-                            (gx * (fx * (1 + (x1 * x1) / (z1 * z1))) +
-                             gy * (fy * x1 * y1 / (z1 * z1))))
+                            (gx * (fx * (1 + (x * x) / (z * z))) +
+                             gy * (fy * x * y / (z * z))))
         assert_almost_equal(j[5],
-                            gx * (-fx * y1 / z1) + gy * (fy * x1 / z1))
+                            gx * (-fx * y / z) + gy * (fy * x / z))
 
-    P0 = np.array([[2, 4, -3],
-                   [-4, 2, 3]])
-    P1 = np.array([[3, -1, 2],
-                   [2, -1, 5]])
+    P = np.array([[3, -1, 2],
+                  [2, -1, 5]])
     didx = np.array([10, -20])
     didy = np.array([-20, 10])
 
     fx, fy = 300, 400
 
-    J = calc_jacobian([fx, fy], didx, didy, P0, P1)
+    J = calc_jacobian([fx, fy], didx, didy, P)
 
-    assert J.shape == (P0.shape[0], 6)
+    assert J.shape == (P.shape[0], 6)
 
-    for i in range(P0.shape[0]):
-        run(J[i], didx[i], didy[i], P0[i], P1[i])
+    for i in range(P.shape[0]):
+        run(J[i], didx[i], didy[i], P[i])