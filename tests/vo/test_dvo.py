0f43b328cf364ea29bf1a5ca6ff63aed65559ec3
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-25 20:03:25 +0900

    Add test of pose change estimation at the finest resolution

--- tests/vo/test_dvo.py
+++ tests/vo/test_dvo.py
@@ -6,12 +6,12 @@ from tadataka.pose import WorldPose
 from tadataka.dataset.new_tsukuba import NewTsukubaDataset
 from tadataka.warp import Warp2D
 from tadataka.metric import photometric_error
-from tadataka.vo.dvo import _PoseChangeEstimator
+from tadataka.vo.dvo import _PoseChangeEstimator, PoseChangeEstimator
 from tadataka.vo.dvo import camera_model_at, image_shape_at
 from tests.dataset.path import new_tsukuba
 
 
-def test_calc_pose_update():
+def test_subestimator():
     level = 6
     dataset = NewTsukubaDataset(new_tsukuba)
     frame0, frame1 = dataset[0][0], dataset[4][1]
@@ -36,3 +36,29 @@ def test_calc_pose_update():
     warp = Warp2D(camera_model0, camera_model1, dpose, WorldPose.identity())
     error_pred = photometric_error(warp, I0, D0, I1)
     assert(error_pred < error_prior * 0.1)
+
+
+def test_pose_change_estimator():
+    dataset = NewTsukubaDataset(new_tsukuba)
+    frame0, frame1 = dataset[0][1], dataset[4][0]
+
+    camera_model0 = frame0.camera_model
+    camera_model1 = frame1.camera_model
+
+    D0 = frame0.depth_map
+    I0 = rgb2gray(frame0.image)
+    I1 = rgb2gray(frame1.image)
+
+    estimator = PoseChangeEstimator(camera_model0, camera_model1,
+                                    n_coarse_to_fine=6, max_iter=10)
+
+    prior = WorldPose.identity()
+    warp = Warp2D(camera_model0, camera_model1, prior, WorldPose.identity())
+    error_prior = photometric_error(warp, I0, D0, I1)
+
+    weight_map = np.ones(D0.shape)
+    dpose = estimator(I0, D0, I1, weight_map, prior)
+
+    warp = Warp2D(camera_model0, camera_model1, dpose, WorldPose.identity())
+    error_pred = photometric_error(warp, I0, D0, I1)
+    assert(error_pred < error_prior * 0.12)
