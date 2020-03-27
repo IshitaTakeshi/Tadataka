c4bcce8ff77e3c4c804a036bdea45e2668ad9712
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-28 00:09:23 +0900

    Use T = (R, t) for pose representation in 'increment_age'

--- tadataka/vo/semi_dense/age.py
+++ tadataka/vo/semi_dense/age.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+from tadataka.warp import warp2d
 from tadataka.utils import is_in_image_range
 from tadataka.coordinates import image_coordinates
+from tadataka.vo.semi_dense.common import invert_depth
 
 
 def increment_(age0, us0, us1):
@@ -13,11 +15,20 @@ def increment_(age0, us0, us1):
     return age1
 
 
-def increment_age(age0, depth_map0, warp01):
-    assert(age0.shape == depth_map0.shape)
+def increment_age(age0, inv_depth_map0,
+                  camera_model0, camera_model1, T0, T1):
+    # T0 = (R0, t0), T1 = (R1, t1)
+    assert(age0.shape == inv_depth_map0.shape)
+
+    depth_map0 = invert_depth(inv_depth_map0)
     image_shape = age0.shape
     depths0 = depth_map0.flatten()
+
     us0 = image_coordinates(image_shape)
-    us1 = np.round(warp01(us0, depths0)).astype(np.int64)
+    xs0 = camera_model0.normalize(us0)
+    xs1 = warp2d(T0, T1, xs0, depths0)
+    us1 = camera_model1.unnormalize(xs1)
+
+    us1 = np.round(us1).astype(np.int64)
     mask = is_in_image_range(us1, image_shape)
     return increment_(age0, us0[mask], us1[mask])
