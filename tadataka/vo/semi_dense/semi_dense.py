9102b06b67d1a4d517e223aaf53f2c0c99be638a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-27 17:14:51 +0900

    Return if estimated depth is negative

--- tadataka/vo/semi_dense/semi_dense.py
+++ tadataka/vo/semi_dense/semi_dense.py
@@ -209,8 +209,10 @@ class InverseDepthEstimator(object):
         image_grad = self.image_grad(u_key)
         variance = self.uncertaintity(x_key, x_ref, x_range_ref, R, t,
                                       image_grad, epipolar_gradient)
+        if depth_key < 0:
+            return (prior_inv_depth, prior_variance,
+                    FLAG.NEGATIVE_ESTIMATED_DEPTH)
         return invert_depth(depth_key), variance, FLAG.SUCCESS
-
 
 
 class InverseDepthMapEstimator(object):
