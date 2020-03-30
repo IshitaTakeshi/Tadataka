051448d75351776eacccbd6b4e6309de6b261e42
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-03-30 22:31:34 +0900

    Preserve edges in regularization

--- tadataka/vo/semi_dense/regularization.py
+++ tadataka/vo/semi_dense/regularization.py
@@ -1,10 +1,43 @@
 import numpy as np
 from scipy.signal import convolve2d
 from tadataka.vo.semi_dense.common import invert_depth
+from numba import njit
 
 
-def regularize(inv_depth_map, variance_map):
+def are_statically_same(value1, value2, variance, factor):
+    # equivalent_condition:
+    #   np.abs(value1-value2) <= factor * np.sqrt(variance1)
+    ds = (value1 - value2) * (value1 - value2)
+    fs = factor * factor
+    return ds <= fs * variance
+
+
+def create_mask_(inv_depth_map, variance):
+    height, width = inv_depth_map.shape
+
+    cy, cx = height // 2, width // 2
+    mask = np.empty((height, width), dtype=np.int64)
+    for y in range(height):
+        for x in range(width):
+            mask[y, x] = are_statically_same(
+                inv_depth_map[cy, cx], inv_depth_map[y, x],
+                variance, factor=2.0
+            )
+    return mask
+
+
+def regularize(inv_depth_map, variance_map, conv_size=3):
+    assert(inv_depth_map.shape == variance_map.shape)
+    height, width = inv_depth_map.shape
     weight_map = invert_depth(variance_map)
-    R = convolve2d(inv_depth_map * weight_map, np.ones((3, 3)), mode="valid")
-    W = convolve2d(weight_map, np.ones((3, 3)), mode="valid")
-    return R / W
+    offset = conv_size // 2
+
+    regularized = np.empty((height-conv_size+1, width-conv_size+1))
+    for y in range(regularized.shape[0]):
+        for x in range(regularized.shape[1]):
+            D = inv_depth_map[y:y+conv_size, x:x+conv_size]
+            W = weight_map[y:y+conv_size, x:x+conv_size]
+            mask = create_mask_(D, variance_map[y+offset, x+offset])
+            M = W * mask.astype(np.float64)
+            regularized[y, x] = (M * D).sum() / M.sum()
+    return regularized
--- tests/vo/semi_dense/test_regularization.py
+++ tests/vo/semi_dense/test_regularization.py
@@ -1,34 +1,70 @@
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_array_equal
 import numpy as np
 
-from tadataka.vo.semi_dense.regularization import regularize
+from tadataka.vo.semi_dense.regularization import (
+    regularize, are_statically_same, create_mask_
+)
+
+
+def test_are_statically_same():
+    def equivalent_condition(v1, v2, var, fac):
+        return np.abs(v1-v2) <= fac * np.sqrt(var)
+
+    value1, value2, variance, factor = 1.0, 3.0, 4.0, 2.0
+    c = equivalent_condition(value1, value2, variance, factor)
+    assert(c == are_statically_same(value1, value2, variance, factor))
+
+    value1, value2, variance, factor = -1.0, 2.9, 4.0, 2.0
+    c = equivalent_condition(value1, value2, variance, factor)
+    assert(c == are_statically_same(value1, value2, variance, factor))
+
+    value1, value2, variance, factor = 1.0, 3.0, 4.0, 1.0
+    c = equivalent_condition(value1, value2, variance, factor)
+    assert(c == are_statically_same(value1, value2, variance, factor))
+
+    variance = 2.0
+    inv_depth_map = np.array([
+        [2, -1, 0],
+        [-3, 1, 3],
+        [4, -2, -4]
+    ])
+    mask = create_mask_(inv_depth_map, variance)
+    assert_array_equal(
+        mask,
+        [[1, 1, 1],
+         [0, 1, 1],
+         [0, 0, 0]]
+    )
 
 
 def test_regularization():
-    def regularize_(inv_depth_map_, variance_map_):
+    def regularize_(D, V):
         s = 0.0
         d = 0.0
         for y in range(3):
             for x in range(3):
-                s += inv_depth_map_[y, x] / variance_map_[y, x]
-                d += 1.0 / variance_map_[y, x]
+                if not are_statically_same(D[1, 1], D[y, x], V[1, 1], 2.0):
+                    continue
+                s += D[y, x] / V[y, x]
+                d += 1.0 / V[y, x]
         return s / d
 
 
     inv_depth_map = np.array([
-        [4, 2, 4, 3],
-        [2, 1, 5, 2],
-        [3, 4, 1, 2],
-        [4, 1, 2, 3]
+        [4, 2, 4, 1],
+        [2, 1, 5, 4],
+        [3, 4, 1, 3],
+        [5, 9, 1, 2]
     ])
     variance_map = np.array([
-        [3, 2, 2, 1],
-        [4, 9, 3, 5],
-        [3, 2, 8, 2],
-        [7, 5, 3, 7]
+        [3, 2, 2, 3],
+        [4, 9, 3, 4],
+        [3, 2, 8, 1],
+        [2, 4, 1, 3]
     ])
 
-    R = regularize(inv_depth_map, variance_map)
+    R = regularize(inv_depth_map, variance_map, conv_size=3)
+    assert(R.shape == (2, 2))
 
     assert_almost_equal(
         R[0, 0],
