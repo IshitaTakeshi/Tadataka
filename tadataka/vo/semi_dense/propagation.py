74579424a4403567e3cefbcd6f9975eabbd0afc5
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-01 22:54:31 +0900

    Add functions to detect significant intensity change on propagation

--- tadataka/vo/semi_dense/propagation.py
+++ tadataka/vo/semi_dense/propagation.py
@@ -31,6 +31,11 @@ def coordinates(warp10, depth_map0):
     us0 = image_coordinates(depth_map0.shape)
     depths0 = depth_map0.flatten()
     us1, depths1 = warp10(us0, depths0)
+
+    mask = is_in_image_range(us1, depth_map0.shape)
+    us0, depths0 = us0[mask], depths0[mask]
+    us1, depths1 = us1[mask], depths1[mask]
+
     return us0, us1, depths0, depths1
 
 
@@ -42,11 +47,7 @@ def propagate_(warp10, depth_map0, variance_map0, uncertaintity_bias=0.01):
 
     us1 = np.round(us1).astype(np.int64)
 
-    mask = is_in_image_range(us1, depth_map0.shape)
-    us0, depths0 = us0[mask], depths0[mask]
-    us1, depths1 = us1[mask], depths1[mask]
-
-    variances0 = variance_map0[us0[:, 1], us0[:, 0]]
+    variances0 = get(variance_map0, us0)
     variances1 = propagate_variance(invert_depth(depths0),
                                     invert_depth(depths1),
                                     variances0, uncertaintity_bias)
@@ -56,11 +57,29 @@ def propagate_(warp10, depth_map0, variance_map0, uncertaintity_bias=0.01):
     return depth_map1, variance_map1
 
 
-def propagate(camera_model0, camera_model1, pose10,
+def propagate(camera_model0, camera_model1, warp10,
               inv_depth_map0, variance_map0):
     depth_map0 = invert_depth(inv_depth_map0)
 
-    warp10 = Warp2D(camera_model0, camera_model1, pose10, WorldPose.identity())
     depth_map1, variance_map1 = propagate_(warp10, depth_map0, variance_map0)
 
     return invert_depth(depth_map1), variance_map1
+
+
+def detect_intensity_change_(warp10, image0, image1, depth_map0,
+                             threshold):
+    from tadataka.interpolation import interpolation
+    us0, us1, depths0, depths1 = coordinates(warp10, depth_map0)
+
+    intensities0 = get(image0, us0)
+    intensities1 = interpolation(image1, us1)
+    mask = (intensities0 - intensities1) > threshold
+    return substitute(np.zeros(image0.shape, dtype=np.bool), us0, mask)
+
+
+def detect_intensity_change(warp10, image0, image1, inv_depth_map0,
+                            threshold=0.2):
+    assert(image0.dtype == np.float64)
+    assert(image1.dtype == np.float64)
+    return detect_intensity_change_(warp10, image0, image1,
+                                    invert_depth(inv_depth_map0), threshold)
