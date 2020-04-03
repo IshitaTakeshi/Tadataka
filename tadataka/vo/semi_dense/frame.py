0929111674f919463cd6511e08908e8b85f98ac9
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-04 00:55:00 +0900

    Represent SemiDenseVO's 'Frame' in class instead of namedtuple

--- tadataka/vo/semi_dense/frame.py
+++ tadataka/vo/semi_dense/frame.py
@@ -1,4 +1,12 @@
-from collections import namedtuple
+from skimage.color import rgb2gray
+from tadataka.camera import CameraModel
 
 
-Frame = namedtuple("Frame", ["camera_model", "image", "R", "t"])
+class Frame(object):
+    def __init__(self, frame):
+        self.camera_model = CameraModel(
+            frame.camera_model.camera_parameters,
+            distortion_model=None
+        )
+        self.image = rgb2gray(frame.image)
+        self.R, self.t = frame.pose.R, frame.pose.t
