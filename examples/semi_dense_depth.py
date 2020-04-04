2211d6e0fcd6cbc6db300b1c14ebbaa01796e1ec
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-04 18:42:31 +0900

    Update the VO runner

--- examples/semi_dense_depth.py
+++ examples/semi_dense_depth.py
@@ -1,19 +1,23 @@
 import numpy as np
 from skimage.color import rgb2gray, gray2rgb
+from skimage.transform import resize
 
 from tqdm import tqdm
 from tadataka.camera import CameraModel
+from tadataka.rigid_motion import LeastSquaresRigidMotion
 from tadataka.coordinates import image_coordinates
 from tadataka.dataset import TumRgbdDataset
 from tadataka.vo.semi_dense.frame import Frame
+from tadataka.vo.semi_dense.frame_selection import ReferenceFrameSelector
 from tadataka.vo.semi_dense.common import invert_depth
-from tadataka.pose import WorldPose
-from tadataka.vo.dvo import PoseChangeEstimator
-from tadataka.matrix import to_homogeneous
-from tadataka.rigid_transform import transform
 from tadataka.vo.semi_dense.flag import ResultFlag as FLAG
 from tadataka.vo.semi_dense.fusion import fusion
 from tadataka.vo.semi_dense.propagation import propagate
+from tadataka.pose import WorldPose
+from tadataka.vo.dvo import PoseChangeEstimator
+from tadataka.matrix import to_homogeneous
+from tadataka.warp import Warp2D
+from tadataka.rigid_transform import transform, Transform
 from tadataka.dataset import NewTsukubaDataset
 from tests.dataset.path import new_tsukuba
 
@@ -24,11 +28,7 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from tadataka.vo.semi_dense.semi_dense import InverseDepthMapEstimator
 
 
-def gray_frame(frame):
-    return Frame(frame.camera_model, rgb2gray(frame.image), frame.pose)
-
-
-def update(keyframe, refframe, prior_inv_depth_map, prior_variance_map):
+def update(keyframe, ref_selector, prior_inv_depth_map, prior_variance_map):
     estimator = InverseDepthMapEstimator(
         keyframe,
         sigma_i=0.01, sigma_l=0.02,
@@ -36,7 +36,7 @@ def update(keyframe, refframe, prior_inv_depth_map, prior_variance_map):
     )
 
     inv_depth_map, variance_map, flag_map = estimator(
-        refframe, prior_inv_depth_map, prior_variance_map
+        ref_selector, prior_inv_depth_map, prior_variance_map
     )
 
     inv_depth_map, variance_map = fusion(prior_inv_depth_map, inv_depth_map,
@@ -45,74 +45,83 @@ def update(keyframe, refframe, prior_inv_depth_map, prior_variance_map):
     return inv_depth_map, variance_map, flag_map
 
 
-# dataset = TumRgbdDataset("datasets/rgbd_dataset_freiburg1_xyz",
-#                          which_freiburg=1)
-
-dataset = NewTsukubaDataset("datasets/NewTsukubaStereoDataset")
-frames = dataset[:200]
-
-inv_depth_map = invert_depth(frames[0][0].depth_map)
-variance_map = 10.0 * np.ones(frames[0][0].depth_map.shape)
-
-EPSILON = 1e-16
-
-trajectory_true = []
-trajectory_pred = []
-pose_pred = WorldPose.identity()
-pose_true = WorldPose.identity()
-offset = 4
-for i in range(len(frames)-offset):
-    kf0_ = frames[i][0]
-    kf1_ = frames[i+1][0]
-    keyframe0 = gray_frame(kf0_)
-    keyframe1 = gray_frame(kf1_)
-    refframe = gray_frame(frames[i+offset][0])
-
-    # plot_prior(keyframe0.image, kf0_.depth_map,
-    #            invert_depth(inv_depth_map), variance_map)
-
-    inv_depth_map, variance_map, flag_map = update(
-        keyframe0, refframe, inv_depth_map, variance_map
+def resize_camera_model(cm, ratio):
+    from tadataka.camera import CameraParameters
+    return CameraModel(
+        CameraParameters(cm.camera_parameters.focal_length * ratio,
+                         cm.camera_parameters.offset * ratio),
+        cm.distortion_model
     )
 
-    # plot_depth(keyframe0.image, refframe.image, flag_map,
-    #            kf0_.depth_map, invert_depth(inv_depth_map), variance_map)
 
-    estimate_pose = PoseChangeEstimator(
-        keyframe0.camera_model, keyframe1.camera_model
-    )
+def estimate_pose_change_(cm0, cm1, I0, D0, I1, W, ratio=1/6):
+    estimator = PoseChangeEstimator(resize_camera_model(cm0, ratio),
+                                    resize_camera_model(cm1, ratio))
 
-    depth_map0 = invert_depth(inv_depth_map)
-    pose10_pred = estimate_pose(keyframe0.image, depth_map0,
-                                keyframe1.image,
-                                weight_map=1/(variance_map+EPSILON))
+    shape = (int(I0.shape[0] * ratio), int(I0.shape[1] * ratio))
+    return estimator(resize(I0, shape), resize(D0, shape),
+                     resize(I1, shape), resize(W, shape))
 
-    pose_pred = pose10_pred * pose_pred
 
-    inv_depth_map, variance_map = propagate(keyframe0.camera_model,
-                                            keyframe1.camera_model,
-                                            pose10_pred, inv_depth_map,
-                                            variance_map)
+def estimate_pose_change(frame0, frame1, inv_depth_map, variance_map):
+    return estimate_pose_change_(frame0.camera_model, frame1.camera_model,
+                                 frame0.image, invert_depth(inv_depth_map),
+                                 frame1.image, invert_depth(variance_map))
 
-    pose10_true = keyframe1.pose.inv() * keyframe0.pose
-    pose_true = pose10_true * pose_true
 
-    print("pred", pose10_pred)
-    print("true", pose10_true)
+def main():
+    dataset = TumRgbdDataset("datasets/rgbd_dataset_freiburg1_desk",
+                             which_freiburg=1)
 
-    trajectory_pred.append(pose_pred.t)
-    trajectory_true.append(pose_true.t)
+    frames = dataset[200:220]
 
-trajectory_true = np.array(trajectory_true)
-trajectory_pred = np.array(trajectory_pred)
-print(trajectory_true)
-print(trajectory_pred)
+    inv_depth_map = invert_depth(frames[0].depth_map)
+    variance_map = 10.0 * np.ones(frames[0].depth_map.shape)
+    ref_selector = ReferenceFrameSelector(Frame(frames[0]), inv_depth_map)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection="3d")
-ax.plot(trajectory_pred[:, 0], trajectory_pred[:, 1], trajectory_pred[:, 2],
-        label="pred")
-ax.plot(trajectory_true[:, 0], trajectory_true[:, 1], trajectory_true[:, 2],
-        label="true")
-plt.legend()
-plt.show()
+    trajectory_true = []
+    trajectory_pred = []
+    pose_pred = WorldPose.identity()
+    pose_true = WorldPose.identity()
+    for i in range(len(frames)-1):
+        frame0_ = frames[i+0]
+        frame1_ = frames[i+1]
+        frame0 = Frame(frame0_)
+        frame1 = Frame(frame1_)
+
+        inv_depth_map, variance_map, flag_map = update(
+            frame0, ref_selector, inv_depth_map, variance_map
+        )
+
+        pose10_pred = estimate_pose_change(frame0, frame1,
+                                           inv_depth_map, variance_map)
+
+        pose_pred = pose10_pred * pose_pred
+
+        warp10 = Warp2D(frame0.camera_model, frame0.camera_model,
+                        pose10_pred, WorldPose.identity())
+        inv_depth_map, variance_map = propagate(warp10,
+                                                inv_depth_map, variance_map)
+
+        pose10_true = frame1_.pose.inv() * frame0_.pose
+        pose_true = pose10_true * pose_true
+
+        trajectory_pred.append(pose_pred.t)
+        trajectory_true.append(pose_true.t)
+
+    trajectory_true = np.array(trajectory_true)
+    trajectory_pred = np.array(trajectory_pred)
+    R, t, s = LeastSquaresRigidMotion(trajectory_pred, trajectory_true).solve()
+    trajectory_pred = Transform(R, t, s)(trajectory_pred)
+    print("MSE: ", np.power(trajectory_pred - trajectory_true, 2).mean())
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection="3d")
+    # ax.plot(trajectory_pred[:, 0], trajectory_pred[:, 1], trajectory_pred[:, 2],
+    #         label="pred")
+    # ax.plot(trajectory_true[:, 0], trajectory_true[:, 1], trajectory_true[:, 2],
+    #         label="true")
+    # plt.legend()
+    # plt.show()
+
+main()
