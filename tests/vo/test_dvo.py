def472c8f3386e6b246e1c240e301feaa2048b17
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-07 01:37:17 +0900

    Modify tests as the photometric error takes local pose as an argument

--- tests/vo/test_dvo.py
+++ tests/vo/test_dvo.py
@@ -2,7 +2,7 @@ import numpy as np
 from skimage.color import rgb2gray
 from skimage.transform import resize
 
-from tadataka.pose import WorldPose
+from tadataka.pose import WorldPose, LocalPose
 from tadataka.dataset.new_tsukuba import NewTsukubaDataset
 from tadataka.warp import Warp2D
 from tadataka.metric import photometric_error
@@ -26,15 +26,15 @@ def test_subestimator():
 
     estimator = _PoseChangeEstimator(camera_model0, camera_model1, max_iter=10)
 
-    prior = WorldPose.identity()
-    warp = Warp2D(camera_model0, camera_model1, prior, WorldPose.identity())
-    error_prior = photometric_error(warp, I0, D0, I1)
+    pose10_prior = LocalPose.identity()
+    error_prior = photometric_error(camera_model0, camera_model1,
+                                    pose10_prior, I0, D0, I1)
 
     weight_map = np.ones(shape)
-    dpose = estimator(I0, D0, I1, prior, weight_map)
+    pose10 = estimator(I0, D0, I1, pose10_prior, weight_map)
 
-    warp = Warp2D(camera_model0, camera_model1, dpose, WorldPose.identity())
-    error_pred = photometric_error(warp, I0, D0, I1)
+    error_pred = photometric_error(camera_model0, camera_model1,
+                                   pose10, I0, D0, I1)
     assert(error_pred < error_prior * 0.1)
 
 
@@ -52,13 +52,14 @@ def test_pose_change_estimator():
     estimator = PoseChangeEstimator(camera_model0, camera_model1,
                                     n_coarse_to_fine=6, max_iter=10)
 
-    prior = WorldPose.identity()
-    warp = Warp2D(camera_model0, camera_model1, prior, WorldPose.identity())
-    error_prior = photometric_error(warp, I0, D0, I1)
+    pose10_prior = WorldPose.identity()
+    error_prior = photometric_error(camera_model0, camera_model1, pose10_prior,
+                                    I0, D0, I1)
 
     weight_map = np.ones(D0.shape)
-    dpose = estimator(I0, D0, I1, weight_map, prior)
+    pose10 = estimator(I0, D0, I1, weight_map, pose10_prior)
 
-    warp = Warp2D(camera_model0, camera_model1, dpose, WorldPose.identity())
-    error_pred = photometric_error(warp, I0, D0, I1)
+    error_pred = photometric_error(camera_model0, camera_model1,
+                                   pose10.to_local(), I0, D0, I1)
+    print("error_pred, error_prior =", error_pred, error_prior)
     assert(error_pred < error_prior * 0.12)
