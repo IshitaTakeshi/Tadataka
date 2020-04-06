9dad16c4a0a0840d2d89799cc554abb8c73794bc
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-07 01:26:04 +0900

    Rewrite photometric error with local coordinate system

--- tadataka/metric.py
+++ tadataka/metric.py
@@ -1,17 +1,24 @@
 import numpy as np
 from tadataka.interpolation import interpolation2d_
+from tadataka.pose import LocalPose
+from tadataka.projection import inv_pi, pi
 from tadataka.coordinates import image_coordinates
 from tadataka.utils import is_in_image_range
-
+from tadataka.rigid_transform import transform
 
 def calc_error_(v1, v2):
     return np.mean(np.power(v1 - v2, 2))
 
 
-def photometric_error(warp2d, gray_image0, depth_map0, gray_image1):
+def photometric_error(camera_model0, camera_model1, pose10: LocalPose,
+                      gray_image0, depth_map0, gray_image1):
     us0 = image_coordinates(depth_map0.shape)
-    depths0 = depth_map0.flatten()
-    us1, depths1 = warp2d(us0, depths0)
+    xs0 = camera_model0.normalize(us0)
+    P0 = inv_pi(xs0, depth_map0.flatten())
+    P1 = transform(pose10.R, pose10.t, P0)
+    xs1 = pi(P1)
+    us1 = camera_model1.unnormalize(xs1)
+
     mask = is_in_image_range(us1, depth_map0.shape)
 
     i0 = gray_image0[us0[mask, 1], us0[mask, 0]]
