9eb5f32c3e1e07d69d68060c2a44d8aa2367df1b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-07 01:31:41 +0900

    Simplify reference frame selector

--- tadataka/vo/semi_dense/frame_selection.py
+++ tadataka/vo/semi_dense/frame_selection.py
@@ -1,25 +1,7 @@
-import numpy as np
-from tadataka.vo.semi_dense.age import increment_age
-
-
-class ReferenceFrameSelector(object):
-    def __init__(self, frame0, inv_depth_map0):
-        self.age_map = np.zeros(inv_depth_map0.shape, dtype=np.int64)
-        self.inv_depth_map0 = inv_depth_map0
-        self.frames = []
-        self.frame0 = frame0
-
-    def update(self, frame1, inv_depth_map1):
-        self.age_map = increment_age(
-            self.age_map, self.inv_depth_map0,
-            self.frame0.camera_model, frame1.camera_model,
-            (self.frame0.R, self.frame0.t), (frame1.R, frame1.t)
-        )
-
-        self.frames.append(self.frame0)
-
-        self.inv_depth_map0 = inv_depth_map1
-        self.frame0 = frame1
+class ReferenceSelector(object):
+    def __init__(self, frames, age_map):
+        self.frames = frames
+        self.age_map = age_map
 
     def __call__(self, u):
         x, y = u
