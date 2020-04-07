ee8c4b84f914ed744e578875ce54093f3475164e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-07 21:56:24 +0900

    Update the runner

--- examples/estimate_depth_known_pose.py
+++ examples/estimate_depth_known_pose.py
@@ -53,8 +53,8 @@ def plot_age_map(age_map):
 def update(keyframe, ref_selector, prior_inv_depth_map, prior_variance_map):
     estimator = InverseDepthMapEstimator(
         keyframe,
-        sigma_i=0.01, sigma_l=0.02,
-        step_size_ref=0.01, min_gradient=20.0
+        sigma_i=0.1, sigma_l=0.2,
+        step_size_ref=0.001, min_gradient=20.0
     )
 
     inv_depth_map, variance_map, flag_map = estimator(
@@ -76,26 +76,49 @@ def to_perspective(camera_model):
     )
 
 
+def create_frame(camera_model, image, pose):
+    return Frame(to_perspective(camera_model), rgb2gray(image), pose)
+
+
+def plot_refframes(refframes, keyframe, age_map):
+    from matplotlib import pyplot as plt
+    N = len(refframes) + 2
+
+    fig = plt.figure()
+    for i, frame in enumerate(refframes):
+        ax = fig.add_subplot(1, N, i+1)
+        ax.set_title(f"refframe {i}")
+        ax.imshow(frame.image, cmap="gray")
+
+    ax = fig.add_subplot(1, N, len(refframes)+1)
+    ax.set_title("keyframe")
+    ax.imshow(keyframe.image, cmap="gray")
+
+    ax = fig.add_subplot(1, N, len(refframes)+2)
+    ax.set_title("pixel age")
+    ax.imshow(age_map, cmap="gray")
+
+    plt.show()
+
+
 def main():
     dataset = TumRgbdDataset("datasets/rgbd_dataset_freiburg1_desk",
                              which_freiburg=1)
-    color_frames = dataset[300:310:3]
+    color_frames = dataset[100:130]
 
     frame0_ = color_frames[0]
+    frame0 = create_frame(frame0_.camera_model, frame0_.image, frame0_.pose)
 
-    frame0 = Frame(to_perspective(frame0_.camera_model),
-                   rgb2gray(frame0_.image), frame0_.pose)
     age_map0 = np.zeros(frame0.image.shape, dtype=np.int64)
-
-    inv_depth_map0 = invert_depth(frame0_.depth_map)
-    variance_map0 = 0.5 * np.ones(frame0.image.shape)
+    # inv_depth_map0 = invert_depth(frame0_.depth_map)
+    inv_depth_map0 = np.random.uniform(0.1, 10.0, frame0.image.shape)
+    variance_map0 = 10.0 * np.ones(frame0.image.shape)
 
     refframes = []
 
     for i in range(1, len(color_frames)):
         frame1_ = color_frames[i]
-        frame1 = Frame(to_perspective(frame1_.camera_model),
-                       rgb2gray(frame1_.image), frame1_.pose)
+        frame1 = create_frame(frame1_.camera_model, frame1_.image, frame1_.pose)
 
         warp10 = Warp2D(frame0.camera_model, frame0.camera_model,
                         frame0_.pose, frame1_.pose)
@@ -106,14 +129,16 @@ def main():
         age_map1 = increment_age(age_map0, warp10, inv_depth_map0)
 
         refframes.append(frame0)
+        # plot_refframes(refframes, frame1, age_map1)
+
         inv_depth_map1, variance_map1, flag_map1 = update(
             frame1, ReferenceSelector(refframes, age_map1),
             prior_inv_depth_map1, prior_variance_map1
         )
 
-        # plot_depth(frame1.image, age_map1,
-        #            flag_map1, frame1_.depth_map,
-        #            invert_depth(inv_depth_map1), variance_map1)
+        plot_depth(frame1.image, age_map1,
+                   flag_map1, frame1_.depth_map,
+                   invert_depth(inv_depth_map1), variance_map1)
 
         frame0 = frame1
         age_map0 = age_map1
