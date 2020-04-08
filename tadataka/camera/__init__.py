4b9afcd00105ddf694ca15f826b63d38f2e7e96a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-09 01:13:14 +0900

    Make 'resize' accessible from 'tadataka.camera'

--- tadataka/camera/__init__.py
+++ tadataka/camera/__init__.py
@@ -1,3 +1,3 @@
 from tadataka.camera.parameters import CameraParameters
-from tadataka.camera.model import CameraModel
+from tadataka.camera.model import CameraModel, resize
 from tadataka.camera.distortion import FOV, RadTan
