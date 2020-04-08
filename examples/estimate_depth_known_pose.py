2e89fd7cfc83eb7457f3960d5ea682cf61795255
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-09 01:05:16 +0900

    Update the runner

--- examples/estimate_depth_known_pose.py
+++ examples/estimate_depth_known_pose.py
@@ -1,6 +1,9 @@
 import numpy as np
 from skimage.transform import resize
 from skimage.color import rgb2gray
+from matplotlib import pyplot as plt
+
+from tadataka import camera
 from tadataka.pose import WorldPose
 from tadataka.camera import CameraModel, CameraParameters
 from tadataka.warp import warp2d, Warp2D
@@ -11,16 +14,17 @@ from tadataka.vo.semi_dense.flag import ResultFlag as FLAG
 from tadataka.vo.semi_dense.fusion import fusion
 from tadataka.vo.semi_dense.frame import Frame
 from tadataka.vo.semi_dense.common import invert_depth
+from tadataka.metric import photometric_error
+from tadataka.warp import LocalWarp2D
 from tadataka.vo.semi_dense.propagation import (
     propagate, detect_intensity_change)
 from tadataka.vo.semi_dense.semi_dense import InverseDepthMapEstimator
 from tadataka.vo.semi_dense.regularization import regularize
 from tadataka.vo.semi_dense.frame_selection import ReferenceSelector
-from examples.plot import plot_depth
+from examples.plot import plot_depth, plot_trajectory
 
 
 def plot_reprojection(keyframe, refframe):
-    from matplotlib import pyplot as plt
     px, py = [200, 400]
     p = keyframe.camera_model.normalize(np.array([px, py], dtype=np.float64))
     q = warp2d((keyframe.pose.R, keyframe.pose.t),
@@ -37,18 +41,6 @@ def plot_reprojection(keyframe, refframe):
     ax.imshow(refframe.image)
     ax.scatter(qx, qy)
 
-    plt.show()
-
-
-def plot_age_map(age_map):
-    from matplotlib import pyplot as plt
-    from mpl_toolkits.axes_grid1 import make_axes_locatable
-    fig = plt.figure()
-    ax = fig.add_subplot()
-    im = ax.imshow(age_map, cmap="gray")
-    divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
-    plt.colorbar(im, cax=cax)
     plt.show()
 
 
@@ -79,7 +71,6 @@ def to_perspective(camera_model):
 
 
 def plot_refframes(refframes, keyframe, age_map):
-    from matplotlib import pyplot as plt
     N = len(refframes) + 2
 
     fig = plt.figure()
@@ -103,17 +94,9 @@ def get(frame):
     return to_perspective(frame.camera_model), rgb2gray(frame.image)
 
 
-def resize_camera_model(cm, scale):
-    return CameraModel(
-        CameraParameters(cm.camera_parameters.focal_length * scale,
-                         cm.camera_parameters.offset * scale),
-        cm.distortion_model
-    )
-
-
-def dvo_(cm0, cm1, I0, D0, I1, W, scale=1.0):
-    estimator = PoseChangeEstimator(resize_camera_model(cm0, scale),
-                                    resize_camera_model(cm1, scale),
+def dvo_(cm0, cm1, I0, D0, I1, W, scale=1/6):
+    estimator = PoseChangeEstimator(camera.resize(cm0, scale),
+                                    camera.resize(cm1, scale),
                                     n_coarse_to_fine=3)
 
     shape = (int(I0.shape[0] * scale), int(I0.shape[1] * scale))
@@ -127,52 +110,58 @@ def dvo(camera_model0, camera_model1, image0, image1,
                 image0, invert_depth(inv_depth_map),
                 image1, invert_depth(variance_map))
 
+
 def main():
     dataset = TumRgbdDataset("datasets/rgbd_dataset_freiburg1_desk",
                              which_freiburg=1)
-    color_frames = dataset[100:130]
+    color_frames = dataset[100:200]
 
-    camera_model0, image0 = get(color_frames[0])
     pose0 = WorldPose.identity()
-    frame0 = Frame(camera_model0, image0, pose0)
+    age_map0 = np.zeros(color_frames[0].image.shape[0:2], dtype=np.int64)
+    # inv_depth_map0 = np.random.uniform(0.1, 10.0, image0.shape)
+    variance_map0 = np.ones(color_frames[0].image.shape[0:2])
 
-    age_map0 = np.zeros(image0.shape, dtype=np.int64)
-    # inv_depth_map0 = invert_depth(frame0_.depth_map)
-    inv_depth_map0 = np.random.uniform(0.1, 10.0, image0.shape)
-    variance_map0 = 10.0 * np.ones(image0.shape)
-
+    trajectory_true = []
+    trajectory_pred = []
     refframes = []
+    for i in range(len(color_frames)-1):
+        frame0_, frame1_ = color_frames[i+0], color_frames[i+1]
 
-    for i in range(1, len(color_frames)):
-        frame1_ = color_frames[i]
+        camera_model0, image0 = get(frame0_)
         camera_model1, image1 = get(frame1_)
+
+        inv_depth_map0 = invert_depth(frame0_.depth_map)
+
         pose10 = dvo(camera_model0, camera_model1, image0, image1,
-                     inv_depth_map0, variance_map0)
+                     inv_depth_map0, np.ones(image0.shape))
         pose1 = pose10 * pose0
-        frame1 = Frame(camera_model1, image1, pose1)
+        print("pose10 pred", pose10)
+        print("pose10 true", frame1_.pose * frame0_.pose.inv())
 
         warp10 = Warp2D(camera_model0, camera_model1, pose0, pose1)
-
-        prior_inv_depth_map1, prior_variance_map1 = propagate(
-            warp10, inv_depth_map0, variance_map0
-        )
         age_map1 = increment_age(age_map0, warp10, inv_depth_map0)
 
-        refframes.append(frame0)
+        refframes.append(Frame(camera_model0, image0, pose0))
         # plot_refframes(refframes, frame1, age_map1)
 
         inv_depth_map1, variance_map1, flag_map1 = update(
-            frame1, ReferenceSelector(refframes, age_map1),
-            prior_inv_depth_map1, prior_variance_map1
+            Frame(camera_model1, image1, pose1),
+            ReferenceSelector(refframes, age_map1),
+            *propagate(warp10, inv_depth_map0, variance_map0)
         )
 
-        plot_depth(frame1.image, age_map1,
-                   flag_map1, frame1_.depth_map,
-                   invert_depth(inv_depth_map1), variance_map1)
+        # plot_depth(image1, age_map1, flag_map1, frame1_.depth_map,
+        #            invert_depth(inv_depth_map1), variance_map1)
 
-        frame0 = frame1
         age_map0 = age_map1
         inv_depth_map0 = inv_depth_map1
         variance_map0 = variance_map1
+        pose0 = pose1
+
+        trajectory_pred.append(pose1.t)
+        trajectory_true.append(frame1_.pose.t)
+
+    plot_trajectory(np.array(trajectory_true), np.array(trajectory_pred))
+
 
 main()
