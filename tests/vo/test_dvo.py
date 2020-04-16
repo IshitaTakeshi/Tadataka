3bce651381240f464d777e4264e8e127fcead629
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-16 19:40:59 +0900

    Compare error with one calculated from ground truth pose

--- tests/vo/test_dvo.py
+++ tests/vo/test_dvo.py
@@ -4,68 +4,49 @@ from skimage.transform import resize
 import pytest
 
 from tadataka import camera
+from tadataka.metric import PhotometricError
 from tadataka.pose import WorldPose
 from tadataka.dataset.new_tsukuba import NewTsukubaDataset
-from tadataka.warp import Warp2D
-from tadataka.metric import photometric_error
+from tadataka.warp import LocalWarp2D, Warp2D
 from tadataka.vo.dvo import _PoseChangeEstimator, PoseChangeEstimator
 from tests.dataset.path import new_tsukuba
 
 
-def load(scale):
-    dataset = NewTsukubaDataset(new_tsukuba)
-    frame0, frame1 = dataset[0][0], dataset[4][1]
-
-    camera_model0 = camera.resize(frame0.camera_model, scale)
-    camera_model1 = camera.resize(frame1.camera_model, scale)
-
-    shape = (int(frame0.image.shape[0] * scale),
-             int(frame0.image.shape[1] * scale))
-    I0 = resize(rgb2gray(frame0.image), shape)
-    D0 = resize(frame0.depth_map, shape)
-    I1 = resize(rgb2gray(frame1.image), shape)
-    return camera_model0, camera_model1, I0, D0, I1
-
-
-def test_subestimator():
-    camera_model0, camera_model1, I0, D0, I1 = load(1/10)
-
-    estimator = _PoseChangeEstimator(camera_model0, camera_model1, max_iter=20)
-
-    prior = WorldPose.identity()
-    warp = Warp2D(camera_model0, camera_model1, prior, WorldPose.identity())
-    error_prior = photometric_error(warp, I0, D0, I1)
-
-    dpose = estimator(I0, D0, I1, prior, None)
-
-    warp = Warp2D(camera_model0, camera_model1, dpose, WorldPose.identity())
-    error_pred = photometric_error(warp, I0, D0, I1)
-    assert(error_pred < error_prior * 0.12)
+def get_resized(frame, scale):
+    camera_model = camera.resize(frame.camera_model, scale)
+    shape = (int(frame.image.shape[0] * scale),
+             int(frame.image.shape[1] * scale))
+    I = resize(rgb2gray(frame.image), shape)
+    D = resize(frame.depth_map, shape)
+    return camera_model, I, D
 
 
 def test_pose_change_estimator():
-    camera_model0, camera_model1, I0, D0, I1 = load(1/6)
+    dataset = NewTsukubaDataset(new_tsukuba)
+
+    frame0_, frame1_ = dataset[0][0], dataset[4][0]
+    scale = 1.0
+    camera_model0, I0, D0 = get_resized(frame0_, scale)
+    camera_model1, I1, __ = get_resized(frame1_, scale)
+
+    pose10_true = frame1_.pose.inv() * frame0_.pose
+
+    error = PhotometricError(camera_model0, camera_model1, I0, D0, I1)
 
     estimator = PoseChangeEstimator(camera_model0, camera_model1,
-                                    n_coarse_to_fine=3)
-
-    def error(pose10):
-        # warp points in t0 coordinate onto the t1 coordinate
-        # we regard pose1 as world origin
-        warp = Warp2D(camera_model0, camera_model1,
-                      pose10, WorldPose.identity())
-        return photometric_error(warp, I0, D0, I1)
+                                    n_coarse_to_fine=5)
 
     def evaluate(weights, rate):
-        identity = WorldPose.identity()
-        pose10_pred = estimator(I0, D0, I1, weights, identity)
-        assert(error(pose10_pred) < error(identity) * rate)
+        pose_identity = WorldPose.identity()
+        pose10_pred = estimator(I0, D0, I1, weights, pose_identity)
+        assert(error(pose10_pred) < error(pose_identity))
+        assert(error(pose10_pred) < error(pose10_true) * rate)
 
-    evaluate(weights=None, rate=0.20)
-    evaluate(weights=np.ones(I0.shape), rate=0.20)
-    evaluate(weights="tukey", rate=0.40)  # currently tukey cannot work well
-    evaluate(weights="student-t", rate=0.20)
-    evaluate(weights="huber", rate=0.20)
+    evaluate(weights=None, rate=1.40)
+    evaluate(weights=np.ones(I0.shape), rate=1.40)
+    evaluate(weights="tukey", rate=2.50)  # currently tukey cannot work well
+    evaluate(weights="student-t", rate=1.40)
+    evaluate(weights="huber", rate=1.50)
 
     with pytest.raises(ValueError, match="No such weights 'random'"):
-        evaluate(weights="random", rate=0.20)
+        evaluate(weights="random", rate=1.40)
