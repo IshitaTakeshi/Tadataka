d1e308a1a3ac6bf28aaeb392af0da4e21de43c56
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-16 19:17:06 +0900

    Use column stack to create the jacobian

--- tadataka/vo/dvo/jacobian.py
+++ tadataka/vo/dvo/jacobian.py
@@ -14,14 +14,14 @@ def calc_jacobian(focal_length, didx, didy, P):
     z2 = z * z
     xy = x * y
 
-    J = np.empty((P.shape[0], 6))
-    J[:, 0] = fgx / z
-    J[:, 1] = fgy / z
-    J[:, 2] = -(fgx * x + fgy * y) / (z * z)
-    J[:, 3] = -(fgx * xy + fgy * (z2 + y * y)) / z2
-    J[:, 4] = (fgx * (z2 + x * x) + fgy * xy) / z2
-    J[:, 5] = (-fgx * y + fgy * x) / z
-    return J
+    return np.column_stack((
+        fgx / z,
+        fgy / z,
+        -(fgx * x + fgy * y) / (z * z),
+        -(fgx * xy + fgy * (z2 + y * y)) / z2,
+        (fgx * (z2 + x * x) + fgy * xy) / z2,
+        (-fgx * y + fgy * x) / z
+    ))
 
 
 def calc_image_gradient(image):
