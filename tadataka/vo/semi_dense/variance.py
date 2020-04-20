dec24b011454e3aaa8e512c543411105bc2510e4
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-21 07:45:23 +0900

    Cleanup

--- tadataka/vo/semi_dense/variance.py
+++ tadataka/vo/semi_dense/variance.py
@@ -14,20 +14,22 @@ def photometric_variance(gradient_along_epipolar_line, sigma_i):
 
 
 def geometric_variance(epipolar_direction, image_gradient, sigma_l, epsilon):
+    # we assume epipolar_direction and image_gradient are not normalized
     ng = np.dot(image_gradient, image_gradient)
     nl = np.dot(epipolar_direction, epipolar_direction)
     normalizer = ng * nl
+    sl2 = (sigma_l * sigma_l)
 
     if normalizer < epsilon:
-        return (sigma_l * sigma_l) / epsilon
+        return sl2 / epsilon
 
     p = np.dot(epipolar_direction, image_gradient)
     product = (p * p) / normalizer
 
     if product < epsilon:
-        return (sigma_l * sigma_l) / epsilon
+        return sl2 / epsilon
 
-    return (sigma_l * sigma_l) / product
+    return sl2 / product
 
 
 def calc_alpha_(x_key, x_ref_i, direction_i, ri, rz, ti, tz):
@@ -43,7 +45,9 @@ def alpha_index(search_step):
     return np.argmax(np.abs(search_step))
 
 
-def calc_alpha(x_key, x_ref, direction, R, t):
+def calc_alpha(T_rk, x_key, direction, prior_inv_depth):
+    x_ref, _ = warp2d_(T_rk, x_key, invert_depth(prior_inv_depth))
+    R_rk, t_rk = get_rotation_translation(T_rk)
     index = alpha_index(direction)
     return calc_alpha_(x_key, x_ref[index], direction[index],
-                       R[index], R[2], t[index], t[2])
+                       R_rk[index], R_rk[2], t_rk[index], t_rk[2])
