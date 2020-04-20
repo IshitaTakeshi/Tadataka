c8f2e9da2195b61ddf79be2b0af7e9e4d79c4961
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-21 07:50:45 +0900

    Test overall of 'calc_alpha'

--- tests/vo/semi_dense/test_variance.py
+++ tests/vo/semi_dense/test_variance.py
@@ -2,8 +2,10 @@ from numpy.testing import assert_almost_equal
 import numpy as np
 from numpy.linalg import norm
 
+from tadataka.projection import pi
+from tadataka.matrix import motion_matrix, to_homogeneous, from_homogeneous
 from tadataka.vo.semi_dense.variance import (
-    alpha_index, calc_alpha_,
+    alpha_index, calc_alpha_, calc_alpha,
     geometric_variance, photometric_variance
 )
 
@@ -41,7 +43,7 @@ def test_photometric_variance():
     assert(variance == 0.8)
 
 
-def test_calc_alpha():
+def test_calc_alpha_():
     R = np.array([
         [0, -1, 0],
         [1, 0, 0],
@@ -72,3 +74,32 @@ def test_alpha_index():
     assert(alpha_index([1.9, 0.2]) == 0)
     assert(alpha_index([0.9, -1.2]) == 1)
     assert(alpha_index([-0.9, -0.2]) == 0)
+
+
+def test_calc_alpha():
+    R_rk = np.array([
+        [0, -1, 0],
+        [1, 0, 0],
+        [0, 0, 1]
+    ])
+    t_rk = np.array([2, 4, -3])
+
+    T_rk = motion_matrix(R_rk, t_rk)
+
+    x_key = np.array([0.3, 0.9])
+    prior_inv_depth = 0.1
+
+    P_key = (1 / prior_inv_depth) * to_homogeneous(x_key)
+    x_ref = pi(from_homogeneous(np.dot(T_rk, to_homogeneous(P_key))))
+
+    direction = np.array([0.1, 0.3])
+    assert_almost_equal(
+        calc_alpha(T_rk, x_key, direction, prior_inv_depth),
+        calc_alpha_(x_key, x_ref[1], direction[1],
+                    R_rk[1], R_rk[2], t_rk[1], t_rk[2]))
+
+    direction = np.array([-2.0, 1.0])
+    assert_almost_equal(
+        calc_alpha(T_rk, x_key, direction, prior_inv_depth),
+        calc_alpha_(x_key, x_ref[0], direction[0],
+                    R_rk[0], R_rk[2], t_rk[0], t_rk[2]))
