b6e294ea9816f2e96eab68a5a7e41b5d299972de
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-21 07:11:29 +0900

    Rename 'coordinates' -> 'warp_coordinates' Take depth arrays as arguments

--- tadataka/vo/semi_dense/coordinates.py
+++ tadataka/vo/semi_dense/coordinates.py
@@ -22,10 +22,7 @@ def coordinates_(warp10, depth_map0):
     return us0, us1, depths0, depths1
 
 
-def coordinates(warp10, inv_depth_map0):
-    depth_map0 = invert_depth(inv_depth_map0)
+def warp_coordinates(warp10, depth_map0):
     us0, us1, depths0, depths1 = coordinates_(warp10, depth_map0)
-
     mask = is_in_image_range(us1, depth_map0.shape)
-    return (us0[mask], us1[mask],
-            invert_depth(depths0[mask]), invert_depth(depths1[mask]))
+    return us0[mask], us1[mask], depths0[mask], depths1[mask]
