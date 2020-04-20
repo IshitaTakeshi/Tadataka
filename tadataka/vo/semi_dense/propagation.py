50a007feac7e59ee371d5947e30a2e8aac634ddc
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-21 07:44:03 +0900

    Make 'substitute' easier to test Temporarily remove 'detect_intensity_change'

--- tadataka/vo/semi_dense/propagation.py
+++ tadataka/vo/semi_dense/propagation.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+from tadataka.coordinates import substitute, get
 from tadataka.vo.semi_dense.stat import are_statically_same
 from tadataka.vo.semi_dense.fusion import fusion
 from tadataka.vo.semi_dense.common import invert_depth
-from tadataka.vo.semi_dense.coordinates import substitute, get, coordinates
+from tadataka.vo.semi_dense.coordinates import warp_coordinates
 
 
 def propagate_variance(inv_depths0, inv_depths1, variances0, uncertaintity):
@@ -19,6 +20,7 @@ def handle_collision_(inv_depth_a, inv_depth_b, variance_a, variance_b):
                            variance_a, variance_b, factor=2.0):
         return fusion(inv_depth_a, inv_depth_b, variance_a, variance_b)
 
+    # b is hidden by a
     if invert_depth(inv_depth_a) < invert_depth(inv_depth_b):
         return (inv_depth_a, variance_a)
     else:
@@ -43,32 +45,23 @@ def substitute_(us, inv_depths, variances, shape):
     return inv_depth_map, variance_map
 
 
-def propagate(warp10, inv_depth_map0, variance_map0,
-              default_inv_depth=1.0, default_variance=1.0,
-              uncertaintity_bias=0.01):
-    assert(inv_depth_map0.shape == variance_map0.shape)
-    shape = inv_depth_map0.shape
+def substitute(us, inv_depths, variances, shape,
+               default_inv_depth, default_variance):
+    inv_depth_map, variance_map = substitute_(us.astype(np.int64),
+                                              inv_depths, variances, shape)
+    inv_depth_map[np.isnan(inv_depth_map)] = default_inv_depth
+    variance_map[np.isnan(variance_map)] = default_variance
+    return inv_depth_map, variance_map
 
-    us0, us1, inv_depths0, inv_depths1 = coordinates(warp10, inv_depth_map0)
+
+def propagation(inv_depth_map0, variance_map0,
+                default_inv_depth=1.0, default_variance=1.0,
+                uncertaintity_bias=1.0):
+    us0, us1, inv_depths0, inv_depths1 = warp_coordinates(
+        warp10, inv_depth_map0
+    )
     variances1 = propagate_variance(inv_depths0, inv_depths1,
                                     get(variance_map0, us0),
                                     uncertaintity_bias)
-
-    inv_depth_map1, variance_map1 = substitute_(us1.astype(np.int64),
-                                                inv_depths1, variances1, shape)
-    inv_depth_map1[np.isnan(inv_depth_map1)] = default_inv_depth
-    variance_map1[np.isnan(variance_map1)] = default_variance
-    return inv_depth_map1, variance_map1
-
-
-from tadataka.interpolation import interpolation
-def detect_intensity_change(warp10, image0, image1, inv_depth_map0,
-                            threshold=0.2):
-    assert(image0.dtype == np.float64)
-    assert(image1.dtype == np.float64)
-    us0, us1, _, _ = coordinates(warp10, inv_depth_map0)
-
-    intensities0 = get(image0, us0)
-    intensities1 = interpolation(image1, us1)
-    mask = (intensities0 - intensities1) > threshold
-    return substitute(np.zeros(image0.shape, dtype=np.bool), us0, mask)
+    return substitute(us1, inv_depths1, variances1, inv_depth_map0.shape,
+                      default_inv_depth, default_variance)
