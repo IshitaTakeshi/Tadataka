d53bd2e2005f97d113ff4448622b47b02f4a7436
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-21 07:46:21 +0900

    Make inv depth estimator smaller

--- tadataka/vo/semi_dense/semi_dense.py
+++ tadataka/vo/semi_dense/semi_dense.py
@@ -1,223 +1,120 @@
+from warnings import warn
+
 import numpy as np
-from numba import njit
 
 from tqdm import tqdm
 
 from tadataka.coordinates import image_coordinates
-from tadataka.vector import normalize_length
-from tadataka.utils import is_in_image_range
-from tadataka.matrix import to_homogeneous
-from tadataka.camera.table import NoramlizationMapTable
-from tadataka.projection import inv_pi, pi
+from tadataka.interpolation import interpolation_
+from tadataka.gradient import grad_x, grad_y
+from tadataka.matrix import to_homogeneous, inv_motion_matrix, get_translation
 from tadataka.rigid_transform import inv_transform
-from tadataka.interpolation import interpolation_, interpolation_
-from tadataka.triangulation import depth_from_triangulation
-from tadataka.warp import warp2d, warp3d
+from tadataka.projection import inv_pi, pi
+from tadataka.utils import is_in_image_range
+from tadataka.vector import normalize_length
 from tadataka.vo.semi_dense.common import invert_depth
-from tadataka.vo.semi_dense.epipolar import (
-    reference_coordinates, key_coordinates
+from tadataka.vo.semi_dense.depth import (
+    calc_ref_inv_depth, calc_key_depth,
+    InvDepthSearchRange, depth_search_range
 )
-from tadataka.vo.semi_dense.variance import (
-    photometric_variance, geometric_variance, calc_alpha
+from tadataka.vo.semi_dense.epipolar import (
+    key_coordinates, key_epipolar_direction,
+    ref_coordinates, ref_search_range
 )
 from tadataka.vo.semi_dense.flag import ResultFlag as FLAG
-from tadataka.vo.semi_dense.intensities import search_intensities
-from tadataka.gradient import grad_x, grad_y
-
-
-def intensity_gradient(intensities, interval):
-    return np.linalg.norm(intensities[1:] - intensities[:-1]) / interval
-
-
-def calc_observation_variance(alpha, geo_variance, photo_variance):
-    return alpha * alpha * (geo_variance + photo_variance)
-
-
-class InverseDepthSearchRange(object):
-    def __init__(self, min_inv_depth, max_inv_depth, factor=2.0):
-        assert(min_inv_depth > 0.0)
-        assert(max_inv_depth > min_inv_depth)
-        self.factor = factor
-        self.min_inv_depth = min_inv_depth
-        self.max_inv_depth = max_inv_depth
-
-    def __call__(self, inv_depth, variance):
-        assert(variance >= 0.0)
-        L = max(inv_depth - self.factor * variance, self.min_inv_depth)
-        U = min(inv_depth + self.factor * variance, self.max_inv_depth)
-        return L, U
-
-
-@njit
-def calc_depth_ref(T_key, T_ref, x_key, depth_key):
-    p_key = inv_pi(x_key, depth_key)
-    p_ref = warp3d(T_key, T_ref, p_key)
-    return p_ref[2]
-
-
-def calc_inv_depth_ref(T_key, T_ref, x_key, inv_depth_key):
-    depth_key = invert_depth(inv_depth_key)
-    depth_ref = calc_depth_ref(T_key, T_ref, x_key, depth_key)
-    return invert_depth(depth_ref)
-
-
-class GradientImage(object):
-    def __init__(self, image_grad_x, image_grad_y):
-        self.grad_x = image_grad_x
-        self.grad_y = image_grad_y
-
-    def __call__(self, u_key):
-        gx = interpolation_(self.grad_x, u_key)
-        gy = interpolation_(self.grad_y, u_key)
-        return np.array([gx, gy])
-
-
-@njit
-def depth_search_range(inv_depth_range):
-    min_inv_depth, max_inv_depth = inv_depth_range
-    min_depth = invert_depth(max_inv_depth)
-    max_depth = invert_depth(min_inv_depth)
-    return min_depth, max_depth
-
-
-@njit
-def epipolar_search_range_(T_key, T_ref, x_key, depth_range):
-    min_depth, max_depth = depth_range
-
-    x_ref_min = warp2d(T_key, T_ref, x_key, min_depth)
-    x_ref_max = warp2d(T_key, T_ref, x_key, max_depth)
-
-    return x_ref_min, x_ref_max
-
-
-@njit
-def epipolar_search_range(T_key, T_ref, x_key, inv_depth_range):
-    depth_range = depth_search_range(inv_depth_range)
-    return epipolar_search_range_(T_key, T_ref, x_key, depth_range)
-
-
-class Uncertaintity(object):
-    def __init__(self, sigma_i, sigma_l):
-        self.sigma_l = sigma_l
-        self.sigma_i = sigma_i
-
-    def __call__(self, x_key, x_ref, x_range_ref, R, t,
-                 image_grad, epipolar_gradient):
-        geo_epsilon = 1e-4
-        x_min_ref, x_max_ref = x_range_ref
-
-        direction = normalize_length(x_max_ref - x_min_ref)
-        alpha = calc_alpha(x_key, x_ref, direction, R, t)
-
-        geo_var = geometric_variance(x_key - pi(t), image_grad,
-                                     self.sigma_l, geo_epsilon)
-        photo_var = photometric_variance(epipolar_gradient, self.sigma_i)
-        return calc_observation_variance(alpha, geo_var, photo_var)
-
-
-def _unnormalize_if_in_image(camera_model, xs, shape):
-    us = camera_model.unnormalize(xs)
-    mask = is_in_image_range(us, shape)
-    return xs[mask], us[mask]
+from tadataka.vo.semi_dense.gradient import GradientImage, gradient1d
+from tadataka.vo.semi_dense.variance import (
+    calc_alpha, calc_observation_variance,
+    photometric_variance, geometric_variance
+)
+from tadataka.vo.semi_dense.hypothesis import Hypothesis
+from tadataka.vo.semi_dense._intensities import search_intensities
+from tadataka.warp import warp2d, warp3d
 
 
 def all_points_in_image(us, image_shape):
     return is_in_image_range(us, image_shape).all()
 
 
-class InverseDepthEstimator(object):
-    def __init__(self, keyframe, sigma_i, sigma_l, step_size_ref,
-                 min_gradient):
-        assert(np.ndim(keyframe.image) == 2)
-        self.keyframe = keyframe
+def calc_gradient(intensities):
+    return np.linalg.norm(gradient1d(intensities))
 
-        self.T_key = (keyframe.R, keyframe.t)
+
+class InvDepthEstimator(object):
+    def __init__(self, camera_model_key, image_key,
+                 inv_depth_search_range, sigma_i, sigma_l,
+                 step_size_ref, min_gradient):
+        assert(np.ndim(image_key) == 2)
+        assert(step_size_ref > 0)
+        self.sigma_i = sigma_i
+        self.sigma_l = sigma_l
         self.min_gradient = min_gradient
-
-        image_key = keyframe.image
-
-        self.uncertaintity = Uncertaintity(sigma_i, sigma_l)
+        self.camera_model_key = camera_model_key
+        self.image_key = image_key
         self.step_size_ref = step_size_ref
         self.image_grad = GradientImage(grad_x(image_key), grad_y(image_key))
-        self.search_range = InverseDepthSearchRange(
-            min_inv_depth=0.1, max_inv_depth=10.0
-        )
+        self.inv_depth_range = InvDepthSearchRange(*inv_depth_search_range)
 
-    def __call__(self, refframe, u_key, prior_inv_depth, prior_variance):
-        key = self.keyframe
-        ref = refframe
+    def __call__(self, camera_model_ref, image_ref, T_rk, u_key, prior):
+        if prior.inv_depth <= 0:
+            return prior, FLAG.NEGATIVE_PRIOR_DEPTH
 
-        T_key = self.T_key  # key to world
-        T_ref = (refframe.R, refframe.t)
+        inv_depth_range = self.inv_depth_range(prior)
+        if inv_depth_range is None:
+            return prior, FLAG.HYPOTHESIS_OUT_OF_SERCH_RANGE
 
-        x_key = key.camera_model.normalize(u_key)
+        x_key = self.camera_model_key.normalize(u_key)
 
-        if prior_inv_depth <= 0:
-            return prior_inv_depth, prior_variance, FLAG.NEGATIVE_PRIOR_DEPTH
-
-        inv_depth_ref = calc_inv_depth_ref(T_key, T_ref,
-                                           x_key, prior_inv_depth)
-
+        # step size / inv depth = approximately const
+        inv_depth_ref = calc_ref_inv_depth(T_rk, x_key, prior.inv_depth)
+        step_size_key = (prior.inv_depth / inv_depth_ref) * self.step_size_ref
         if inv_depth_ref <= 0:
-            return prior_inv_depth, prior_variance, FLAG.NEGATIVE_REF_DEPTH
+            return prior, FLAG.NEGATIVE_REF_DEPTH
 
-        step_size_key = (prior_inv_depth / inv_depth_ref) * self.step_size_ref
+        xs_key = key_coordinates(get_translation(T_rk), x_key, step_size_key)
+        us_key = self.camera_model_key.unnormalize(xs_key)
+        if not all_points_in_image(us_key, self.image_key.shape):
+            return prior, FLAG.KEY_OUT_OF_RANGE
 
-        assert(step_size_key > 0)
-        assert(self.step_size_ref > 0)
-        # ref.t is the position of ref camera center in the world coordinate
-        # inv_transform(*T_key, ref.t) brings the ref camera center onto
-        # the key camera coordinate
-        # pi project it onto the key image plane
-        t_image_ref = pi(inv_transform(*T_key, ref.t))
-        xs_key = key_coordinates(x_key, t_image_ref, step_size_key)
-        us_key = key.camera_model.unnormalize(xs_key)
+        intensities_key = interpolation_(self.image_key, us_key)
+        gradient_key = calc_gradient(intensities_key)
 
-        if not all_points_in_image(us_key, key.image.shape):
-            return prior_inv_depth, prior_variance, FLAG.KEY_OUT_OF_RANGE
+        if gradient_key < self.min_gradient:
+            return prior, FLAG.INSUFFICIENT_GRADIENT
 
-        intensities_key = interpolation_(key.image, us_key)
-        epipolar_gradient = intensity_gradient(intensities_key, step_size_key)
-        if epipolar_gradient < self.min_gradient:
-            return prior_inv_depth, prior_variance, FLAG.INSUFFICIENT_GRADIENT
-
-        inv_depth_range = self.search_range(prior_inv_depth, prior_variance)
-        x_range_ref = epipolar_search_range(T_key, T_ref,
-                                            x_key, inv_depth_range)
-
-        xs_ref = reference_coordinates(x_range_ref, self.step_size_ref)
+        x_range_ref = ref_search_range(T_rk, x_key,
+                                       depth_search_range(*inv_depth_range))
+        xs_ref = ref_coordinates(x_range_ref, self.step_size_ref)
         if len(xs_ref) < len(xs_key):
-            return prior_inv_depth, prior_variance, FLAG.EPIPOLAR_TOO_SHORT
+            return prior, FLAG.REF_EPIPOLAR_TOO_SHORT
 
-        us_ref = ref.camera_model.unnormalize(xs_ref)
-        if not all_points_in_image(us_ref, ref.image.shape):
-            return prior_inv_depth, prior_variance, FLAG.REF_OUT_OF_RANGE
+        us_ref = camera_model_ref.unnormalize(xs_ref)
 
-        intensities_ref = interpolation_(ref.image, us_ref)
+        if not all_points_in_image(us_ref, image_ref.shape):
+            return prior, FLAG.REF_OUT_OF_RANGE
+
+        intensities_ref = interpolation_(image_ref, us_ref)
         argmin = search_intensities(intensities_key, intensities_ref)
+        depth_key = calc_key_depth(T_rk, x_key, xs_ref[argmin])
 
-        R_ref, t_ref = T_ref
-        R_key, t_key = T_key
-
-        # transformation from key camera coordinate to ref camera coordinate
-        R = np.dot(R_ref.T, R_key)
-        t = np.dot(R_ref.T, t_key - t_ref)
-
-        depth_key = depth_from_triangulation(R, t, x_key, xs_ref[argmin])
-
-        x_ref = warp2d(T_key, T_ref, x_key, invert_depth(prior_inv_depth))
-        image_grad = self.image_grad(u_key)
-        variance = self.uncertaintity(x_key, x_ref, x_range_ref, R, t,
-                                      image_grad, epipolar_gradient)
-        return invert_depth(depth_key), variance, FLAG.SUCCESS
+        x_max_ref, x_min_ref = x_range_ref
+        direction = normalize_length(x_max_ref - x_min_ref)
+        variance = calc_observation_variance(
+            alpha=calc_alpha(T_rk, x_key, direction, prior.inv_depth),
+            geo_variance=geometric_variance(
+                key_epipolar_direction(get_translation(T_rk), x_key),
+                self.image_grad(u_key), self.sigma_l, epsilon=1e-4),
+            photo_variance=photometric_variance(gradient_key / step_size_key,
+                                                self.sigma_i)
+        )
+        return Hypothesis(invert_depth(depth_key), variance), FLAG.SUCCESS
 
 
-class InverseDepthMapEstimator(object):
+class InvDepthMapEstimator(object):
     def __init__(self, *args, **kwargs):
-        self._estimator = InverseDepthEstimator(*args, **kwargs)
+        self._estimator = InvDepthEstimator(*args, **kwargs)
 
-    def __call__(self, reference_selector,
-                 inv_depth_map, variance_map):
+    def __call__(self, reference_selector, inv_depth_map, variance_map):
         image_shape = inv_depth_map.shape
 
         flag_map = np.full(image_shape, FLAG.NOT_PROCESSED)
@@ -228,8 +125,7 @@ class InverseDepthMapEstimator(object):
                 continue
 
             inv_depth, variance, flag = self._estimator(
-                refframe, u_key,
-                inv_depth_map[y, x], variance_map[y, x]
+                refframe, u_key, inv_depth_map[y, x], variance_map[y, x]
             )
 
             inv_depth_map[y, x] = inv_depth
