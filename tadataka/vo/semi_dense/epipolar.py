e5d3a9fd0df973ae9ae644293e4be8901c19e844
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-21 07:22:46 +0900

    Add a function to compute reference search range

--- tadataka/vo/semi_dense/epipolar.py
+++ tadataka/vo/semi_dense/epipolar.py
@@ -4,6 +4,10 @@ from tadataka.projection import pi
 from tadataka.vector import normalize_length
 from tadataka.utils import is_in_image_range
 from tadataka.matrix import inv_motion_matrix, get_translation
+from tadataka.warp import warp2d_
+
+
+EPSILON = 1e-16
 
 
 def coordinates_along_line(start, step, disparities):
@@ -13,12 +17,12 @@ def coordinates_along_line(start, step, disparities):
 def calc_coordinates(x_min, x_max, step_size):
     d = x_max - x_min
     norm = np.linalg.norm(d)
-    direction = d / norm
+    direction = d / (norm + EPSILON)
     N = norm // step_size
     return coordinates_along_line(x_min, step_size * direction, np.arange(N))
 
 
-def reference_coordinates(x_range, step_size):
+def ref_coordinates(x_range, step_size):
     x_min, x_max = x_range
     return calc_coordinates(x_min, x_max, step_size)
 
@@ -26,7 +30,7 @@ def reference_coordinates(x_range, step_size):
 sampling_steps = np.array([-2, -1, 0, 1, 2])
 
 
-def key_coordinates(epipolar_direction, x_key, step_size):
+def key_coordinates_(epipolar_direction, x_key, step_size):
     direction = normalize_length(epipolar_direction)
     step = step_size * direction
     return coordinates_along_line(x_key, step, sampling_steps)
@@ -34,3 +38,17 @@ def key_coordinates(epipolar_direction, x_key, step_size):
 
 def key_epipolar_direction(t_rk, x_key):
     return x_key - pi(t_rk)
+
+
+def key_coordinates(t_rk, x_key, step_size_key):
+    return key_coordinates_(key_epipolar_direction(t_rk, x_key),
+                            x_key, step_size_key)
+
+
+def ref_search_range(T_rk, x_key, depth_range):
+    min_depth, max_depth = depth_range
+
+    x_ref_min, _ = warp2d_(T_rk, x_key, min_depth)
+    x_ref_max, _ = warp2d_(T_rk, x_key, max_depth)
+
+    return x_ref_min, x_ref_max
