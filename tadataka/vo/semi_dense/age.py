ce3aad8a601a4819afed5906f8c4ff049492d7fd
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-22 01:00:25 +0900

    Fix importing

--- tadataka/vo/semi_dense/age.py
+++ tadataka/vo/semi_dense/age.py
@@ -1,9 +1,10 @@
 import numpy as np
-from tadataka.vo.semi_dense.coordinates import substitute, get, coordinates
+from tadataka.coordinates import substitute, get
+from tadataka.vo.semi_dense.coordinates import warp_coordinates
 
 
 def increment_age(age_map0, warp10, inv_depth_map0):
     assert(age_map0.shape == inv_depth_map0.shape)
-    us0, us1, _, _ = coordinates(warp10, inv_depth_map0)
+    us0, us1, _, _ = warp_coordinates(warp10, invert_depth(inv_depth_map0))
     age_map1 = np.zeros(age_map0.shape, dtype=age_map0.dtype)
     return substitute(age_map1, us1.astype(np.int64), get(age_map0, us0) + 1)
