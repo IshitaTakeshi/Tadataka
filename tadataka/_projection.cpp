f582e884c28c587053acc60b39d04579b32d063f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-24 23:58:24 +0900

    Iterate over elements in multiple inv projections

--- tadataka/_projection.cpp
+++ tadataka/_projection.cpp
@@ -1,32 +1,37 @@
+#include <assert.h>
 #include <pybind11/eigen.h>
 
 namespace py = pybind11;
 
 
-Eigen::VectorXd inv_pi(Eigen::Ref<const Eigen::Vector2d> x, double depth) {
+const Eigen::Vector3d inv_pi(const Eigen::Vector2d x, double depth) {
   Eigen::Vector3d y;
   y << x(0), x(1), 1;
   return depth * y;
 }
 
 
-Eigen::MatrixXd inv_pi(
-    Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, 2>> xs,
-    Eigen::Ref<const Eigen::VectorXd> depths) {
+const Eigen::MatrixXd inv_pi(
+    const Eigen::Matrix<double, Eigen::Dynamic, 2> xs,
+    const Eigen::VectorXd depths) {
+  assert(xs.rows() == depths.size());
+
   const int N = xs.rows();
+
   Eigen::MatrixXd ys(N, 3);
-  ys(Eigen::seq(0, N), Eigen::seq(0, 1)) = depths.asDiagonal() * xs;
-  ys(Eigen::seq(0, N), 2) = depths;
+  for(int i = 0; i < N; i++) {
+    ys.row(i) = inv_pi(xs.row(i), depths(i));
+  }
   return ys;
 }
 
 
 PYBIND11_MODULE(_projection, m) {
   m.def("inv_pi",
-        py::overload_cast<Eigen::Ref<const Eigen::Vector2d>,
-                          double>(&inv_pi));
+        py::overload_cast<const Eigen::Vector2d, double>(&inv_pi));
+
   m.def("inv_pi",
         py::overload_cast<
-          Eigen::Ref<const Eigen::Matrix<double, Eigen::Dynamic, 2>>,
-          Eigen::Ref<const Eigen::VectorXd>>(&inv_pi));
+          const Eigen::Matrix<double, Eigen::Dynamic, 2>,
+                              const Eigen::VectorXd>(&inv_pi));
 }
