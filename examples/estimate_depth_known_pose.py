ef661600a69aaf149d8f61f60cd6645984d0c38d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-04-30 02:32:00 +0900

    Make the depth estimator work on hypothesis map

--- examples/estimate_depth_known_pose.py
+++ examples/estimate_depth_known_pose.py
@@ -10,10 +10,12 @@ from tadataka.camera import CameraModel, CameraParameters
 from tadataka.gradient import grad_x, grad_y
 from tadataka.warp import warp2d, Warp2D
 from tadataka.dataset import NewTsukubaDataset, TumRgbdDataset
+from tadataka.vo.dvo import PoseChangeEstimator
 from tadataka.vo.semi_dense.age import AgeMap, increment_age
 from tadataka.vo.semi_dense.flag import ResultFlag as FLAG
 from tadataka.vo.semi_dense.fusion import fusion
 from tadataka.vo.semi_dense.frame import ReferenceSelector
+from tadataka.vo.semi_dense.hypothesis import HypothesisMap
 from tadataka.numeric import safe_invert
 from tadataka.metric import PhotometricError
 from tadataka.warp import LocalWarp2D
@@ -88,20 +90,17 @@ def dvo(camera_model0, camera_model1, image0, image1, depth_map0, weights):
     return estimator(image0, depth_map0, image1, weights)
 
 
-def update(estimator, prior_depth_map, prior_variance_map,
-           reference_selector):
-    prior_inv_depth_map = safe_invert(prior_depth_map)
+def update(estimator, reference_selector, prior_map):
     inv_depth_map, variance_map, flag_map = estimator(
-        prior_inv_depth_map, prior_variance_map,
+        prior_map.inv_depth, prior_map.variance,
         reference_selector
     )
 
-    inv_depth_map, variance_map = fusion(prior_inv_depth_map, inv_depth_map,
-                                         prior_variance_map, variance_map)
-
+    inv_depth_map, variance_map = fusion(prior_map.inv_depth, inv_depth_map,
+                                         prior_map.variance, variance_map)
     inv_depth_map = regularize(inv_depth_map, variance_map)
 
-    return safe_invert(inv_depth_map), variance_map, flag_map
+    return HypothesisMap(inv_depth_map, variance_map), flag_map
 
 
 from tests.dataset.path import new_tsukuba
@@ -127,49 +126,47 @@ class Estimator(object):
 
         self.refframes = [first_frame]
 
-        self.depth_range = [60, 1000]
+        self.inv_depth_range = [1 / 1000, 1 / 60]
         default_variance = 100
-        self.propagate = Propagation(default_depth=200,
+        self.propagate = Propagation(default_inv_depth=1/200,
                                      default_variance=default_variance,
                                      uncertaintity_bias=1.0)
-        self.depth_map = np.random.uniform(*self.depth_range, image.shape)
-        self.variance_map = default_variance * np.ones(image.shape)
+        self._map = HypothesisMap(
+            inv_depth=np.random.uniform(*self.inv_depth_range, image.shape),
+            variance=default_variance * np.ones(image.shape)
+        )
+
         self.age_map = np.zeros(image.shape, dtype=np.int64)
 
     def make_estimator(self, camera_model_key, image_key):
-        min_depth, max_depth = self.depth_range
-        inv_depth_range = [1 / max_depth, 1 / min_depth]
         return InvDepthMapEstimator(
             InvDepthEstimator(camera_model_key, image_key,
-                              inv_depth_range, **estimator_params)
+                              self.inv_depth_range, **estimator_params)
         )
 
     def __call__(self, current_keyframe):
         last_camera_model, _, last_pose = self.refframes[-1]
-        last_depth_map = self.depth_map
-        last_variance_map = self.variance_map
+        last_map = self._map
 
         current_camera_model, current_image, current_pose = current_keyframe
         warp_cl = Warp2D(last_camera_model, current_camera_model,
                          last_pose, current_pose)
-        self.age_map = increment_age(self.age_map, warp_cl, last_depth_map)
-        current_depth_map, current_variance_map = self.propagate(
-            warp_cl, last_depth_map, last_variance_map
-        )
+        self.age_map = increment_age(self.age_map, warp_cl, last_map.depth)
+        current_map = self.propagate(warp_cl, last_map)
 
         assert(self.age_map.max() == len(self.refframes))
 
-        reference_selector = AgeDependentValues(
-            self.age_map,
-            [to_relative(f, current_pose) for f in self.refframes]
-        )
-        self.depth_map, self.variance_map, flag_map = update(
+        self._map, flag_map = update(
             self.make_estimator(current_camera_model, current_image),
-            current_depth_map, current_variance_map, reference_selector
+            AgeDependentValues(
+                self.age_map,
+                [to_relative(f, current_pose) for f in self.refframes]
+            ),
+            current_map,
         )
 
         self.refframes.append(current_keyframe)
-        return self.depth_map, self.variance_map, flag_map
+        return self._map, flag_map
 
 
 def main():
@@ -179,10 +176,11 @@ def main():
     estimator = Estimator(get(frames[0]))
 
     for frame in frames[1:]:
-        depth_map, variance_map, flag_map = estimator(get(frame))
+        map_, flag_map = estimator(get(frame))
         plot_depth(frame.image, np.zeros(frame.depth_map.shape),
                    flag_map, frame.depth_map,
-                   depth_map, variance_map)
+                   map_.depth, map_.variance)
+
 
     # plot_trajectory(np.array(trajectory_true), np.array(trajectory_pred))
 
