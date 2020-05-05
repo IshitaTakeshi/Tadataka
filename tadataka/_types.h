32b9908ed38e16bc761afebed010bb3bcde5a083
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-05 09:47:16 +0900

    Add '_types.h'

---
+++ tadataka/_types.h
@@ -0,0 +1,2 @@
+template<int rows, int cols>
+using RowMajorMatrixXd = Eigen::Matrix<double, rows, cols, Eigen::RowMajor>;
--- tests/camera/test_normalizer.py
+++ tests/camera/test_normalizer.py
@@ -5,16 +5,16 @@ from tadataka.camera.parameters import CameraParameters
 from tadataka.camera.normalizer import Normalizer
 
 
-camera_parameters = CameraParameters(focal_length=[10, 20], offset=[2, 4])
+camera_parameters = CameraParameters(focal_length=[10., 20.], offset=[2., 4.])
 
 unnormalized = np.array([
-    [12, 24],
-    [0, 0],
-    [8, 10]
+    [12., 24.],
+    [0., 0.],
+    [8., 10.]
 ])
 
 normalized = np.array([
-    [1, 1.0],
+    [1.0, 1.0],
     [-0.2, -0.2],
     [0.6, 0.3]
 ])
@@ -28,7 +28,6 @@ def test_normalize():
 
 
 def test_unnormalize():
-
     assert_array_equal(
         Normalizer(camera_parameters).unnormalize(normalized),
         unnormalized
