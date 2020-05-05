5c572a25930795053e9e5aa0bd94e77006aa022e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-05 19:04:14 +0900

    Move functions from 'matrix.cpp' to 'matrix.hpp'

--- tadataka/_matrix.cpp
+++ tadataka/_matrix.cpp
@@ -1,22 +1,6 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/eigen.h>
-
+#include "tadataka/_matrix.hpp"
 
 namespace py = pybind11;
-
-using Matrix33d = Eigen::Matrix<double, 3, 3>;
-using Matrix44d = Eigen::Matrix<double, 4, 4>;
-
-
-inline Matrix33d get_rotation(Eigen::Ref<const Matrix44d> T) {
-  return T.block<3, 3>(0, 0);
-}
-
-
-inline Eigen::Vector3d get_translation(Eigen::Ref<const Matrix44d> T) {
-  return T.block<3, 1>(0, 3);
-}
-
 
 PYBIND11_MODULE(_matrix, m) {
   m.def("get_rotation", &get_rotation,
---
+++ tadataka/_matrix.hpp
@@ -0,0 +1,16 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+
+using Matrix33d = Eigen::Matrix<double, 3, 3>;
+using Matrix44d = Eigen::Matrix<double, 4, 4>;
+
+
+inline Matrix33d get_rotation(Eigen::Ref<const Matrix44d> T) {
+  return T.block<3, 3>(0, 0);
+}
+
+
+inline Eigen::Vector3d get_translation(Eigen::Ref<const Matrix44d> T) {
+  return T.block<3, 1>(0, 3);
+}
