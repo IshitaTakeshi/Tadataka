13f4ea2c1edf52d5b8788d84366a6c1b6c7275b3
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-21 03:41:02 +0900

    Implement zero division safe float inversion

---
+++ src/numeric.rs
@@ -0,0 +1,19 @@
+extern crate test;
+
+static EPSILON: f64 = 1e-16;
+
+pub fn safe_invert(v: f64) -> f64 {
+    1. / (v + EPSILON)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_safe_invert() {
+        assert_eq!(safe_invert(10.0), 0.1);
+        assert_eq!(safe_invert(0.0), 1. / EPSILON);
+    }
+}
