15e24cd2f3e27aafd74fea41225908a3b0279b2f
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-23 04:02:23 +0900

    Reimplement homogeneous in trait

--- src/homogeneous.rs
+++ src/homogeneous.rs
@@ -1,4 +1,52 @@
-use ndarray::{arr1, arr2, stack, Array, Array1, Array2, ArrayView1, ArrayView2, Axis};
+use ndarray::{arr1, arr2, stack, Array, Array1, Array2,
+              ArrayView, ArrayView1, ArrayView2, Axis,
+              Ix1, Ix2};
+
+
+pub trait ToHomogeneous {
+    type D;
+    fn to_homogeneous(&self) -> Array<f64, Self::D>;
+}
+
+pub trait FromHomogeneous {
+    type D;
+    fn from_homogeneous(&self) -> Array<f64, Self::D>;
+}
+
+impl ToHomogeneous for Array<f64, Ix1> {
+    type D = Ix1;
+
+    fn to_homogeneous(&self) -> Array<f64, Ix1> {
+        stack![Axis(0), self.view(), Array::ones(1)]
+    }
+}
+
+impl ToHomogeneous for Array<f64, Ix2> {
+    type D = Ix2;
+
+    fn to_homogeneous(&self) -> Array<f64, Ix2> {
+        let n = self.shape()[0];
+        stack![Axis(1), self.view(), Array::ones((n, 1))]
+    }
+}
+
+impl FromHomogeneous for Array<f64, Ix1> {
+    type D = Ix1;
+
+    fn from_homogeneous(&self) -> Array<f64, Ix1> {
+        let n = self.shape()[0];
+        self.slice(s![0..n - 1]).to_owned()
+    }
+}
+
+impl FromHomogeneous for Array<f64, Ix2> {
+    type D = Ix2;
+
+    fn from_homogeneous(&self) -> Array<f64, Ix2> {
+        let n_cols = self.shape()[1];
+        self.slice(s![.., 0..n_cols - 1]).to_owned()
+    }
+}
 
 pub fn to_homogeneous_vec(x: ArrayView1<'_, f64>) -> Array1<f64> {
     stack![Axis(0), x, Array::ones(1)]
@@ -24,23 +72,30 @@ mod tests {
     use super::*; // import names from outer scope
 
     #[test]
-    fn test_to_homogeneous_vecs() {
+    fn test_to_homogeneous_1d() {
+        let point = arr1(&[2., 3.]);
+        let expected = arr1(&[2., 3., 1.]);
+        assert_eq!(point.to_homogeneous(), expected);
+    }
+
+    #[test]
+    fn test_to_homogeneous_2d() {
         let points = arr2(&[[2., 3.], [4., 5.]]);
         let expected = arr2(&[[2., 3., 1.], [4., 5., 1.]]);
-        assert_eq!(to_homogeneous_vecs(points.view()), expected);
+        assert_eq!(points.to_homogeneous(), expected);
     }
 
     #[test]
-    fn test_to_homogeneous_vec() {
-        let p = arr1(&[2., 3.]);
-        let expected = arr1(&[2., 3., 1.]);
-        assert_eq!(to_homogeneous_vec(p.view()), expected);
+    fn test_from_homogeneous_1d() {
+        let point = arr1(&[2., 3., 1.]);
+        let expected = arr1(&[2., 3.]);
+        assert_eq!(point.from_homogeneous(), expected);
     }
 
     #[test]
-    fn test_from_homogeneous_vecs() {
+    fn test_from_homogeneous_2d() {
         let points = arr2(&[[2., 3., 1.], [4., 5., 1.]]);
         let expected = arr2(&[[2., 3.], [4., 5.]]);
-        assert_eq!(from_homogeneous_vecs(&points.view()), expected);
+        assert_eq!(points.from_homogeneous(), expected);
     }
 }
