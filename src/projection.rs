e3dcf1b66800321da1b77be0b9e08697f7682f7a
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-25 13:38:26 +0900

    Implement inv projection in traits

--- src/projection.rs
+++ src/projection.rs
@@ -1,11 +1,12 @@
-use crate::homogeneous::to_homogeneous_vec;
+use crate::homogeneous::Homogeneous;
 use ndarray::{arr1, arr2, Array, Array1, Array2, ArrayView1, ArrayView2, Ix1, Ix2};
 
 static EPSILON: f64 = 1e-16;
 
-pub trait Projection {
+pub trait Projection<DepthType> {
     type D;
     fn project(&self) -> Array<f64, Self::D>;
+    fn inv_project(&self, depth: DepthType) -> Array<f64, Self::D>;
 }
 
 fn project_impl(x: ArrayView1<'_, f64>) -> Array1<f64> {
@@ -13,15 +14,23 @@ fn project_impl(x: ArrayView1<'_, f64>) -> Array1<f64> {
     &x.slice(s![0..2]) / z
 }
 
-impl Projection for Array<f64, Ix1> {
+fn inv_project_impl(x: ArrayView1<'_, f64>, depth: f64) -> Array1<f64> {
+    x.to_homogeneous() * depth
+}
+
+impl Projection<f64> for Array<f64, Ix1> {
     type D = Ix1;
 
     fn project(&self) -> Array<f64, Ix1> {
         project_impl(self.view())
     }
+
+    fn inv_project(&self, depth: f64) -> Array1<f64> {
+        inv_project_impl(self.view(), depth)
+    }
 }
 
-impl Projection for Array<f64, Ix2> {
+impl Projection<ArrayView1<'_, f64>> for Array<f64, Ix2> {
     type D = Ix2;
 
     fn project(&self) -> Array<f64, Ix2> {
@@ -32,6 +41,16 @@ impl Projection for Array<f64, Ix2> {
             us.slice_mut(s![i, ..]).assign(&u);
         }
         us
+    }
+
+    fn inv_project(&self, depths: ArrayView1<'_, f64>) -> Array2<f64> {
+        let n = self.shape()[0];
+        let mut ps = Array::zeros((n, 3));
+        for i in 0..n {
+            let p = inv_project_impl(self.slice(s![i, ..]), depths[i]);
+            ps.slice_mut(s![i, ..]).assign(&p);
+        }
+        ps
     }
 }
 
@@ -51,7 +70,7 @@ pub fn project_vecs(xs: ArrayView2<'_, f64>) -> Array2<f64> {
 }
 
 pub fn inv_project_vec(x: ArrayView1<'_, f64>, depth: f64) -> Array1<f64> {
-    to_homogeneous_vec(x) * depth
+    x.to_homogeneous() * depth
 }
 
 pub fn inv_project_vecs(
@@ -88,20 +107,21 @@ mod tests {
         assert_eq!(Projection::project(&arr1(&[3., 5., 5.])), arr1(&[0.6, 1.]));
     }
 
-    // fn test_inv_projection() {
-    //     let xs = arr2(
-    //         &[[0.5, 2.],
-    //           [-0.2, 0.6]]
-    //     );
-    //     let depths = arr1(&[2., 5.]);
+    #[test]
+    fn test_inv_projection() {
+        let xs = arr2(
+            &[[0.5, 2.],
+              [-0.2, 0.6]]
+        );
+        let depths = arr1(&[2., 5.]);
 
-    //     assert_eq!(inv_projection(xs, depths),
-    //                arr2(&[[1., 4., 2.],
-    //                       [-1., 3., 5.]]));
+        assert_eq!(Projection::inv_project(&xs, depths.view()),
+                   arr2(&[[1., 4., 2.],
+                          [-1., 3., 5.]]));
 
-    //     let x = arr1(&[0.5, 2.]);
-    //     let depth = 2.;
-    //     assert_eq!(inv_projection(x, depth),
-    //                arr1(&[1., 4., 2.]));
-    // }
+        let x = arr1(&[0.5, 2.]);
+        let depth = 2.;
+        assert_eq!(Projection::inv_project(&x, depth),
+                   arr1(&[1., 4., 2.]));
+    }
 }
