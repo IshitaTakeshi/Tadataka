5e9b6d222495eed8c60c1b4c3dcefd8b31fef10e
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-25 17:44:02 +0900

    Reimplement warp functions using traits

--- src/warp.rs
+++ src/warp.rs
@@ -1,26 +1,40 @@
-use crate::projection::{inv_project_vec, inv_project_vecs,
-                        project_vec, project_vecs};
 use crate::transform::Transform;
-use ndarray::{arr1, arr2, Array1, Array2, ArrayView1, ArrayView2};
+use crate::projection::Projection;
+use ndarray::{arr1, arr2, Array, Array1, Array2,
+              ArrayBase, ArrayView1, ArrayView2, Data, Ix1, Ix2};
 
-pub fn warp_vecs(
-    transform10: ArrayView2<'_, f64>,
-    xs0: ArrayView2<'_, f64>,
-    depths0: ArrayView1<'_, f64>,
-) -> Array2<f64> {
-    let points0 = inv_project_vecs(xs0, depths0);
-    let points1 = transform10.transform(&points0);
-    project_vecs(points1.view())
+pub trait Warp<XType, DepthType, D> {
+    fn warp(&self, x0: XType, depth0: DepthType) -> Array<f64, D>;
 }
 
-pub fn warp_vec(
-    transform10: ArrayView2<'_, f64>,
-    x0: ArrayView1<'_, f64>,
-    depth0: f64,
-) -> Array1<f64> {
-    let point0 = inv_project_vec(x0, depth0);
-    let point1 = transform10.transform(&point0);
-    project_vec(point1.view())
+impl<S> Warp<ArrayView2<'_, f64>, ArrayView1<'_, f64>, Ix2> for ArrayBase<S, Ix2>
+where
+    S: Data<Elem = f64>
+{
+    fn warp(
+        &self,
+        xs0: ArrayView2<'_, f64>,
+        depths0: ArrayView1<'_, f64>
+    ) -> Array<f64, Ix2> {
+        let points0 = Projection::inv_project(&xs0, depths0);
+        let points1 = self.transform(&points0);
+        Projection::project(&points1.view())
+    }
+}
+
+impl<S> Warp<ArrayView1<'_, f64>, f64, Ix1> for ArrayBase<S, Ix2>
+where
+    S: Data<Elem = f64>
+{
+    fn warp(
+        &self,
+        x0: ArrayView1<'_, f64>,
+        depth0: f64,
+    ) -> Array<f64, Ix1> {
+        let point0 = Projection::inv_project(&x0, depth0);
+        let point1 = self.transform(&point0);
+        Projection::project(&point1.view())
+    }
 }
 
 #[cfg(test)]
@@ -29,7 +43,7 @@ mod tests {
     use test::Bencher;
 
     #[test]
-    fn test_warp_vecs() {
+    fn test_warp_2d() {
         let transform10 = arr2(&[
             [0., 0., 1., 0.],
             [0., 1., 0., 0.],
@@ -39,14 +53,11 @@ mod tests {
         let xs0 = arr2(&[[0., 0.], [2., -1.]]);
         let depths0 = arr1(&[2., 4.]);
         let xs1 = arr2(&[[0.5, 0.0], [-1.0, 1.0]]);
-        assert_eq!(
-            warp_vecs(transform10.view(), xs0.view(), depths0.view()),
-            xs1
-        );
+        assert_eq!(Warp::warp(&transform10.view(), xs0.view(), depths0.view()), xs1);
     }
 
     #[test]
-    fn test_warp_vec() {
+    fn test_warp_1d() {
         let transform10 = arr2(&[
             [0., 0., 1., 0.],
             [0., 1., 0., 0.],
@@ -56,11 +67,11 @@ mod tests {
         let xs0 = arr1(&[0., 0.]);
         let depth0 = 2.;
         let xs1 = arr1(&[0.5, 0.0]);
-        assert_eq!(warp_vec(transform10.view(), xs0.view(), depth0), xs1);
+        assert_eq!(Warp::warp(&transform10.view(), xs0.view(), depth0), xs1);
     }
 
     #[bench]
-    fn bench_warp_vecs(b: &mut Bencher) {
+    fn bench_warp_2d(b: &mut Bencher) {
         let transform10 = arr2(&[
             [0., 0., 1., 0.],
             [0., 1., 0., 0.],
@@ -72,6 +83,6 @@ mod tests {
         let transform10_view = transform10.view();
         let xs0_view = xs0.view();
         let depths0_view = depths0.view();
-        b.iter(|| warp_vecs(transform10_view, xs0_view, depths0_view))
+        b.iter(|| Warp::warp(&transform10_view, xs0_view, depths0_view))
     }
 }
