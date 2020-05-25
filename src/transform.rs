b02f2b4d1d74dc74ba724d418c4b9308ab07aaad
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-25 13:10:04 +0900

    Implement Transform traits using macros

--- src/transform.rs
+++ src/transform.rs
@@ -1,22 +1,38 @@
-extern crate test;
+use crate::homogeneous::Homogeneous;
+use ndarray::{arr1, arr2, Array, Array2, ArrayView, ArrayView2, ArrayBase,
+              RawData, Data, Dim, LinalgScalar, Ix1, Ix2};
 
-use crate::homogeneous::{from_homogeneous_vecs, to_homogeneous_vecs};
-use ndarray::{arr2, Array2, ArrayView2};
-
-pub fn transform(transform10: ArrayView2<'_, f64>, points0: ArrayView2<'_, f64>) -> Array2<f64> {
-    let points0 = to_homogeneous_vecs(points0);
-    let points0 = points0.t();
-    let points1 = transform10.dot(&points0);
-    let points1 = points1.t();
-    from_homogeneous_vecs(&points1.view()).to_owned()
+pub trait Transform<A, D, Rhs> {
+    fn transform(&self, points0: &Rhs) -> Array<A, D>;
 }
+
+macro_rules! impl_transform {
+    (for $($d:ty),+) => {
+        $(impl<A, S1, S2> Transform<A, $d, ArrayBase<S2, $d>> for ArrayBase<S1, Ix2>
+        where
+            S1: Data<Elem = A>,
+            S2: Data<Elem = A>,
+            A: LinalgScalar,
+        {
+            fn transform(&self, points0: &ArrayBase<S2, $d>) -> Array<A, $d> {
+                let points0 = &points0.to_homogeneous();
+                let points0 = points0.t();
+                let points1 = self.dot(&points0);
+                let points1 = points1.t();
+                points1.from_homogeneous().to_owned()
+            }
+        })*
+    }
+}
+
+impl_transform!(for Ix1, Ix2);
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_transform() {
+    fn test_transform_points() {
         let points0 = arr2(&[[1., 2., 5.], [4., -2., 3.]]);
 
         let transform10 = arr2(&[
@@ -28,6 +44,23 @@ mod tests {
 
         let points1 = arr2(&[[2., -3., 5.], [5., -1., 1.]]);
 
-        assert_eq!(transform(transform10.view(), points0.view()), points1)
+        // assert_eq!(transform(&transform10, &points0), points1);
+        assert_eq!(transform10.transform(&points0), points1);
+    }
+
+    #[test]
+    fn test_transform_point() {
+        let point0 = arr1(&[1., 2., 5.]);
+
+        let transform10 = arr2(&[
+            [1., 0., 0., 1.],
+            [0., 0., -1., 2.],
+            [0., 1., 0., 3.],
+            [0., 0., 0., 1.],
+        ]);
+
+        let point1 = arr1(&[2., -3., 5.]);
+
+        assert_eq!(transform10.transform(&point0), point1);
     }
 }
--- src/warp.rs
+++ src/warp.rs
@@ -1,17 +1,26 @@
-extern crate test;
+use crate::projection::{inv_project_vec, inv_project_vecs,
+                        project_vec, project_vecs};
+use crate::transform::Transform;
+use ndarray::{arr1, arr2, Array1, Array2, ArrayView1, ArrayView2};
 
-use crate::projection::{inv_project_vecs, project_vecs};
-use crate::transform::transform;
-use ndarray::{arr1, arr2, Array2, ArrayView1, ArrayView2};
-
-pub fn warp(
+pub fn warp_vecs(
     transform10: ArrayView2<'_, f64>,
     xs0: ArrayView2<'_, f64>,
     depths0: ArrayView1<'_, f64>,
 ) -> Array2<f64> {
     let points0 = inv_project_vecs(xs0, depths0);
-    let points1 = transform(transform10, points0.view());
+    let points1 = transform10.transform(&points0);
     project_vecs(points1.view())
+}
+
+pub fn warp_vec(
+    transform10: ArrayView2<'_, f64>,
+    x0: ArrayView1<'_, f64>,
+    depth0: f64,
+) -> Array1<f64> {
+    let point0 = inv_project_vec(x0, depth0);
+    let point1 = transform10.transform(&point0);
+    project_vec(point1.view())
 }
 
 #[cfg(test)]
@@ -20,7 +29,7 @@ mod tests {
     use test::Bencher;
 
     #[test]
-    fn test_warp() {
+    fn test_warp_vecs() {
         let transform10 = arr2(&[
             [0., 0., 1., 0.],
             [0., 1., 0., 0.],
@@ -30,11 +39,28 @@ mod tests {
         let xs0 = arr2(&[[0., 0.], [2., -1.]]);
         let depths0 = arr1(&[2., 4.]);
         let xs1 = arr2(&[[0.5, 0.0], [-1.0, 1.0]]);
-        assert_eq!(warp(transform10.view(), xs0.view(), depths0.view()), xs1);
+        assert_eq!(
+            warp_vecs(transform10.view(), xs0.view(), depths0.view()),
+            xs1
+        );
+    }
+
+    #[test]
+    fn test_warp_vec() {
+        let transform10 = arr2(&[
+            [0., 0., 1., 0.],
+            [0., 1., 0., 0.],
+            [-1., 0., 0., 4.],
+            [0., 0., 0., 1.],
+        ]);
+        let xs0 = arr1(&[0., 0.]);
+        let depth0 = 2.;
+        let xs1 = arr1(&[0.5, 0.0]);
+        assert_eq!(warp_vec(transform10.view(), xs0.view(), depth0), xs1);
     }
 
     #[bench]
-    fn bench_warp(b: &mut Bencher) {
+    fn bench_warp_vecs(b: &mut Bencher) {
         let transform10 = arr2(&[
             [0., 0., 1., 0.],
             [0., 1., 0., 0.],
@@ -46,6 +72,6 @@ mod tests {
         let transform10_view = transform10.view();
         let xs0_view = xs0.view();
         let depths0_view = depths0.view();
-        b.iter(|| warp(transform10_view, xs0_view, depths0_view))
+        b.iter(|| warp_vecs(transform10_view, xs0_view, depths0_view))
     }
 }
