4827842374f90f33cae408017a8b97e9fca900c2
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-26 16:16:21 +0900

    Add functions to make matrix and get (R, t)

--- src/transform.rs
+++ src/transform.rs
@@ -1,6 +1,6 @@
 use crate::homogeneous::Homogeneous;
-use ndarray::{arr1, arr2, Array, Array2, ArrayView, ArrayView2, ArrayBase,
-              RawData, Data, Dim, LinalgScalar, Ix1, Ix2};
+use ndarray::{arr1, arr2, Array, Array2, ArrayBase, ArrayView1, ArrayView2, Axis,
+              Data, LinalgScalar, Ix1, Ix2, stack};
 
 pub trait Transform<A, D, Rhs> {
     fn transform(&self, points0: &Rhs) -> Array<A, D>;
@@ -27,6 +27,38 @@ macro_rules! impl_transform {
 
 impl_transform!(for Ix1, Ix2);
 
+pub fn get_rotation<'a, A: LinalgScalar>(
+    transform: &'a ArrayView2<'a, A>
+) -> ArrayView2<'a, A> {
+    transform.slice(s![0..3, 0..3])
+}
+
+pub fn get_translation<'a, A: LinalgScalar>(
+    transform: &'a ArrayView2<'a, A>
+) -> ArrayView1<'a, A> {
+    transform.slice(s![0..3, 3])
+}
+
+pub fn make_matrix<A, S1, S2>(
+    rotation: &ArrayBase<S1, Ix2>,
+    translation: &ArrayBase<S2, Ix1>
+) -> Array2<A>
+where
+    S1: Data<Elem = A>,
+    S2: Data<Elem = A>,
+    A: LinalgScalar
+{
+    let d = rotation.shape()[1];
+
+    let translation = translation.to_owned();
+    let translation = translation.into_shape((d, 1)).unwrap();
+
+    stack![Axis(0),
+        stack![Axis(1), rotation.view(), translation.view()],
+        stack![Axis(1), Array::zeros((1, d)), Array::ones((1, 1))]
+    ]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -44,7 +76,6 @@ mod tests {
 
         let points1 = arr2(&[[2., -3., 5.], [5., -1., 1.]]);
 
-        // assert_eq!(transform(&transform10, &points0), points1);
         assert_eq!(transform10.transform(&points0), points1);
     }
 
@@ -62,5 +93,40 @@ mod tests {
         let point1 = arr1(&[2., -3., 5.]);
 
         assert_eq!(transform10.transform(&point0), point1);
+    }
+
+    #[test]
+    fn test_get_rotation() {
+        let transform = arr2(&[[1., 2., 3., -1.],
+                               [4., 5., 6., -2.],
+                               [7., 8., 9., -3.],
+                               [0., 0., 0., 1.]]);
+        let expected = arr2(&[[1., 2., 3.],
+                              [4., 5., 6.],
+                              [7., 8., 9.]]);
+        assert_eq!(get_rotation(&transform.view()), expected.view());
+    }
+
+    #[test]
+    fn test_get_translation() {
+        let transform = arr2(&[[1., 2., 3., -1.],
+                               [4., 5., 6., -2.],
+                               [7., 8., 9., -3.],
+                               [0., 0., 0., 1.]]);
+        let expected = arr1(&[-1., -2., -3.]);
+        assert_eq!(get_translation(&transform.view()), expected.view());
+    }
+
+    #[test]
+    fn test_make_matrix() {
+        let rotation = arr2(&[[1., 2., 3.],
+                              [4., 5., 6.],
+                              [7., 8., 9.]]);
+        let translation = arr1(&[-1., -2., -3.]);
+        let expected = arr2(&[[1., 2., 3., -1.],
+                              [4., 5., 6., -2.],
+                              [7., 8., 9., -3.],
+                              [0., 0., 0., 1.]]);
+        assert_eq!(make_matrix(&rotation, &translation), expected);
     }
 }
