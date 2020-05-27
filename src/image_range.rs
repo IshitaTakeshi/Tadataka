937e657034e92c2b6957cb1115b3f7a6aa33101b
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-27 18:44:57 +0900

    Take image shape in &[usize]

--- src/image_range.rs
+++ src/image_range.rs
@@ -1,16 +1,16 @@
-use ndarray::{arr1, arr2, ArrayBase, ArrayView2, Data, Ix1, Ix2};
+use ndarray::{arr1, arr2, ArrayBase, Data, Ix1, Ix2};
 use num::NumCast;
 use num_traits::float::Float;
 use std::vec::Vec;
 
 pub trait ImageRange<D, OutputType> {
-    fn is_in_range(&self, image_shape: (i64, i64)) -> OutputType;
+    fn is_in_range(&self, image_shape: &[usize]) -> OutputType;
 }
 
 #[inline]
-fn is_in_range<A: Float>(x: A, y: A, image_shape: (i64, i64)) -> bool {
-    let h = image_shape.0 as f64;
-    let w = image_shape.1 as f64;
+fn is_in_range<A: Float>(x: A, y: A, image_shape: &[usize]) -> bool {
+    let h = image_shape[0] as f64;
+    let w = image_shape[1] as f64;
     let x = NumCast::from(x).unwrap();
     let y = NumCast::from(y).unwrap();
     0. <= x && x <= w-1. && 0. <= y && y <= h-1.
@@ -21,7 +21,7 @@ where
     S: Data<Elem = A>,
     A: Float,
 {
-    fn is_in_range(&self, image_shape: (i64, i64)) -> bool {
+    fn is_in_range(&self, image_shape: &[usize]) -> bool {
         is_in_range(self[0], self[1], image_shape)
     }
 }
@@ -31,7 +31,7 @@ where
     S: Data<Elem = A>,
     A: Float,
 {
-    fn is_in_range(&self, image_shape: (i64, i64)) -> Vec<bool> {
+    fn is_in_range(&self, image_shape: &[usize]) -> Vec<bool> {
         let n = self.shape()[0];
         let mut mask = Vec::new();
         for i in 0..n {
@@ -44,7 +44,7 @@ where
 
 pub fn all_in_range<S: Data<Elem = A>, A: Float>(
     keypoints: &ArrayBase<S, Ix2>,
-    image_shape: (i64, i64)  // NOTE the order (h, w)
+    image_shape: &[usize],
 ) -> bool {
     let n = keypoints.shape()[0];
     for i in 0..n {
@@ -63,12 +63,12 @@ mod tests {
     #[test]
     fn test_is_in_range() {
         let (width, height) = (20, 30);
-        let image_shape = (height, width);
-        assert!(arr1(&[0., 0.]).is_in_range(image_shape));
-        assert!(!arr1(&[-1., 0.]).is_in_range(image_shape));
-        assert!(!arr1(&[0., -1.]).is_in_range(image_shape));
-        assert!(!arr1(&[0., 30.]).is_in_range(image_shape));
-        assert!(!arr1(&[20., 0.]).is_in_range(image_shape));
+        let image_shape: [usize; 2] = [height, width];
+        assert!(arr1(&[0., 0.]).is_in_range(&image_shape));
+        assert!(!arr1(&[-1., 0.]).is_in_range(&image_shape));
+        assert!(!arr1(&[0., -1.]).is_in_range(&image_shape));
+        assert!(!arr1(&[0., 30.]).is_in_range(&image_shape));
+        assert!(!arr1(&[20., 0.]).is_in_range(&image_shape));
 
         let keypoints = arr2(
             &[[19., 29.],
@@ -82,7 +82,7 @@ mod tests {
         );
 
         assert_eq!(
-            keypoints.is_in_range(image_shape),
+            keypoints.is_in_range(&image_shape),
             [true, true, true, false, false, false, false, false]
         );
 
@@ -98,7 +98,7 @@ mod tests {
         );
 
         assert_eq!(
-            keypoints.is_in_range(image_shape),
+            keypoints.is_in_range(&image_shape),
             [true, false, false, false, true, false, false, false]
         );
     }
@@ -106,7 +106,7 @@ mod tests {
     #[test]
     fn test_all_in_range() {
         let (width, height) = (20, 30);
-        let image_shape = (height, width);
+        let image_shape: [usize; 2] = [height, width];
 
         let keypoints = arr2(
             &[[19., 29.],
@@ -115,7 +115,7 @@ mod tests {
               [-1., 29.]] // this is out of image range
         );
 
-        assert!(!all_in_range(&keypoints, image_shape));
+        assert!(!all_in_range(&keypoints, &image_shape));
 
         let keypoints = arr2(
             &[[19., 29.],
@@ -123,6 +123,6 @@ mod tests {
               [0., 29.]]
         );
 
-        assert!(all_in_range(&keypoints, image_shape));
+        assert!(all_in_range(&keypoints, &image_shape));
     }
 }
