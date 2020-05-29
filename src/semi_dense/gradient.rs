4e30a39c4b687e5c00e711d8493379276f96bbfd
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-29 19:50:19 +0900

    Add a struct to manage image gradient

--- src/semi_dense/gradient.rs
+++ src/semi_dense/gradient.rs
@@ -1,22 +1,53 @@
-use ndarray::{arr1, Array, Array1, ArrayView1};
+use ndarray::{arr1, arr2, Array1, Array2, ArrayBase, Data, Ix1, Ix2};
+use crate::gradient::{sobel_x, sobel_y};
+use crate::interpolation::Interpolation;
 
-fn gradient(intensities: ArrayView1<'_, f64>) -> Array1<f64> {
-    let n = intensities.shape()[0];
-    let mut grad = Array::zeros(n - 1);
-    for i in 0..n - 1 {
-        grad[i] = intensities[i + 1] - intensities[i];
+pub struct ImageGradient {
+    gx: Array2<f64>,
+    gy: Array2<f64>
+}
+
+impl ImageGradient {
+    pub fn new<S: Data<Elem = f64>>(image: &ArrayBase<S, Ix2>) -> Self {
+        let gx = sobel_x(image);
+        let gy = sobel_y(image);
+        ImageGradient { gx: gx, gy: gy }
     }
-    grad
+
+    pub fn get<S: Data<Elem = f64>>(
+        &self,
+        coordinate: &ArrayBase<S, Ix1>
+    ) -> Array1<f64> {
+        let gx = self.gx.interpolate(coordinate);
+        let gy = self.gy.interpolate(coordinate);
+        arr1(&[gx, gy])
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn run(grad: &ImageGradient, c: &Array1<f64>) {
+        let expected = arr1(&[grad.gx.interpolate(c), grad.gy.interpolate(c)]);
+        assert_eq!(grad.get(&c), expected);
+    }
+
     #[test]
-    fn test_gradient() {
-        let intensities = arr1(&[-1., 1., 0., 3., -2.]);
-        let expected = arr1(&[1. - (-1.), 0. - 1., 3. - 0., -2. - 3.]);
-        assert_eq!(gradient(intensities.view()), expected);
+    fn test_get() {
+        let gx = arr2(
+            &[[0., 2., 1.],
+              [0., 1., 1.],
+              [1., 2., 0.]]
+        );
+        let gy = arr2(
+            &[[1., 1., 0.],
+              [0., 2., 1.],
+              [1., 0., 1.]]
+        );
+        let c = arr1(&[0.3, 1.2]);
+        let grad = ImageGradient { gx: gx, gy: gy };
+
+        run(&grad, &c);
     }
 }
