e276da1e7189feb7f0aad09345fc3a9dc2e7f261
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-05-31 10:27:35 +0900

    Compute epipolar direction in 'epipolar.rs'

--- src/semi_dense/epipolar.rs
+++ src/semi_dense/epipolar.rs
@@ -1,17 +1,17 @@
-extern crate test;
+use crate::projection::Projection;
 use crate::vector::normalize;
-use ndarray::{arr1, arr2, Array, Array2, ArrayView1};
+use ndarray::{arr1, arr2, Array, Array1, Array2, ArrayView1};
 use ndarray_linalg::Norm;
 
 static EPSILON: f64 = 1e-16;
 
-fn key_coordinates(
-    epipolar_direction: ArrayView1<'_, f64>,
-    x_key: ArrayView1<'_, f64>,
+fn key_coordinates_(
+    epipolar_direction: &Array1<f64>,
+    x_key: &Array1<f64>,
     step_size: f64,
 ) -> Array2<f64> {
     let sampling_steps = arr1(&[-2., -1., 0., 1., 2.]);
-    let direction = normalize(epipolar_direction.view());
+    let direction = normalize(&epipolar_direction);
     let n = sampling_steps.shape()[0];
     let mut coordinates = Array::zeros((n, 2));
     for (i, step) in sampling_steps.iter().enumerate() {
@@ -21,9 +21,18 @@ fn key_coordinates(
     coordinates
 }
 
-fn ref_coordinates(
-    x_min: ArrayView1<'_, f64>,
-    x_max: ArrayView1<f64>,
+pub fn key_coordinates(
+    t_rk: &ArrayView1<f64>,
+    x_key: &Array1<f64>,
+    step_size: f64
+) -> Array2<f64> {
+    let d = x_key.to_owned() - Projection::project(t_rk);
+    key_coordinates_(&d, x_key, step_size)
+}
+
+pub fn ref_coordinates(
+    x_min: &Array1<f64>,
+    x_max: &Array1<f64>,
     step_size: f64,
 ) -> Array2<f64> {
     let direction = x_max.to_owned() - x_min;
@@ -45,7 +54,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_key_coordinates() {
+    fn test_key_coordinates_() {
         let x_key = arr1(&[7., 8.]);
         let direction = arr1(&[9., 12.]);
         let step_size = 5.;
@@ -58,7 +67,7 @@ mod tests {
             [7. + 2. * 3., 8. + 2. * 4.],
         ]);
         assert_eq!(
-            key_coordinates(direction.view(), x_key.view(), step_size),
+            key_coordinates_(&direction, &x_key, step_size),
             expected
         );
     }
@@ -68,7 +77,7 @@ mod tests {
         let search_step = 5.0;
         let x_min = arr1(&[-15.0, -20.0]);
         let x_max = arr1(&[15.0, 20.0]);
-        let xs = ref_coordinates(x_min.view(), x_max.view(), search_step);
+        let xs = ref_coordinates(&x_min, &x_max, search_step);
 
         let xs_true = arr2(&[
             [-15., -20.],
