b812425a613e33cdc242a061bbf56fa1a3ecb798
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-06-04 14:04:31 +0900

    Represent flags in i64

--- src/semi_dense/flag.rs
+++ src/semi_dense/flag.rs
@@ -2,14 +2,14 @@
 #[derive(Debug)]
 #[derive(PartialEq)]
 pub enum Flag {
-    Success,
-    HypothesisOutOfSerchRange,
-    KeyOutOfRange,
-    RefCloseOutOfRange,
-    RefFarOutOfRange,
-    RefEpipolarTooShort,
-    InsufficientGradient,
-    NegativePriorDepth,
-    NegativeRefDepth,
-    NotProcessed
+    Success = 0,
+    HypothesisOutOfSerchRange = -1,
+    KeyOutOfRange = -2,
+    RefCloseOutOfRange = -3,
+    RefFarOutOfRange = -4,
+    RefEpipolarTooShort = -5,
+    InsufficientGradient = -6,
+    NegativePriorDepth = -7,
+    NegativeRefDepth = -8,
+    NotProcessed = -9
 }
