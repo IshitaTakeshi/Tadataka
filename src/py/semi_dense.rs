f673a48ea2ca0b551b36f7d1c17457a534616367
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-06-18 18:39:08 +0900

    Add a debug interface for depth estimation

--- src/py/semi_dense.rs
+++ src/py/semi_dense.rs
@@ -1,13 +1,11 @@
 use crate::camera::CameraParameters;
-use crate::semi_dense::semi_dense;
-use crate::semi_dense::{Frame, Params};
+use crate::semi_dense::{Flag, Frame, Hypothesis, Params, VarianceCoefficients};
+use crate::semi_dense::gradient::ImageGradient;
+use crate::semi_dense::hypothesis;
 use crate::semi_dense::numeric::Inverse;
-use crate::semi_dense::VarianceCoefficients;
-use numpy::{IntoPyArray, PyArray, PyArray1, PyArray2};
-use numpy::types::TypeNum;
-use numpy::npyffi;
-use ndarray::{Ix1, Dimension};
-use pyo3::PyDowncastError;
+use crate::semi_dense::semi_dense;
+use ndarray::arr1;
+use numpy::{IntoPyArray, PyArray1, PyArray2};
 use pyo3::types::{PyAny, PyList, PyTuple};
 use pyo3::prelude::{pyfunction, pymodule, PyModule, PyResult,
                     Py, Python, pymethods, ToPyObject, PyObject, FromPyObject};
@@ -77,6 +75,39 @@ impl ToPyObject for Ret {
     }
 }
 
+// interface for debug
+#[pyfunction]
+fn estimate_debug_<'a>(
+    _py: Python<'a>,
+    u_key: &PyArray1<i64>,
+    prior_depth: f64,
+    prior_variance: f64,
+    keyframe: &Frame,
+    refframe: &Frame,
+    params: &Params,
+) -> (f64, f64, i64) {
+    let image_grad = ImageGradient::new(&keyframe.image);
+
+    let result = hypothesis::check_args(prior_depth.inv(),
+                                        prior_variance, params.inv_depth_range);
+    if let Err(flag) = result {
+        return (prior_depth, prior_variance, flag as i64);
+    }
+
+    let prior = Hypothesis::new(prior_depth.inv(), prior_variance,
+                                params.inv_depth_range);
+
+    let u_key = u_key.as_array();
+    let u_key = arr1(&[u_key[0] as f64, u_key[1] as f64]);
+    let result = semi_dense::estimate(&u_key, &prior,
+                                      keyframe, refframe, &image_grad, params);
+    match result {
+        Err(flag) => return (prior_depth, prior_variance, flag as i64),
+        Ok(h) => return (h.inv_depth.inv(), h.variance, Flag::Success as i64),
+    };
+}
+
+
 #[pyfunction]
 fn update_depth<'a>(
     py: Python<'a>,
@@ -113,6 +144,7 @@ fn update_depth<'a>(
 fn semi_dense_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<Frame>()?;
     m.add_class::<Params>()?;
+    m.add_wrapped(wrap_pyfunction!(estimate_debug_))?;
     m.add_wrapped(wrap_pyfunction!(update_depth))?;
 
     Ok(())
