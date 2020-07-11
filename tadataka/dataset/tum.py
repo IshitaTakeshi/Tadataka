1921cf4804dc5bd8ceab4026e1154fed524aa7ba
IshitaTakeshi <ishitah.takeshi@gmail.com>
2020-07-11 14:49:00 +0900

    Add comments

--- tadataka/dataset/tum.py
+++ tadataka/dataset/tum.py
@@ -49,11 +49,17 @@ def load_image_paths(filepath, prefix, delimiter=' '):
 
 
 def synchronize(timestamps1, timestamps2, timestamps_ref, max_diff=np.inf):
+    # match timestamps and return indices
+    # timestamps1[matches01[i, 0]] should be the closest to
+    # timestamps_ref[matches01[i, 1]]
     matches01 = match_timestamps(timestamps_ref, timestamps1, max_diff)
     matches02 = match_timestamps(timestamps_ref, timestamps2, max_diff)
+
+    # find shared neighbors
     indices_ref, indices1, indices2 = np.intersect1d(
         matches01[:, 0], matches02[:, 0], return_indices=True
     )
+
     return np.column_stack((matches01[indices1, 1],
                             matches02[indices2, 1],
                             indices_ref))
